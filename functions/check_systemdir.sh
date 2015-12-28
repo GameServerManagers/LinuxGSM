496463f45c278ffa2f2ad12b9d419ffbb849861d
Daniel Gibbs <me@danielgibbs.co.uk>
2015-12-28 16:57:10 +0000

    Added ability to be logged if available.

--- functions/check_systemdir.sh
+++ functions/check_systemdir.sh
@@ -6,5 +6,8 @@ lgsm_version="271215"
 
 if [ ! -d "${systemdir}" ]; then
 	fn_printfailnl "Cannot access ${systemdir}: No such directory"
+	if [ -d "${scriptlogdir}" ]; then
+		fn_scriptlog "Cannot access ${systemdir}: No such directory."
+	fi		
 	exit 1
 fi
