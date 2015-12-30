f74be89e1c03a926133d429c3252b479dd7f6bd8
Daniel Gibbs <me@danielgibbs.co.uk>
2015-12-30 20:47:38 +0000

    removed server fixes menu

--- functions/command_install.sh
+++ functions/command_install.sh
@@ -26,11 +26,7 @@ elif [ ! -z "${appid}" ]; then
 fi
 
 # Configuration
-echo ""
-echo "Applying ${gamename} Server Fixes"
-echo "================================="
 fix.sh
-echo ""
 install_logs.sh
 install_gsquery.sh
 install_config.sh
