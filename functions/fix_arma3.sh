dbc0832894141c030cd0e9fe929591412a289d8e
Daniel Gibbs <me@danielgibbs.co.uk>
2015-12-30 20:57:27 +0000

    renamed function

--- functions/fix_arma3.sh
+++ functions/fix_arma3.sh
@@ -6,7 +6,7 @@ lgsm_version="301215"
 
 # Fixes line 63: 20150 Segmentation fault (core dumped) #488
 
-fn_fix_steamcmd_msg_start(){
+fn_msg_start(){
 	fn_printdots "Applying ${fixname} fix: ${gamename}"
 	sleep 1
 	fn_printinfo "Applying ${fixname} fix: ${gamename}"
@@ -14,7 +14,7 @@ fn_fix_steamcmd_msg_start(){
 	sleep 1
 }
 
-fn_fix_steamcmd_msg_end(){
+fn_msg_end(){
 	if [ $? -ne 0 ]; then
 		fn_printfailnl "Applying ${fixname} fix: ${gamename}"
 		fn_scriptlog "Failure! Applying ${fixname} fix: ${gamename}"
@@ -27,7 +27,7 @@ fn_fix_steamcmd_msg_end(){
 # Fixes: server not always creating steam_appid.txt file.
 if [ -d "${rootdir}/.local/share/Arma\ 3" ]; then
 	local fixname="20150 Segmentation fault (core dumped)"
-	fn_fix_steamcmd_msg_start
+	fn_msg_start
 	mkdir -p "${rootdir}/.local/share/Arma\ 3"
-	fn_fix_steamcmd_msg_end
+	fn_msg_end
 fi
--- functions/fix_csgo.sh
+++ functions/fix_csgo.sh
@@ -6,7 +6,7 @@ lgsm_version="301215"
 
 # Description: Resolves various issues with csgo.
 
-fn_fix_steamcmd_msg_start(){
+fn_msg_start(){
 	fn_printdots "Applying ${fixname} fix: ${gamename}"
 	sleep 1
 	fn_printinfo "Applying ${fixname} fix: ${gamename}"
@@ -14,7 +14,7 @@ fn_fix_steamcmd_msg_start(){
 	sleep 1
 }
 
-fn_fix_steamcmd_msg_end(){
+fn_msg_end(){
 	if [ $? -ne 0 ]; then
 		fn_printfailnl "Applying ${fixname} fix: ${gamename}"
 		fn_scriptlog "Failure! Applying ${fixname} fix: ${gamename}"
@@ -27,35 +27,35 @@ fn_fix_steamcmd_msg_end(){
 # Fixes: server not always creating steam_appid.txt file.
 if [ ! -f "${filesdir}/steam_appid.txt" ]; then
 	local fixname="730 steam_appid.txt"
-	fn_fix_steamcmd_msg_start
+	fn_msg_start
 	echo -n "730" >> "${filesdir}/steam_appid.txt"
-	fn_fix_steamcmd_msg_end
+	fn_msg_end
 fi
 
 # Fixes: Error parsing BotProfile.db - unknown attribute 'Rank'".
 if ! grep -q "//Rank" "${systemdir}/botprofile.db" > /dev/null 2>&1; then
 	local fixname="botprofile.db"
-	fn_fix_steamcmd_msg_start
+	fn_msg_start
 	sed -i 's/\tRank/\t\/\/Rank/g' "${systemdir}/botprofile.db" > /dev/null 2>&1
-	fn_fix_steamcmd_msg_end
+	fn_msg_end
 fi
 
 # Fixes: Unknown command "cl_bobamt_vert".
 if ! grep -q "//exec default" "${servercfgdir}/valve.rc" > /dev/null 2>&1 || ! grep -q "//exec joystick" "${servercfgdir}/valve.rc" > /dev/null 2>&1; then
 	local fixname="valve.rc"
-	fn_fix_steamcmd_msg_start
+	fn_msg_start
 	sed -i 's/exec default.cfg/\/\/exec default.cfg/g' "${servercfgdir}/valve.rc" > /dev/null 2>&1
 	sed -i 's/exec joystick.cfg/\/\/exec joystick.cfg/g' "${servercfgdir}/valve.rc" > /dev/null 2>&1
-	fn_fix_steamcmd_msg_end
+	fn_msg_end
 fi
 
 # Fixes: workshop map issue.
 # http://forums.steampowered.com/forums/showthread.php?t=3170366.
 if [ -f "${systemdir}/subscribed_collection_ids.txt" ]||[ -f "${systemdir}/subscribed_file_ids.txt" ]||[ -f "${systemdir}/ugc_collection_cache.txt" ]; then
 	local fixname="workshop map"
-	fn_fix_steamcmd_msg_start
+	fn_msg_start
 	rm -f "${systemdir}/subscribed_collection_ids.txt"
 	rm -f "${systemdir}/subscribed_file_ids.txt"
 	rm -f "${systemdir}/ugc_collection_cache.txt"
-	fn_fix_steamcmd_msg_end
+	fn_msg_end
 fi
