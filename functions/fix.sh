0d05da1a2294d1098c7e5090d66d473d53e276c8
Daniel Gibbs <me@danielgibbs.co.uk>
2016-01-01 21:30:14 +0000

    Fix menu will now only appear for fixes

--- functions/fix.sh
+++ functions/fix.sh
@@ -2,7 +2,7 @@
 # LGSM fix.sh function
 # Author: Daniel Gibbs
 # Website: http://gameservermanagers.com
-lgsm_version="301215"
+lgsm_version="010116"
 
 # Description: Overall function for managing fixes.
 # Runs functions that will fix an issue.
@@ -26,17 +26,29 @@ fi
 # Fixes that are run on install only.
 if [ "${function_selfname}" == "command_install.sh" ]; then
 	fix_glibc.sh
-	echo ""
-	echo "Applying ${gamename} Server Fixes"
-	echo "================================="
-	sleep 1
 	if [ "${gamename}" == "Killing Floor" ]; then
+		echo ""
+		echo "Applying ${gamename} Server Fixes"
+		echo "================================="
+		sleep 1			
 		fix_kf.sh
 	elif [ "${gamename}" == "Red Orchestra: Ostfront 41-45" ]; then
+		echo ""
+		echo "Applying ${gamename} Server Fixes"
+		echo "================================="
+		sleep 1		
 		fix_ro.sh
 	elif [ "${gamename}" == "Unreal Tournament 2004" ]; then
+		echo ""
+		echo "Applying ${gamename} Server Fixes"
+		echo "================================="
+		sleep 1		
 		fix_ut2k4.sh
 	elif [ "${gamename}" == "Unreal Tournament 99" ]; then
+		echo ""
+		echo "Applying ${gamename} Server Fixes"
+		echo "================================="
+		sleep 1		
 		fix_ut99.sh
 	fi
 fi
