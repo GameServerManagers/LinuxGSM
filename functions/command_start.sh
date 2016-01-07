9dd1b0c83e1f04e19245de1e5d7b167c1dde2791
Daniel Gibbs <me@danielgibbs.co.uk>
2016-01-07 22:45:06 +0000

    minor fix

--- functions/command_start.sh
+++ functions/command_start.sh
@@ -55,7 +55,7 @@ mv "${scriptlog}" "${scriptlogdate}"
 # Create lock file
 date > "${rootdir}/${lockselfname}"
 cd "${executabledir}"
-if [ "${ts2serverpass}" == "1" ];then
+if [ "${ts3serverpass}" == "1" ];then
 	./ts3server_startscript.sh start serveradmin_password="${newpassword}" 
 else
 	./ts3server_startscript.sh start inifile="${servercfgfullpath}" > /dev/null 2>&1
