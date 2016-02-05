12e8dd1fc2747cef9255de6bfee4ab9a2a77caba
Daniel Gibbs <me@danielgibbs.co.uk>
2016-02-05 10:54:08 +0000

    date

--- functions/core_functions.sh
+++ functions/core_functions.sh
@@ -2,7 +2,7 @@
 # LGSM core_functions.sh function
 # Author: Daniel Gibbs
 # Website: http://gameservermanagers.com
-lgsm_version="170116"
+lgsm_version="030216"
 
 # Description: Defines all functions to allow download and execution of functions using fn_runfunction.
 # This function is called first before any other function. Without this file other functions would not load.
