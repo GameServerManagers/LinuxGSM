2162b357233bf51b1c00a03d57dc8e3ec93a084a
Daniel Gibbs <me@danielgibbs.co.uk>
2016-02-05 11:34:37 +0000

    using ut99 as test

--- functions/install_dl_ut99.sh
+++ functions/install_dl_ut99.sh
@@ -2,34 +2,17 @@
 # LGSM install_dl_ut99.sh function
 # Author: Daniel Gibbs
 # Website: http://gameservermanagers.com
-lgsm_version="271215"
+lgsm_version="050216"
 
 echo ""
 echo "Downloading Server Files"
 echo "================================="
 sleep 1
-cd "${filesdir}"
-if [ ! -f ut-server-436.tar.gz ]; then
-	wget http://gameservermanagers.com/files/ut99/ut-server-436.tar.gz
-else
-	echo "ut-server-436.tar.gz already downloaded!"
-fi
-echo "Running MD5 checksum to verify the file"
-sleep 1
-echo "MD5 checksum: 10cd7353aa9d758a075c600a6dd193fd"
-md5check=$(md5sum ut-server-436.tar.gz|awk '{print $1;}')
-echo "File returned: ${md5check}"
-if [ "${md5check}" != "10cd7353aa9d758a075c600a6dd193fd" ]; then
-	echo "MD5 checksum: FAILED!"
-	read -p "Retry download? [y/N]" yn
-	case $yn in
-	[Yy]* ) rm -fv ut-server-436.tar.gz; fn_filesdl;;
-	[Nn]* ) echo Exiting; exit;;
-	* ) echo "Please answer yes or no.";;
-	esac
-else
-	echo "MD5 checksum: PASSED"
-fi
+fn_dl "ut-server-436.tar.gz" "${filesdir}" "http://gameservermanagers.com/files/ut99/ut-server-436.tar.gz" "10cd7353aa9d758a075c600a6dd193fd"
+
+
+
+
 if [ ! -f UTPGPatch451.tar.bz2 ]; then
 	wget http://gameservermanagers.com/files/ut99/UTPGPatch451.tar.bz2
 else
