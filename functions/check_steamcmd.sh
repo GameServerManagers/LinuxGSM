ea45e1adc0757211bd3f038974669f7f45905f6b
Daniel Gibbs <me@danielgibbs.co.uk>
2016-02-16 22:24:49 +0000

    turned in to functions

--- functions/check_steamcmd.sh
+++ functions/check_steamcmd.sh
@@ -4,60 +4,69 @@
 # Website: http://gameservermanagers.com
 lgsm_version="281215"
 
-# Description: Downloads SteamCMD on install and checks if missing before running functions that require SteamCMD
+# Description: Checks SteamCMD is installed and correct.
 
+
+fn_install_steamcmd(){
+echo "Installing steamCMD..."
+if [ ! -d "${steamcmddir}" ]; then
+	mkdir -v "${steamcmddir}"
+fi
+fn_fetch_file "http://media.steampowered.com/client/steamcmd_linux.tar.gz" "${steamcmddir}" "steamcmd_linux.tar.gz"
+tar --verbose -zxf "${steamcmddir}/steamcmd_linux.tar.gz" -C "${steamcmddir}"
+rm -v "${steamcmddir}/steamcmd_linux.tar.gz"
+chmod +x "${steamcmddir}/steamcmd.sh"	
+}
+
+
+fn_check_steamcmd_user(){
+# Checks steamuser is setup. 
+if [ "${steamuser}" == "username" ]; then
+	fn_printfailnl "Steam login not set. Update steamuser."	
+	echo "	* Change steamuser=\"username\" to a valid steam login."
+	if [ -d "${scriptlogdir}" ]; then
+		fn_scriptlog "edit ${selfname}. change steamuser=\"username\" to a valid steam login."
+		exit 1
+	fi
+fi
+# Anonymous user is set if steamuser is missing
+if [ -z "${steamuser}" ]; then
+	fn_printwarnnl "Steam login not set. Using anonymous login."
+	if [ -d "${scriptlogdir}" ]; then
+		fn_scriptlog "Steam login not set. Using anonymous login."
+	fi
+	steamuser="anonymous"
+	steampass=""
+	sleep 2
+fi	
+}
+
+fn_check_steamcmd_sh(){
+# Checks if SteamCMD exists when starting or updating a server.
+# Re-installs if missing.
+steamcmddir="${rootdir}/steamcmd"
+if [ ! -f "${steamcmddir}/steamcmd.sh" ]; then
+	fn_printwarnnl "SteamCMD is missing"
+	fn_scriptlog "SteamCMD is missing"
+	sleep 1
+	fn_install_steamcmd
+fi
+}
+
+fn_check_steamcmd_guard(){
+if [ "${function_selfname}" == "command_update.sh" ]||[ "${function_selfname}" == "command_validate.sh" ]; then
+	# Checks that steamcmd is working correctly and will prompt Steam Guard if required.
+	"${steamcmddir}"/steamcmd.sh +login "${steamuser}" "${steampass}" +quit
+	if [ $? -ne 0 ]; then
+		fn_printfailurenl "Error running SteamCMD"	
+	fi		
+fi		
+}
 
 if [ "${gamename}" == "Unreal Tournament 99" ]||[ "${gamename}" == "Unreal Tournament 2004" ]||[ "${gamename}" == "Mumble" ]||[ "${gamename}" == "Teamspeak 3" ]; then
 	: # These servers do not require SteamCMD. Check is skipped.
 else
-	# Checks steamuser is setup. 
-	if [ "${steamuser}" == "username" ]; then
-	fn_printfailnl "Steam login not set. Update steamuser."	
-	echo "	* Change steamuser=\"username\" to a valid steam login."
-	if [ -d ${scriptlogdir} ]; then
-		fn_scriptlog "edit ${selfname}. change steamuser=\"username\" to a valid steam login."
-		exit 1
-	fi
-	fi
-	if [ -z "${steamuser}" ]; then
-		fn_printwarnnl "Steam login not set. Using anonymous login."
-		if [ -d "${scriptlogdir}" ]; then
-			fn_scriptlog "Steam login not set. Using anonymous login."
-		fi
-		steamuser="anonymous"
-		steampass=""
-		sleep 2
-	fi	
-	# Checks if SteamCMD exists when starting or updating a server.
-	# Re-installs if missing.
-	steamcmddir="${rootdir}/steamcmd"
-	if [ ! -f "${steamcmddir}/steamcmd.sh" ]; then
-		fn_printwarnnl "SteamCMD is missing"
-		fn_scriptlog "SteamCMD is missing"
-		sleep 1
-		if [ ! -d "${steamcmddir}" ]; then
-			mkdir -v "${steamcmddir}"
-		fi
-		curl=$(curl --fail -o "${steamcmddir}/steamcmd_linux.tar.gz" "http://media.steampowered.com/client/steamcmd_linux.tar.gz" 2>&1)
-		exitcode=$?
-		echo -e "downloading steamcmd_linux.tar.gz...\c"
-		if [ $exitcode -eq 0 ]; then
-			fn_printokeol
-		else
-			fn_printfaileol
-			echo "${curl}"
-			echo -e "${githuburl}\n"
-			exit $exitcode
-		fi
-		tar --verbose -zxf "${steamcmddir}/steamcmd_linux.tar.gz" -C "${steamcmddir}"
-		rm -v "${steamcmddir}/steamcmd_linux.tar.gz"
-		chmod +x "${steamcmddir}/steamcmd.sh"
-	fi		
-	if [ "${function_selfname}" == "command_update.sh" ]||[ "${function_selfname}" == "command_validate.sh" ]; then
-		# Checks that steamcmd is working correctly and will prompt Steam Guard if required.
-		"${steamcmddir}"/steamcmd.sh +login "${steamuser}" "${steampass}" +quit
-		if [ $? -ne 0 ]; then
-			fn_printfailurenl "Error running SteamCMD"	
-		fi		
-	fi	
+	fn_check_steamcmd_user
+	fn_check_steamcmd_sh
+	fn_check_steamcmd_guard
 fi
