6648ac9170e44eea249a79c5919cc51bb9273ad5
Daniel Gibbs <me@danielgibbs.co.uk>
2016-02-19 23:24:47 +0000

    Altered header to link contributors page on Github

--- functions/install_header.sh
+++ functions/install_header.sh
@@ -2,19 +2,13 @@
 # LGSM install_header.sh function
 # Author: Daniel Gibbs
 # Website: http://gameservermanagers.com
-lgsm_version="271215"
+lgsm_version="190216"
 
 clear
 echo "================================="
 echo "${gamename}"
 echo "Linux Game Server Manager"
 echo "by Daniel Gibbs"
-if [ "${gamename}" == "ARMA 3" ]; then
-	echo "contributions by Scarsz"
-elif [ "${gamename}" == "Left 4 Dead" ]; then
-	echo "contributions by Summit Singh Thakur"
-elif [ "${gamename}" == "Teeworlds" ]; then
-	echo "contributions by Bryce Van Dyk (SingingTree)"	
-fi
 echo "http://gameservermanagers.com"
+echo "Contributors: http://gameservermanagers.com/contib"
 echo "================================="
