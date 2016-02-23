5b5d01394fb9bd11971b1a8b4ff4720e5946de3a
lrob <robin.labadie@gmail.com>
2016-02-23 01:00:45 +0100

    Double quotes

--- functions/logs.sh
+++ functions/logs.sh
@@ -17,8 +17,8 @@ if [ -n "${consolelog}" ]; then
 fi
 
 # For games not displaying a console, and having logs into their game folder
-if [ -n ${gamelogfile} ]; then
-	if [ -n $(find ${systemdir} -name gamelog*.log ) ]; then
+if [ -n "${gamelogfile}" ]; then
+	if [ -n "$(find "${systemdir}" -name gamelog*.log)" ]; then
 		fn_printinfo "Moving game logs to ${gamelogdir}"
 		fn_scriptlog "Moving game logs to ${gamelogdir}"
 		echo -en "\n"
