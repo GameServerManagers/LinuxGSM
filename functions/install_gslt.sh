364856e81916f711b79170a177161de78d1340d0
Daniel Gibbs <me@danielgibbs.co.uk>
2016-02-27 19:50:03 +0000

    Optimised function

--- functions/install_gslt.sh
+++ functions/install_gslt.sh
@@ -2,41 +2,34 @@
 # LGSM install_gslt.sh function
 # Author: Daniel Gibbs
 # Website: http://gameservermanagers.com
-lgsm_version="271215"
+lgsm_version="270216"
 
 # Description: Configures GSLT.
 
-if [ -z "${autoinstall}" ]; then
-	echo ""
-	echo "Game Server Login Token"
-	echo "============================"
-	sleep 1
-	if [ "${gamename}" == "Counter Strike: Global Offensive" ]; then
-		echo "GSLT is required to run a public ${gamename} server"
-	else
-		echo "GSLT is an optional feature for ${gamename} server"
-	fi
+echo ""
+echo "Game Server Login Token"
+echo "============================"
+sleep 1
+if [ "${gamename}" == "Counter Strike: Global Offensive" ]; then
+	echo "GSLT is required to run a public ${gamename} server"
+	fn_scriptlog "GSLT is required to run a public ${gamename} server"
+else
+	echo "GSLT is an optional feature for ${gamename} server"
+	fn_scriptlog "GSLT is an optional feature for ${gamename} server"
+fi
 
-	echo "Get more info and a token here:"
-	echo "http://gameservermanagers.com/gslt"
-	echo ""
+echo "Get more info and a token here:"
+echo "http://gameservermanagers.com/gslt"
+fn_scriptlog "Get more info and a token here:"
+fn_scriptlog "http://gameservermanagers.com/gslt"
+echo ""
+if [ -z "${autoinstall}" ]; then
 	echo "Enter token below (Can be blank)."
 	echo -n "GSLT TOKEN: "
 	read token
 	sed -i -e "s/gslt=\"\"/gslt=\"${token}\"/g" "${rootdir}/${selfname}"
-	sleep 1
-	echo "The GSLT can be changed by editing ${selfname}."
-	echo ""
-else
-	if [ "${gamename}" == "Counter Strike: Global Offensive" ]; then
-		fn_printinfomationnl "GSLT is required to run a public ${gamename} server"
-	else
-		fn_printinfomationnl "GSLT is an optional feature for ${gamename} server"
-	fi
-	echo "Get more info and a token here:"
-	echo "http://gameservermanagers.com/gslt"
-	echo ""	
-	sleep 1
-	echo "The GSLT can be changed by editing ${selfname}."	
-	sleep 1
 fi
+sleep 1
+echo "The GSLT can be changed by editing ${selfname}."
+fn_scriptlog "The GSLT can be changed by editing ${selfname}."
+echo ""
