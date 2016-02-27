ddb475fa2e75ebe4c1c61094e5066ecbebdbf417
Daniel Gibbs <me@danielgibbs.co.uk>
2016-02-27 15:49:15 +0000

    Added auto-install mode

--- functions/install_ut2k4_key.sh
+++ functions/install_ut2k4_key.sh
@@ -4,6 +4,7 @@
 # Website: http://gameservermanagers.com
 lgsm_version="271215"
 
+
 echo ""
 echo "Enter ${gamename} CD Key"
 echo "================================="
@@ -12,8 +13,14 @@ echo "To get your server listed on the Master Server list"
 echo "you must get a free CD key. Get a key here:"
 echo "http://www.unrealtournament.com/ut2004server/cdkey.php"
 echo ""
-echo "Once you have the key enter it below"
-echo -n "KEY: "
-read CODE
-echo ""\""CDKey"\""="\""${CODE}"\""" > "${systemdir}/cdkey"
+if [ -z "${autoinstall}" ]; then
+	echo "Once you have the key enter it below"
+	echo -n "KEY: "
+	read CODE
+	echo ""\""CDKey"\""="\""${CODE}"\""" > "${systemdir}/cdkey"
+
+else
+	echo "You can add your key using the following command"
+	echo "./${selfname} server-cd-key"
+fi	
 echo ""
