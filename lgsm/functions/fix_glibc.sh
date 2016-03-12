97ee2d8d7ad7f7f42779742048cb432aab801ce7
Daniel Gibbs <me@danielgibbs.co.uk>
2016-03-12 23:47:41 +0000

    Working on new glibc fixes

---
+++ lgsm/functions/fix_glibc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# LGSM fix_glibc.sh function
+# Author: Daniel Gibbs
+# Website: http://gameservermanagers.com
+lgsm_version="020116"
+
+info_glibc.sh
+
+if [ $(printf '%s\n$glibc_required\n' $glibc_version | sort -V | head -n 1) != $glibc_required ]; then
+	echo "Version $(ldd --version | sed -n '1s/.* //p') is lower than $glibc_required"
+	if [ ${glibcfix} == "yes" ]; then 
+		export LD_LIBRARY_PATH=:${filesdir}:${filesdir}/bin:{$LD_LIBRARY_PATH}
+	else
+		echo "no glibc fix available you need to upgrade bro!!"
+	fi	
+else
+	echo "GLIBC is OK no fix required"
+fi
