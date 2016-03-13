703d3e59122d1e16216480d05492102fdb7cfbd0
Daniel Gibbs <me@danielgibbs.co.uk>
2016-03-13 01:01:35 +0000

    added download for dabserver

--- lgsm/functions/fix_glibc.sh
+++ lgsm/functions/fix_glibc.sh
@@ -6,6 +6,48 @@ lgsm_version="020116"
 
 info_glibc.sh
 
+# Blade Symphony
+if [ "${gamename}" == "Blade Symphony" ]; then
+	:
+# Dont Starve Together
+elif [ "${gamename}" == "Don't Starve Together" ]; then
+	:
+# Double Action: Boogaloo
+elif [ "${gamename}" == "Double Action: Boogaloo" ]; then
+	fn_fetch_file_github "lgsm/lib/ubuntu12.04/i386" "libm.so.6" "${lgsmdir}/lib" "noexecutecmd" "norun" "noforce" "nomd5"
+
+# Fistful of Frags
+elif [ "${gamename}" == "Fistful of Frags" ]; then
+	:
+# Garry's Mod
+elif [ "${gamename}" == "Garry's Mod" ]; then
+	:
+# Insurgency
+elif [ "${gamename}" == "Insurgency" ]; then
+	:
+elif [ "${gamename}" == "Left 4 Dead" ]; then
+	:
+# Natural Selection 2
+elif [ "${gamename}" == "Natural Selection 2" ]; then
+	:
+# NS2: Combat
+elif [ "${gamename}" == "NS2: Combat" ]; then
+	:
+# No More Room in Hell
+elif [ "${gamename}" == "No More Room in Hell" ]; then
+	:
+# ARMA 3
+elif [ "${gamename}" == "ARMA 3" ]; then
+	:
+# Just Cause 2
+elif [ "${gamename}" == "Just Cause 2" ]; then
+	:
+# Serious Sam 3: BFE
+elif [ "${gamename}" == "Serious Sam 3: BFE" ]; then
+	:
+fi
+
+
 if [ "$(printf '%s\n$glibc_required\n' $glibc_version | sort -V | head -n 1)" != "${glibc_required}" ]; then
 	echo "Version $(ldd --version | sed -n '1s/.* //p') is lower than $glibc_required"
 	if [ "${glibcfix}" == "yes" ]; then 
