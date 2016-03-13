fb38ef5ad0e6dc4d3ecd925eb75e6ba3611926c3
Daniel Gibbs <me@danielgibbs.co.uk>
2016-03-13 00:19:04 +0000

    fixed line endings

--- lgsm/functions/fix_glibc.sh
+++ lgsm/functions/fix_glibc.sh
@@ -6,9 +6,10 @@ lgsm_version="020116"
 
 info_glibc.sh
 
-if [ $(printf '%s\n$glibc_required\n' $glibc_version | sort -V | head -n 1) != $glibc_required ]; then
+if [ $(printf '%s\n$glibc_required\n' $glibc_version | sort -V | head -n 1) != ${glibc_required} ]; then
 	echo "Version $(ldd --version | sed -n '1s/.* //p') is lower than $glibc_required"
-	if [ ${glibcfix} == "yes" ]; then 
+	if [ "${glibcfix}" == "yes" ]; then 
+		echo "applied glibc fix"
 		export LD_LIBRARY_PATH=:${lgsmdir}/lib/ubuntu12.04/i386
 	else
 		echo "no glibc fix available you need to upgrade bro!!"
