3a2d4e14533541ef9f94e1129fd565eeaeaf5430
Daniel Gibbs <me@danielgibbs.co.uk>
2016-03-18 19:46:07 +0000

    minor changes

--- lgsm/functions/fix_glibc.sh
+++ lgsm/functions/fix_glibc.sh
@@ -25,7 +25,6 @@ do
 	fi	
 done
 
-
 glibc_version="$(ldd --version | sed -n '1s/.* //p')"
 if [ "$(printf '%s\n$glibc_required\n' $glibc_version | sort -V | head -n 1)" != "${glibc_required}" ]; then
 	if [ "${glibcfix}" == "yes" ]; then 
@@ -35,10 +34,13 @@ if [ "$(printf '%s\n$glibc_required\n' $glibc_version | sort -V | head -n 1)" !=
 		export LD_LIBRARY_PATH=:"${libdir}"
 	else
 		fn_print_warn_nl "Glibc fix: No Glibc fix available!"
+		echo -en "\n"
 		echo "	* glibc required: $glibc_required"
 		echo "	* glibc installed: $glibc_version"
-		fn_print_infomation "The game server is unlikly to work. A distro upgrade is required"
-	fi	
+		echo -en "\n"
+		fn_print_infomation "The game server will probably not work. A distro upgrade is required!"
+	fi
+	echo -en "\n"
 else
 	echo "GLIBC is OK no fix required"
 fi
