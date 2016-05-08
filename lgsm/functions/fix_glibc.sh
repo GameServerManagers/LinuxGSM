0248451a891f9e0fbc0a4e5a6a92721ada67c11f
Daniel Gibbs <me@danielgibbs.co.uk>
2016-05-08 20:27:09 +0100

    removed duplicate code

--- lgsm/functions/fix_glibc.sh
+++ lgsm/functions/fix_glibc.sh
@@ -6,9 +6,6 @@ lgsm_version="220416"
 
 # Description: Downloads required glibc files and applys teh glibc fix if required
 
-info_glibc.sh
-info_distro.sh
-
 local libstdc_servers_array=( "ARMA 3" "Blade Symphony" "Garry's Mod" "Just Cause 2" )
 for libstdc_server in "${libstdc_servers_array[@]}"
 do
@@ -25,19 +22,4 @@ do
 	fi	
 done
 
-if [ "$(printf '%s\n'${glibcrequired}'\n' ${glibcversion} | sort -V | head -n 1)" != "${glibcrequired}" ]; then
-	if [ "${glibcfix}" == "yes" ]; then 
-		fn_print_info_nl "Glibc fix: Using Glibc fix"
-		echo "	* glibc required: ${glibcrequired}"
-		echo "	* glibc installed: ${glibcversion}"
-		export LD_LIBRARY_PATH=:"${libdir}"
-	else
-		fn_print_warn_nl "Glibc fix: No Glibc fix available!"
-		echo -en "\n"
-		echo "	* glibc required: ${glibcrequired}"
-		echo "	* glibc installed: ${glibcversion}"
-		echo -en "\n"
-		fn_print_infomation "The game server will probably not work. A distro upgrade is required!"
-	fi
-	echo -en "\n"
-fi
+check_glibc.sh
