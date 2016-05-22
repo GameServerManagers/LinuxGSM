b7701042ea508f354803685339031106b3dcc118
UltimateByte <robin.labadie@gmail.com>
2016-05-22 21:21:17 +0200

    move stupid games' logs only on game start

--- lgsm/functions/logs.sh
+++ lgsm/functions/logs.sh
@@ -17,7 +17,7 @@ if [ -n "${consolelog}" ]; then
 fi
 
 # For games not displaying a console, and having logs into their game folder
-if [ -n "${gamelogfile}" ]; then
+if [ "${function_selfname}" == "command_start.sh" ] && [ -n "${gamelogfile}" ]; then
 	if [ -n "$(find "${systemdir}" -name "gamelog*.log")" ]; then
 		fn_print_info "Moving game logs to ${gamelogdir}"
 		fn_scriptlog "Moving game logs to ${gamelogdir}"
