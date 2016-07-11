7a28922101914a7d1c88a4a455585f828cd5be1d
Daniel Gibbs <me@danielgibbs.co.uk>
2016-07-11 22:59:21 +0100

    command_update.sh

--- lgsm/functions/command_start.sh
+++ lgsm/functions/command_start.sh
@@ -172,7 +172,7 @@ logs.sh
 # Will check for updates is updateonstart is yes
 if [ "${status}" == "0" ]; then
 	if [ "${updateonstart}" == "yes" ]||[ "${updateonstart}" == "1" ]||[ "${updateonstart}" == "on" ]; then
-		update_check.sh
+		command_update.sh
 	fi
 fi
 
