43fc590f1e1fc07686b827338d4e9490ed002df6
UltimateByte <robin.labadie@gmail.com>
2016-07-27 06:29:07 +0200

    contrib

--- lgsm/functions/info_config.sh
+++ lgsm/functions/info_config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # LGSM info_config.sh function
 # Author: Daniel Gibbs
+# Contributor: UltimateByte
 # Website: https://gameservermanagers.com
 # Description: Gets specific details from config files.
 
