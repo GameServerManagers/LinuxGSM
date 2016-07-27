1ba937212210dbc0c7126d0158538a1f63c757d0
Daniel Gibbs <me@danielgibbs.co.uk>
2016-07-27 22:20:09 +0100

    added physicaltotalmb

--- lgsm/functions/info_distro.sh
+++ lgsm/functions/info_distro.sh
@@ -61,12 +61,12 @@ else
 fi
 
 physmemtotal=$(free ${humanreadable} | awk '/Mem:/ {print $2}')
+physmemtotalmb=$(free -m | awk '/Mem:/ {print $2}')
 physmemused=$(free ${humanreadable} | awk '/Mem:/ {print $3}')
 physmemfree=$(free ${humanreadable} | awk '/Mem:/ {print $4}')
 swaptotal=$(free ${humanreadable} | awk '/Swap:/ {print $2}')
 swapused=$(free ${humanreadable} | awk '/Swap:/ {print $3}')
 swapfree=$(free ${humanreadable} | awk '/Swap:/ {print $4}')
-
 
 ### Disk Infomation
 
