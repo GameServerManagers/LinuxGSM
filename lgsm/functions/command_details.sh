8f046e5956e9084f0d67de273f04bc567b3b1862
UltimateByte <robin.labadie@gmail.com>
2016-07-27 04:25:03 +0200

    Mumble details

--- lgsm/functions/command_details.sh
+++ lgsm/functions/command_details.sh
@@ -286,7 +286,7 @@ fn_details_ports(){
 	echo -e "Change ports by editing the parameters in:"
 
 	parmslocation="${red}UNKNOWN${default}"
-	local ports_edit_array=( "avalanche" "dontstarve" "projectzomboid" "idtech3" "realvirtuality" "seriousengine35" "teeworlds" "terraria" "unreal" "unreal2" "Teamspeak 3" "7 Days To Die" )
+	local ports_edit_array=( "avalanche" "dontstarve" "projectzomboid" "idtech3" "realvirtuality" "seriousengine35" "teeworlds" "terraria" "unreal" "unreal2" "Teamspeak 3" "Mumble" "7 Days To Die" )
 	for port_edit in "${ports_edit_array[@]}"
 	do
 		if [ "${engine}" == "${port_edit}" ]||[ "${gamename}" == "${port_edit}" ]; then
@@ -438,6 +438,17 @@ fn_details_teamspeak3(){
 		echo -e "> Voice\tINBOUND\t${port}\tudp"
 		echo -e "> ServerQuery\tINBOUND\t${queryport}\ttcp"
 		echo -e "> File transfer\tINBOUND\t${fileport}\ttcp"
+	} | column -s $'\t' -t
+}
+
+fn_details_mumble(){
+	echo -e "netstat -atunp | grep murmur"
+	echo -e ""
+	port=$(cat "${servercfgfullpath}" | grep 'port=' | awk -F'=' '{ print $2 }'
+	{
+		echo -e "DESCRIPTION\tDIRECTION\tPORT\tPROTOCOL"
+		echo -e "> Voice\tIN&OUT\t${port}\tudp"
+		echo -e "> ServerQuery\tIN&OUT\t${port}\ttcp"
 	} | column -s $'\t' -t
 }
 
@@ -608,6 +619,8 @@ elif [ "${gamename}" == "7 Days To Die" ]; then
 	fn_details_sdtd
 elif [ "${gamename}" == "Teamspeak 3" ]; then
 	fn_details_teamspeak3
+elif [ "${gamename}" == "Mumble" ]; then
+	fn_details_mumble
 elif [ "${gamename}" == "Rust" ]; then
 	fn_details_rust
 else
