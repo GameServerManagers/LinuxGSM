44278ce10b03678a53e23a321c1f839c09b9a58f
Marvin Lehmann <marvinlehmann@live.de>
2016-08-03 21:00:02 +0200

    Added dependencies to enable readline
    Like #212

--- lgsm/functions/check_deps.sh
+++ lgsm/functions/check_deps.sh
@@ -158,7 +158,7 @@ if [ -n "$(command -v dpkg-query)" ]; then
 	elif [ "${gamename}" ==  "7 Days To Die" ]; then
 		array_deps_required+=( telnet expect )
 	# No More Room in Hell
-	elif [ "${gamename}" == "No More Room in Hell" ]; then
+	elif [ "${gamename}" == "No More Room in Hell" ]||[ "${gamename}" == "Counter Strike: Source" ]||[ "${gamename}" == "Garry's Mod" ]; then
 		array_deps_required+=( lib32tinfo5 )
 	# Brainbread 2 and Don't Starve Together
 	elif [ "${gamename}" == "Brainbread 2" ]||[ "${gamename}" == "Don't Starve Together" ]; then
@@ -205,7 +205,7 @@ elif [ -n "$(command -v yum)" ]; then
 	elif [ "${gamename}" ==  "7 Days To Die" ]; then
 		array_deps_required+=( telnet expect )
 	# No More Room in Hell
-	elif [ "${gamename}" == "No More Room in Hell" ]; then
+	elif [ "${gamename}" == "No More Room in Hell" ]||[ "${gamename}" == "Counter Strike: Source" ]||[ "${gamename}" == "Garry's Mod" ]; then
 		array_deps_required+=( ncurses-libs.i686 )
 	# Brainbread 2 and Don't Starve Together
 	elif [ "${gamename}" == "Brainbread 2" ]||[ "${gamename}" == "Don't Starve Together" ]; then
