f6a440233ab0bfafaf67ffb7e71e5eba38f8bc6a
Daniel Gibbs <me@danielgibbs.co.uk>
2016-08-09 00:22:11 +0100

    added minecraft

--- lgsm/functions/command_update.sh
+++ lgsm/functions/command_update.sh
@@ -15,6 +15,8 @@ check.sh
 
 if [ "${gamename}" == "TeamSpeak 3" ]; then
 	update_ts3.sh
+elif [ "${engine}" == "minecraft" ]; then
+	update_mumble.sh
 elif [ "${gamename}" == "Mumble" ]; then
 	update_mumble.sh
 else
