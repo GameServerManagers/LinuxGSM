70e1e6fbf9f22bb0432a898f03964a2305458fbc
Daniel Gibbs <me@danielgibbs.co.uk>
2016-08-10 00:40:24 +0100

    added Codename CURE glibc fix #735

--- lgsm/functions/fix_glibc.sh
+++ lgsm/functions/fix_glibc.sh
@@ -16,7 +16,7 @@ do
 	fi
 done
 
-local libm_servers_array=( "Double Action: Boogaloo" "Fistful of Frags" "Garry's Mod" "Insurgency" "Natural Selection 2" "NS2: Combat" "No More Room in Hell" )
+local libm_servers_array=( "Codename CURE" "Double Action: Boogaloo" "Fistful of Frags" "Garry's Mod" "Insurgency" "Natural Selection 2" "NS2: Combat" "No More Room in Hell" )
 for libm_server in "${libm_servers_array[@]}"
 do
 	if [ "${gamename}" == "${libm_server}" ]; then
