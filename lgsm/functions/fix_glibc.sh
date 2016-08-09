8a6d7b92b739ff5826a5320d5f3d29a496c3b352
Daniel Gibbs <me@danielgibbs.co.uk>
2016-08-09 16:14:22 +0100

    Improved support for glibcfixes with Gmod

--- lgsm/functions/fix_glibc.sh
+++ lgsm/functions/fix_glibc.sh
@@ -16,11 +16,27 @@ do
 	fi
 done
 
-local libm_servers_array=( "Double Action: Boogaloo" "Fistful of Frags" "Insurgency" "Natural Selection 2" "NS2: Combat" "No More Room in Hell" )
+local libm_servers_array=( "Double Action: Boogaloo" "Fistful of Frags" "Garry's Mod" "Insurgency" "Natural Selection 2" "NS2: Combat" "No More Room in Hell" )
 for libm_server in "${libm_servers_array[@]}"
 do
 	if [ "${gamename}" == "${libm_server}" ]; then
 		fn_fetch_file_github "lgsm/lib/ubuntu12.04/i386" "libm.so.6" "${lgsmdir}/lib" "noexecutecmd" "norun" "noforce" "nomd5"
+	fi
+done
+
+local libc_servers_array=( "Garry's Mod" )
+for libc_server in "${libc_servers_array[@]}"
+do
+	if [ "${gamename}" == "${libc_server}" ]; then
+		fn_fetch_file_github "lgsm/lib/ubuntu12.04/i386" "libc.so.6" "${lgsmdir}/lib" "noexecutecmd" "norun" "noforce" "nomd5"
+	fi
+done
+
+local libpthread_servers_array=( "Garry's Mod" )
+for libpthread_server in "${libpthread_servers_array[@]}"
+do
+	if [ "${gamename}" == "${libpthread_server}" ]; then
+		fn_fetch_file_github "lgsm/lib/ubuntu12.04/i386" "libpthread.so.0" "${lgsmdir}/lib" "noexecutecmd" "norun" "noforce" "nomd5"
 	fi
 done
 
