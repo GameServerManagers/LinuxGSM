f93d373230e14af4bc006c1a0c9d0e0551954b09
Daniel Gibbs <me@danielgibbs.co.uk>
2016-08-12 15:05:16 +0100

    Fixed system_requirements

--- lgsm/functions/check_system_requirements.sh
+++ lgsm/functions/check_system_requirements.sh
@@ -22,12 +22,14 @@ if [ "${gamename}" == "ARMA 3" ]; then
 fi
 
 # If the game or engine has a minimum RAM Requirement, compare it to system's available RAM.
-if [ -n "${ramrequirement}" ]; then
+if [ -n "${ramrequirementmb}" ]; then
 	if [ "${physmemtotalmb}" -lt "${ramrequirementmb}" ]; then
+		fn_print_dots "Check RAM"
+		sleep 0.5
 		# Warn the user
-		fn_print_warn "Insufficient memory: ${ramrequirementgb}G required, ${physmemtotal} available"
+		fn_print_warn_nl "Check RAM: ${ramrequirementgb}G required, ${physmemtotal} available"
 		sleep 1
-		fn_print_warning "You may experiance poor performance from your server"
+		echo  "	* ${gamename} server may fail to run or experience poor performance."
 		sleep 1
 	fi
 fi
