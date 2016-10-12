c547ecfe1e57a3e20a870763c1182c98196ecb4e
Alexander Hurd <hurdad@gmail.com>
2016-10-12 02:46:37 -0400

    name fix

--- lgsm/functions/install_server_files.sh
+++ lgsm/functions/install_server_files.sh
@@ -23,7 +23,7 @@ fn_install_server_files(){
 		fileurl="http://files.gameservermanagers.com/UnrealTournament/UnrealTournament-Server-XAN-3045522-Linux.zip"; filedir="${tmpdir}"; filename="UnrealTournament-Server-XAN-3045522-Linux.zip";  executecmd="noexecute" run="norun"; force="noforce"; md5="553fed5645a9fc623e92563049bf79f6"
 	elif [ "${gamename}" == "GoldenEye: Source" ]; then
 		fileurl="http://files.gameservermanagers.com/GoldenEyeSource/GoldenEye_Source_v5.0.1_full_server_linux.tar.bz2"; filedir="${tmpdir}"; filename="GoldenEye_Source_v5.0.1_server_full_Linux.tar.bz2";  executecmd="noexecute" run="norun"; force="noforce"; md5="ea227a150300abe346e757380325f84c"
-	elif [ "${gamename}" == "San Andresas Multiplayer" ]; then
+	elif [ "${gamename}" == "San Andreas Multiplayer" ]; then
 		fileurl="http://files.sa-mp.com/samp037svr_R2-1.tar.gz"; filedir="${tmpdir}"; filename="samp037svr_R2-1.tar.gz";  executecmd="noexecute" run="norun"; force="noforce"; md5="93705e165550c97484678236749198a4"
 	fi
 	fn_fetch_file "${fileurl}" "${filedir}" "${filename}" "${executecmd}" "${run}" "${force}" "${md5}"
