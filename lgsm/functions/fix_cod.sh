176c2a86a2cbf68bd46fc1738b25f136e4ac5ba8
Alexander Hurd <hurdad@gmail.com>
2016-10-13 02:27:55 -0400

    adding glibc fix for cod

---
+++ lgsm/functions/fix_cod.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# LGSM fix_ins.sh function
+# Author: Alexander Hurd
+# Website: https://gameservermanagers.com
+# Description: Glibc Fix for Call of Duty
+
+local commandname="FIX"
+local commandaction="Fix"
+
+#Glibc Fix for Call of Duty
+export LD_LIBRARY_PATH=${libdir}:${LD_LIBRARY_PATH}
+
