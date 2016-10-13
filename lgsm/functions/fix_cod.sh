693e006a283c3e8f15745e8646d5d718b8f7f85f
Alexander Hurd <hurdad@gmail.com>
2016-10-13 03:50:29 -0400

    force glibc fix

--- lgsm/functions/fix_cod.sh
+++ lgsm/functions/fix_cod.sh
@@ -2,12 +2,11 @@
 # LGSM fix_cod.sh function
 # Author: Alexander Hurd
 # Website: https://gameservermanagers.com
-# Description: Glibc Fix for Call of Duty
+# Description: Fixes for Call of Duty
 
 local commandname="FIX"
 local commandaction="Fix"
 
-#Glibc Fix for Call of Duty
-#export LD_LIBRARY_PATH=${libdir}:${LD_LIBRARY_PATH}
-#echo $LD_LIBRARY_PATH
+# Force glibc fix
+fix_glibc.sh
 
