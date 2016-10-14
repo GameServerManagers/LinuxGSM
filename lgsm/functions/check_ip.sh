947e6114d7a17d0c0ae7a9fa6a6f3226b3987610
Daniel Gibbs <me@danielgibbs.co.uk>
2016-10-14 21:06:38 +0100

    Revert "ET now bypasses IP check"
    This reverts commit 44f3a7018c1889cee6b36e95e0aa795c55756187.

--- lgsm/functions/check_ip.sh
+++ lgsm/functions/check_ip.sh
@@ -8,7 +8,7 @@
 local commandname="CHECK"
 local function_selfname="$(basename $(readlink -f "${BASH_SOURCE[0]}"))"
 
-if [ "${gamename}" != "TeamSpeak 3" ] && [ "${gamename}" != "Mumble" ] && [ "${gamename}" != "Wolfenstein: Enemy Territory" ]; then
+if [ "${gamename}" != "TeamSpeak 3" ] && [ "${gamename}" != "Mumble" ]; then
 	if [ ! -f "/bin/ip" ]; then
 		ipcommand="/sbin/ip"
 	else
