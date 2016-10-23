345f3456accf2351d8800da893fe251eaa890d27
Daniel Gibbs <me@danielgibbs.co.uk>
2016-10-23 21:18:38 +0100

    removed #

--- lgsm/functions/command_dev_detect_glibc.sh
+++ lgsm/functions/command_dev_detect_glibc.sh
@@ -37,4 +37,4 @@ while IFS= read -r -d $'\0' line; do
 done
 echo ""
 cat "${tmpdir}/detect_glibc.tmp"|sort|uniq|sort -r --version-sort
-#rm "${tmpdir}/detect_glibc.tmp"
+rm "${tmpdir}/detect_glibc.tmp"
