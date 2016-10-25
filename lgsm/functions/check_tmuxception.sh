6f601b46adf03f6c0ecf287a7b7669a29a0204c2
CasperAlant <caalant@gmail.com>
2016-10-25 22:16:58 +0200

    Fixes #1154
    Users are prevented from starting an LGSM server within a tmux session, unless the session was started by another user.

--- lgsm/functions/check_tmuxception.sh
+++ lgsm/functions/check_tmuxception.sh
@@ -17,16 +17,5 @@ fn_check_is_in_tmux(){
 		core_exit.sh
 	fi
 }
-fn_check_is_in_screen(){
-	if [ "$TERM" == "screen" ];then
-		fn_print_fail_nl "tmuxception error: Sorry Cobb you cannot start a tmux session inside of a screen session."
-		fn_script_log_fatal "tmuxception error: Attempted to start a tmux session inside of a screen session."
-		fn_print_information_nl "LGSM creates a tmux session when starting the server."
-		echo "It is not possible to run a tmux session inside screen session"
-		echo "https://github.com/GameServerManagers/LinuxGSM/wiki/Tmux#tmuxception"
-		core_exit.sh
-	fi
-}
 
 fn_check_is_in_tmux
-fn_check_is_in_screen
