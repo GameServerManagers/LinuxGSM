cab1cef5e5541c0e3de84281c073c2cef0fcbdf1
Daniel Gibbs <me@danielgibbs.co.uk>
2016-11-02 17:48:11 +0000

    Fix is not required for CoD
    Was missing a dependency rather than requiring a glibc fix

--- lgsm/functions/fix_glibc.sh
+++ lgsm/functions/fix_glibc.sh
@@ -40,13 +40,4 @@ do
 	fi
 done
 
-if [ "${gamename}" == "Call of Duty" ]; then
-	fn_fetch_file_github "lgsm/lib/gcc3" "libgcc_s.so.1" "${lgsmdir}/lib" "noexecutecmd" "norun" "noforce" "nomd5"
-	fn_fetch_file_github "lgsm/lib/gcc3" "libstdc++.so.5.0.3" "${lgsmdir}/lib" "noexecutecmd" "norun" "noforce" "nomd5"
-	if [ ! -f "${lgsmdir}/lib/libstdc++.so.5" ]; then	
-		ln -s "${lgsmdir}/lib/libstdc++.so.5.0.3" "${lgsmdir}/lib/libstdc++.so.5"
-	fi
-fi
-
-
 export LD_LIBRARY_PATH=:"${libdir}"
