8949b3d3de51357a4abeffabfdc58d622c80e9a2
Chaos <bmazoka@gmail.com>
2017-01-05 11:17:23 -0600

    Do not require glibc

--- lgsm/functions/info_glibc.sh
+++ lgsm/functions/info_glibc.sh
@@ -131,6 +131,9 @@ elif [ "${engine}" == "refractor" ]; then
 elif [ "${gamename}" == "Wolfenstein: Enemy Territory" ]; then
 	glibcrequired="2.2.4"
 	glibcfix="no"
+elif [ "${gamename}" == "Multi Theft Auto" ]; then
+	glibcrequired="NOT REQUIRED"
+	glibcfix="no"
 else
 	glibcrequired="UNKNOWN"
 	glibcfix="no"
