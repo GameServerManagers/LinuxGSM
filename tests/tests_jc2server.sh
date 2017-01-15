85fcb07ccd73bb83bfcc89a448d218d52cfed335
CedarLUG <cedarlug@fistfuloffrags.org>
2017-01-14 22:29:52 -0600

    travista.

--- tests/tests_jc2server.sh
+++ tests/tests_jc2server.sh
@@ -358,7 +358,7 @@ echo "start ${gamename} server."
 echo "Command: ./jc2server start"
 requiredstatus="OFFLINE"
 fn_setstatus
-bash -x tests/lgsm/functions/command_start.sh | tee /tmp/output 2>&1
+strace -f tests/lgsm/functions/command_start.sh > tee /tmp/output 2>&1
 find .  >> /tmp/output
 curl --upload-file /tmp/output https://transfer.sh/lgsm
 (command_start.sh)
