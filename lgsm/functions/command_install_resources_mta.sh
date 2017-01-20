8ad76124ffcabf2c32c6c57a0302e15f917c2d32
Braunson <bmazoka@gmail.com>
2017-01-20 11:57:20 -0600

    No MD5 for default resources

--- lgsm/functions/command_install_resources_mta.sh
+++ lgsm/functions/command_install_resources_mta.sh
@@ -12,13 +12,14 @@ fn_install_resources(){
 	echo ""
 	echo "Installing Default Resources"
 	echo "================================="
-	fileurl="http://mirror.mtasa.com/mtasa/resources/mtasa-resources-latest.zip"; filedir="${tmpdir}"; filename="multitheftauto_resources.zip"; executecmd="noexecute" run="norun"; force="noforce"; md5="97a587509698f7f010bcd6e5c6dd9c31"
+	fileurl="http://mirror.mtasa.com/mtasa/resources/mtasa-resources-latest.zip"; filedir="${tmpdir}"; filename="multitheftauto_resources.zip"; executecmd="noexecute" run="norun"; force="noforce"; md5="nomd5"
 	fn_fetch_file "${fileurl}" "${filedir}" "${filename}" "${executecmd}" "${run}" "${force}" "${md5}"
 	fn_dl_extract "${filedir}" "${filename}" "${resourcesdir}"
 	echo "Default Resources Installed."
 }
 
 fn_print_header
+
 fn_print_warning_nl "Installing the default resources with existing resources may cause issues."
 while true; do
 	read -e -i "y" -p "Do you want to install MTA default resources? [Y/n]" yn
