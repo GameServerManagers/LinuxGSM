fa5bafbed3163d7e80b603d1653ba48d40d22cfb
Braunson <bmazoka@gmail.com>
2017-01-20 11:11:24 -0600

    Removed default resource question when running the install after Daniel said so

--- lgsm/functions/install_mta_resources.sh
+++ lgsm/functions/install_mta_resources.sh
@@ -31,28 +31,7 @@ fn_install_libmysqlclient16(){
 	fi
 }
 
-fn_install_resources(){
-	echo ""
-	echo "Installing Default Resources"
-	echo "================================="
-  fileurl="http://mirror.mtasa.com/mtasa/resources/mtasa-resources-latest.zip"; filedir="${tmpdir}"; filename="multitheftauto_resources.zip"; executecmd="noexecute" run="norun"; force="noforce"; md5="97a587509698f7f010bcd6e5c6dd9c31"
-  fn_fetch_file "${fileurl}" "${filedir}" "${filename}" "${executecmd}" "${run}" "${force}" "${md5}"
-	fn_dl_extract "${filedir}" "${filename}" "${resourcesdir}"
-  echo "Default Resources Installed."
-}
-
 fn_install_libmysqlclient16
 
-if [ -z "${autoinstall}" ]; then
-	echo ""
-	while true; do
-		read -e -i "y" -p "Do you want to have the default resources downloaded? (Server is inoperable without resources!) [y/N]" yn
-		case $yn in
-		[Yy]* ) fn_install_resources && break;;
-		[Nn]* ) break;;
-		* ) echo "Please answer yes or no.";;
-		esac
-	done
-else
-fn_print_warning_nl "./${selfname} auto-install does not download the default resources. If you require them use ./${selfname} install"
-fi
+fn_print_header
+fn_print_information_nl "Server is inoperable by default without resources, you can install default ones by running the command install-default-resources"
