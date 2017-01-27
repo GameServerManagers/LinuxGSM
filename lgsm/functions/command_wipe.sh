63e0c184c320952af0612ada908fc90b96910b72
UltimateByte <robin.labadie@gmail.com>
2017-01-27 06:05:53 +0100

    file init

---
+++ lgsm/functions/command_wipe.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# LGSM command_backup.sh function
+# Author: Daniel Gibbs
+# Contributor: UltimateByte
+# Website: https://gameservermanagers.com
+# Description: Wipes server data for Rust, useful after monthly updates
+
