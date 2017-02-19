ab8ad9084c89c5871646b022d5fcf00a71aa8e7c
UltimateByte <robin.labadie@gmail.com>
2017-02-19 06:41:44 +0100

    Only create dirs if they don't already exist

--- lgsm/functions/install_logs.sh
+++ lgsm/functions/install_logs.sh
@@ -15,12 +15,22 @@ if [ "${checklogs}" != "1" ]; then
 fi
 sleep 1
 # Create dir's for the script and console logs
-mkdir -v "${rootdir}/log"
-mkdir -v "${scriptlogdir}"
-touch "${scriptlog}"
+if [ ! -d "${rootdir}/log" ]; then
+	mkdir -v "${rootdir}/log"
+fi
+if [ ! -d "${scriptlogdir}" ]; then
+	mkdir -v "${scriptlogdir}"
+fi
+if [ ! -f "${scriptlog}" ]; then
+	touch "${scriptlog}"
+fi
 if [ -n "${consolelogdir}" ]; then
-	mkdir -v "${consolelogdir}"
-	touch "${consolelog}"
+	if [ ! -d "${consolelogdir}" ]; then
+		mkdir -v "${consolelogdir}"
+	fi
+	if [ ! -f "${consolelog}" ]; then
+		touch "${consolelog}"
+	fi
 fi
 
 # If a server is source or goldsource, TeamSpeak 3, Starbound, Project Zomhoid create a symbolic link to the game server logs.
@@ -32,7 +42,9 @@ fi
 
 # If a server is unreal2 or unity3d create a dir.
 if [ "${engine}" == "unreal2" ]||[ "${engine}" == "unity3d" ]||[ "${gamename}" == "Teeworlds" ]||[ "${gamename}" == "seriousengine35" ]; then
-	mkdir -pv "${gamelogdir}"
+	if [ ! -d ${gamelogdir}" ]; then
+		mkdir -pv "${gamelogdir}"
+	fi
 fi
 
 # If server uses SteamCMD create a symbolic link to the Steam logs.
