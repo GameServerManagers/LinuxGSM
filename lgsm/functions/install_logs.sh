4155af5d60939f69ee6cfebcbae0dbe55ff22d52
Daniel Gibbs <me@danielgibbs.co.uk>
2017-02-19 12:42:02 +0000

    Reverting commit, no requirement to bypass this.
    Rather than get no output mkdir message states that the directory already exists. This looks better than having nothing at all.

--- lgsm/functions/install_logs.sh
+++ lgsm/functions/install_logs.sh
@@ -15,22 +15,12 @@ if [ "${checklogs}" != "1" ]; then
 fi
 sleep 1
 # Create dir's for the script and console logs
-if [ ! -d "${rootdir}/log" ]; then
-	mkdir -v "${rootdir}/log"
-fi
-if [ ! -d "${scriptlogdir}" ]; then
-	mkdir -v "${scriptlogdir}"
-fi
-if [ ! -f "${scriptlog}" ]; then
-	touch "${scriptlog}"
-fi
+mkdir -v "${rootdir}/log"
+mkdir -v "${scriptlogdir}"
+touch "${scriptlog}"
 if [ -n "${consolelogdir}" ]; then
-	if [ ! -d "${consolelogdir}" ]; then
-		mkdir -v "${consolelogdir}"
-	fi
-	if [ ! -f "${consolelog}" ]; then
-		touch "${consolelog}"
-	fi
+	mkdir -v "${consolelogdir}"
+	touch "${consolelog}"
 fi
 
 # If a server is source or goldsource, TeamSpeak 3, Starbound, Project Zomhoid create a symbolic link to the game server logs.
@@ -42,9 +32,7 @@ fi
 
 # If a server is unreal2 or unity3d create a dir.
 if [ "${engine}" == "unreal2" ]||[ "${engine}" == "unity3d" ]||[ "${gamename}" == "Teeworlds" ]||[ "${gamename}" == "seriousengine35" ]; then
-	if [ ! -d "${gamelogdir}" ]; then
-		mkdir -pv "${gamelogdir}"
-	fi
+	mkdir -pv "${gamelogdir}"
 fi
 
 # If server uses SteamCMD create a symbolic link to the Steam logs.
