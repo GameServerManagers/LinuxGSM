dd55ccf979689812fa6e5d8f17ed7bb94a340597
UltimateByte <robin.labadie@gmail.com>
2017-02-23 03:41:53 +0100

    fn_print_header
    As per #1035

--- lgsm/functions/command_ts3_server_pass.sh
+++ lgsm/functions/command_ts3_server_pass.sh
@@ -6,15 +6,12 @@
 # Description: Changes TS3 serveradmin password.
 
 local commandname="TS3-CHANGE-PASS"
-local commandaction="TS3 Change Password"
+local commandaction="ServerAdmin Password Change"
 local function_selfname="$(basename $(readlink -f "${BASH_SOURCE[0]}"))"
 
 
 fn_serveradmin_password_prompt(){
-	echo ""
-	echo "${gamename} ServerAdmin Password Change"
-	echo "================================="
-	echo ""
+	fn_print_header
 	echo "Press \"CTRL+b d\" to exit console."
 	fn_print_information_nl "You are about to change the ${gamename} ServerAdmin password."
 	fn_print_warning_nl "${gamename} will restart during this process."
