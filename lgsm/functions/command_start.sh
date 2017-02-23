9dae232b4af4e27003f0f99986435a89881cd38a
UltimateByte <robin.labadie@gmail.com>
2017-02-23 09:02:19 +0100

    Try reverting changes to check why travis fails

--- lgsm/functions/command_start.sh
+++ lgsm/functions/command_start.sh
@@ -85,15 +85,15 @@ fn_start_tmux(){
 
 	# tmux pipe-pane not supported in tmux versions < 1.6
 	if [ "$(tmux -V|sed "s/tmux //"|sed -n '1 p'|tr -cd '[:digit:]')" -lt "16" ]; then
-		echo "Console logging disabled: Tmux => 1.6 required
-		https://gameservermanagers.com/tmux-upgrade
-		Currently installed: $(tmux -V)" > "${consolelog}"
+		echo "Console logging disabled: Tmux => 1.6 required" >> "${consolelog}"
+		echo "https://gameservermanagers.com/tmux-upgrade" >> "${consolelog}"
+		echo "Currently installed: $(tmux -V)" >> "${consolelog}"
 
 	# Console logging disabled: Bug in tmux 1.8 breaks logging
 	elif [ "$(tmux -V|sed "s/tmux //"|sed -n '1 p'|tr -cd '[:digit:]')" -eq "18" ]; then
-		echo "Console logging disabled: Bug in tmux 1.8 breaks logging
-		https://gameservermanagers.com/tmux-upgrade
-		Currently installed: $(tmux -V)" > "${consolelog}"
+		echo "Console logging disabled: Bug in tmux 1.8 breaks logging" >> "${consolelog}"
+		echo "https://gameservermanagers.com/tmux-upgrade" >> "${consolelog}"
+		echo "Currently installed: $(tmux -V)" >> "${consolelog}"
 
 	# Console logging enable or not set
 	elif [ "${consolelogging}" == "on" ]||[ -z "${consolelogging}" ]; then
