938d94c8f855c0076d9e8febbd667376048f3092
Daniel Gibbs <me@danielgibbs.co.uk>
2017-05-04 19:24:45 +0100

    setup redirect

---
+++ lgsm/functions/command_update_functions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# LinuxGSM command_update_functions.sh function
+# Author: Daniel Gibbs
+# Website: https://gameservermanagers.com
+# Description: Deletes the functions dir to allow re-downloading of functions from GitHub.
+# Legacy Command
+
+command_update_linuxgsm.sh
