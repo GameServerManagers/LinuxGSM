baf6705ff562b75a0ac5b126cbef1d9abefb4d8d
Daniel Gibbs <me@danielgibbs.co.uk>
2017-05-08 22:11:43 +0100

    reverted back to tmpdir

--- linuxgsm.sh
+++ linuxgsm.sh
@@ -364,8 +364,8 @@ else
 		source "${configdirserver}/${servicename}.cfg"
 	fi
 	# Load the linuxgsm.sh in to tmpdir. If missing download it
-	if [ ! -f "${functionsdir}/linuxgsm.sh" ];then
-		fn_fetch_file_github "" "linuxgsm.sh" "${functionsdir}" "chmodx" "norun" "noforcedl" "nomd5"
+	if [ ! -f "${tmpdir}/linuxgsm.sh" ];then
+		fn_fetch_file_github "" "linuxgsm.sh" "${tmpdir}" "chmodx" "norun" "noforcedl" "nomd5"
 	fi
 	getopt=$1
 	core_getopt.sh
