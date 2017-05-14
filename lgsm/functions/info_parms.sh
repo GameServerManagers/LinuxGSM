fc84e30d95a768ad5b8ae16855710c24c958adfa
Daniel Gibbs <me@danielgibbs.co.uk>
2017-05-14 19:42:14 +0100

    Added more servers

--- lgsm/functions/info_parms.sh
+++ lgsm/functions/info_parms.sh
@@ -67,6 +67,17 @@ fn_info_parms_quake2(){
 	defaultmap=${defaultmap:-"NOT SET"}
 }
 
+fn_info_parms_rust(){
+	servername=${servername:-"NOT SET"}
+	port=${port:-"0"}
+	rconport=${rconport:-"0"}
+	rconpassword=${rconpassword:-"NOT SET"}
+	rconweb=${rconweb:-"NOT SET"}
+	maxplayers=${maxplayers:-"0"}
+	saveinterval=${saveinterval:-"0"}
+	tickrate=${tickrate:-"0"}
+}
+
 fn_info_parms_source(){
 	defaultmap=${defaultmap:-"NOT SET"}
 	maxplayers=${maxplayers:-"0"}
@@ -85,6 +96,11 @@ fn_info_parms_spark(){
 	webadminpass=${webadminpass:-"NOT SET"}
 	webadminport=${webadminport:-"0"}
 	mods=${mods:-"NOT SET"}
+}
+
+fn_info_config_towerunite(){
+	port=${port:-"0"}
+	queryport=${queryport:-"0"}
 }
 
 fn_info_parms_unreal(){
@@ -110,11 +126,19 @@ elif [ "${gamename}" == "QuakeWorld" ]; then
 	fn_info_parms_quakeworld
 elif [ "${gamename}" == "Quake 2" ]||[ "${gamename}" == "Quake 3: Arena" ]; then
 	fn_info_parms_quake2
+# Rust
+elif [ "${gamename}" == "Rust" ]; then
+	fn_info_parms_rust
+# Serious Sam
+elif [ "${engine}" == "seriousengine35" ]; then
+	fn_info_config_seriousengine35
 elif [ "${engine}" == "source" ]||[ "${engine}" == "goldsource" ]; then
 	fn_info_parms_source
 # Spark
 elif [ "${engine}" == "spark" ]; then
 	fn_info_parms_spark
+elif [ "${gamename}" == "Tower Unite" ]; then
+	fn_info_config_towerunite
 # Unreal/Unreal 2 engine
 elif [ "${engine}" == "unreal" ]||[ "${engine}" == "unreal2" ]; then
 	fn_info_parms_unreal
