5672fc16a1c6c1d61036733aafa4a0a8599d7012
Daniel Gibbs <me@danielgibbs.co.uk>
2017-07-16 14:53:34 +0100

    Corrected game config case

--- lgsm/functions/install_config.sh
+++ lgsm/functions/install_config.sh
@@ -452,7 +452,7 @@ elif [ "${gamename}" == "Serious Sam 3: BFE" ]; then
 	fn_set_config_vars
 elif [ "${gamename}" == "Squad" ]; then
 	gamedirname="Squad"
-	array_configs+=( server.cfg )
+	array_configs+=( Server.cfg )
 	fn_fetch_default_config
 	fn_default_config_remote
 	fn_set_config_vars
