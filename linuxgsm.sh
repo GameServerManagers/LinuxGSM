4945817f2f5eab3ca57b601281c4120fea96f2e2
jimwald1221 <31221082+jimwald1221@users.noreply.github.com>
2017-08-28 10:45:23 -0400

    Reverted git back to main

--- linuxgsm.sh
+++ linuxgsm.sh
@@ -42,9 +42,9 @@ configdirdefault="${lgsmdir}/config-default"
 ## GitHub Branch Select
 # Allows for the use of different function files
 # from a different repo and/or branch.
-githubuser="jimwald1221"
-githubrepo="LinuxGSM-Testing"
-githubbranch="backup-testing"
+githubuser="GameServerManagers"
+githubrepo="LinuxGSM"
+githubbranch="master"
 
 # Core Function that is required first
 core_functions.sh(){
