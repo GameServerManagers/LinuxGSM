94a4b5c6d42f51ab4df773c2ad1672e447522457
Daniel Gibbs <me@danielgibbs.co.uk>
2017-08-31 16:28:38 +0100

    Altered MailGun print

--- lgsm/functions/alert_mailgun.sh
+++ lgsm/functions/alert_mailgun.sh
@@ -8,15 +8,15 @@ local commandname="ALERT"
 local commandaction="Alert"
 local function_selfname="$(basename $(readlink -f "${BASH_SOURCE[0]}"))"
 
-fn_print_dots "Sending Email (Mailgun) alert"
+fn_print_dots "Sending Email alert: Mailgun: ${email}"
 sleep 0.5
 mailgunsend=$(curl -s --user "api:${mailguntoken}" -F from="LinuxGSM <${mailgunemailfrom}>" -F to="LinuxGSM Admin <${mailgunemail}>" -F subject="${alertemoji} ${alertsubject} ${alertemoji}" --form-string html="<br><br><b>Message</b><br>${alertbody}<br><br><b>Game</b><br>${gamename}<br><br><b>Server name</b><br>${servername}<br><br><b>Hostname</b><br>${HOSTNAME}<br><br><b>Server IP</b><br><a href='https://www.gametracker.com/server_info/${ip}:${port}'>${ip}:${port}</a><br><br><b>More info</b><br><a href='${alerturl}'>${alerturl}</a>" https://api.mailgun.net/v3/${mailgundomain}/messages)
 
 if [ -z "${mailgunsend}" ]; then
-	fn_print_fail_nl "Sending Email (Mailgun) alert"
-	fn_script_log_fatal "Sending Email (Mailgun) alert"
+	fn_print_fail_nl "Sending Email alert: Mailgun: ${email}"
+	fn_script_log_fatal "Sending Email alert: Mailgun: ${email}"
 else
-	fn_print_ok_nl "Sending Email (Mailgun) alert"
-	fn_script_log_pass "Sent Email (Mailgun) alert"
+	fn_print_ok_nl "Sending Email alert: Mailgun: ${email}"
+	fn_script_log_pass "Sending Email alert: Mailgun: ${email}"
 fi
 
