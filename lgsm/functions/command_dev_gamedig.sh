3cabe86ec8e0eff62cad5d3820ab71a5d67708bd
Daniel Gibbs <me@danielgibbs.co.uk>
2018-03-12 13:47:36 +0000

    removed gamedig from develop not ready for this branch

--- lgsm/functions/command_dev_gamedig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# LinuxGSM command_dev_gamedig.sh function
-# Author: Daniel Gibbs
-# Website: https://gameservermanagers.com
-# Description: Displays output of gamedig in json format.
-
-local commandname="GAMEDIG"
-local commandaction="Gamedig"
-local function_selfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
