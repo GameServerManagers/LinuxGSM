80b13ac739b4daaa27711417b2f5e8939ca336f2
Daniel Gibbs <me@danielgibbs.co.uk>
2018-03-14 19:34:13 +0000

    tests

--- tests/tests_shellcheck.sh
+++ tests/tests_shellcheck.sh
@@ -2,11 +2,10 @@
 # Project: Game Server Managers - LinuxGSM
 # Author: Daniel Gibbs
 # License: MIT License, Copyright (c) 2017 Daniel Gibbs
-# Purpose: TravisCI Tests: Teamspeak 3 | Linux Game Server Management Script
+# Purpose: TravisCI Tests: Shellcheck | Linux Game Server Management Script
 # Contributors: https://github.com/GameServerManagers/LinuxGSM/graphs/contributors
 # Documentation: https://github.com/GameServerManagers/LinuxGSM/wiki
 # Website: https://gameservermanagers.com
 
-
-ls -al
-shellcheck --shell=bash --exclude=SC2154,SC2034 lgsm/functions/*
+find . -type f -name "*.sh" -exec shellcheck --shell=bash --exclude=SC2154,SC2034 {} \;
+find . -type f -name "*.cfg" -exec shellcheck --shell=bash --exclude=SC2154,SC2034 {} \;
