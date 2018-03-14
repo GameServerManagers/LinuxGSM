bd59b54ac4b42b23e58bdb2c9990b183fb57a1d1
Daniel Gibbs <me@danielgibbs.co.uk>
2018-03-14 22:32:57 +0000

    SC2046

--- lgsm/functions/install_config.sh
+++ lgsm/functions/install_config.sh
@@ -62,7 +62,7 @@ fn_set_config_vars(){
 		fn_script_log_info "changing hostname."
 		sleep 1
 
-		if [ $(grep SERVERNAME=SERVERNAME \"${lgsmdir}/config-default/config-game/${config}\" 2>/dev/null) ]; then
+		if [ "$(grep SERVERNAME=SERVERNAME \"${lgsmdir}/config-default/config-game/${config}\" 2>/dev/null)" ]; then
 			sed -i "s/SERVERNAME=SERVERNAME/SERVERNAME=${servername}/g" "${servercfgfullpath}"
 		else
 			sed -i "s/SERVERNAME/${servername}/g" "${servercfgfullpath}"
