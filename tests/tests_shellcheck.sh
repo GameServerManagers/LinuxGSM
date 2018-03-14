5111f4eb75db5b6267f8de2ce6f3ba402284a0dc
Daniel Gibbs <me@danielgibbs.co.uk>
2018-03-14 17:54:03 +0000

    Testing shellcheck in travis

---
+++ tests/tests_shellcheck.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Project: Game Server Managers - LinuxGSM
+# Author: Daniel Gibbs
+# License: MIT License, Copyright (c) 2017 Daniel Gibbs
+# Purpose: TravisCI Tests: Teamspeak 3 | Linux Game Server Management Script
+# Contributors: https://github.com/GameServerManagers/LinuxGSM/graphs/contributors
+# Documentation: https://github.com/GameServerManagers/LinuxGSM/wiki
+# Website: https://gameservermanagers.com
+
+githubuser="GameServerManagers"
+githubrepo="LinuxGSM"
+githubbranch="$TRAVIS_BRANCH"
+
+git clone
+shellcheck --shell=bash --exclude=SC2154,SC2034 GameServerManagers/LinuxGSM/lgsm/functions/*
