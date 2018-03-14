53b177df3b0261891918af7e5d511129d43c50c9
Daniel Gibbs <me@danielgibbs.co.uk>
2018-03-14 20:17:13 +0000

    test

--- tests/tests_shellcheck.sh
+++ tests/tests_shellcheck.sh
@@ -7,5 +7,5 @@
 # Documentation: https://github.com/GameServerManagers/LinuxGSM/wiki
 # Website: https://gameservermanagers.com
 
-find . -type f -name "shunit2-2.1.6" -prune -name "*.sh" -exec shellcheck --shell=bash --exclude=SC2154,SC2034 {} \;
-find . -type f -name "shunit2-2.1.6" -prune -name "*.cfg" -exec shellcheck --shell=bash --exclude=SC2154,SC2034 {} \;
+find . -type d \( ! -name "shunit2-2.1.6" \) -type f -prune -name "*.sh" -exec shellcheck --shell=bash --exclude=SC2154,SC2034 {} \;
+find . -type d \( ! -name "shunit2-2.1.6" \) -type f -prune -name "*.cfg" -exec shellcheck --shell=bash --exclude=SC2154,SC2034 {} \;
