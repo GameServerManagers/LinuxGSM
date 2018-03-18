6ad869783d296c139e6c81ea15e88414cb53d156
Daniel Gibbs <me@danielgibbs.co.uk>
2018-03-18 15:40:55 +0000

    Message stating IP automatically set

--- lgsm/functions/check_ip.sh
+++ lgsm/functions/check_ip.sh
@@ -72,6 +72,9 @@ if [ "${gamename}" != "TeamSpeak 3" ]&&[ "${gamename}" != "Mumble" ]&&[ "${travi
 			fn_script_log_fatal "https://linuxgsm.com/network-interfaces\n"
 			core_exit.sh
 		else
+			fn_print_info_nl "Check IP: ${getip}"
+			fn_script_log_info "IP automatically set as: ${getip}"
+			sleep 1
 			ip="${getip}"
 		fi
 	fi
