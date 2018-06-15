f3a9f524fd16743ef16cdc1716c6c4cf4aa1c744
Daniel Gibbs <me@danielgibbs.co.uk>
2018-06-15 22:04:19 +0100

    Added BrainBread

--- lgsm/functions/install_server_files.sh
+++ lgsm/functions/install_server_files.sh
@@ -140,7 +140,7 @@ elif [ "${gamename}" == "Multi Theft Auto" ]; then
 elif [ "${gamename}" == "Factorio" ]; then
   update_factorio.sh
   install_factorio_save.sh
-elif [ -z "${appid}" ]||[ "${gamename}" == "GoldenEye: Source" ]||[ "${gamename}" == "Base Defense" ]; then
+elif [ -z "${appid}" ]||[ "${gamename}" == "GoldenEye: Source" ]||[ "${gamename}" == "Base Defense" ]||[ "${gamename}" == "BrainBread" ]; then
 	if [ "${gamename}" == "Unreal Tournament" ]; then
 		install_unreal_tournament_eula.sh
 	fi
