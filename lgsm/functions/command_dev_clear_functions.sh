514ec0e686dfb0b2e80880e05c6f9f3b6d20b24e
Daniel Gibbs <me@danielgibbs.co.uk>
2018-06-16 17:03:20 +0100

    clear-functions now removes the _default.cfg

--- lgsm/functions/command_dev_clear_functions.sh
+++ lgsm/functions/command_dev_clear_functions.sh
@@ -10,5 +10,6 @@ echo "================================="
 echo ""
 if fn_prompt_yn "Do you want to delete all functions?" Y; then
 	rm -rfv "${functionsdir}"/*
+	rm -rfv "${configdirdefault}"/*
 fi
 core_exit.sh
