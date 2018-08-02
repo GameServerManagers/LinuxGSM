88d31dd1b3b82772c8b4b3a68e12578a9551a3d9
Daniel Gibbs <me@danielgibbs.co.uk>
2018-08-02 22:31:49 +0100

    will not run if no IP address

--- lgsm/functions/info_distro.sh
+++ lgsm/functions/info_distro.sh
@@ -182,9 +182,11 @@ if [ -z "${extip}" ]; then
 fi
 
 # Steam Master Server - checks if detected by master server
-steammaster=$(${curlpath} -s 'https://api.steampowered.com/ISteamApps/GetServersAtAddress/v0001?addr=${ip}:${port}&format=json' | jq '.response.servers[]|.addr' | wc -l)
-if [ "${steammaster}" == "1" ]; then
-	steammaster="yes"
-else
-	steammaster="no"
+if [ "${ip}" ] && [ "${port}" ]; then
+	steammaster=$(${curlpath} -s 'https://api.steampowered.com/ISteamApps/GetServersAtAddress/v0001?addr=${ip}:${port}&format=json' | jq '.response.servers[]|.addr' | wc -l)
+	if [ "${steammaster}" == "1" ]; then
+		steammaster="true"
+	else
+		steammaster="false"
+	fi
 fi
