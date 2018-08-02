41db1231bb0ee498584c91a31f311c847fcc7568
Daniel Gibbs <me@danielgibbs.co.uk>
2018-08-02 22:37:17 +0100

    Fixed memory header deails alignment

--- lgsm/functions/info_messages.sh
+++ lgsm/functions/info_messages.sh
@@ -71,7 +71,7 @@ fn_info_message_performance(){
 	} | column -s $'\t' -t
 	echo -e ""
 	{
-		echo -e "${blue}Mem:\t${blue}total\t used\t free\t cached\t available${default}"
+		echo -e "${blue}Mem:\t${blue}total\tused\tfree\tcached\tavailable${default}"
 		echo -e "${blue}Physical:\t${default}${physmemtotal}\t${physmemused}\t${physmemfree}\t${physmemcached}\t${physmemavailable}${default}"
 		echo -e "${blue}Swap:\t${default}${swaptotal}\t${swapused}\t${swapfree}${default}"
 	} | column -s $'\t' -t
