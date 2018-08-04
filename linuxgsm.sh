a4341b84e7b65715cdbba45f6a6b491201b6dbb9
Mathias Sass Michno <m@michno.me>
2018-08-04 21:03:55 +0200

    Change default github user for fetching scripts

--- linuxgsm.sh
+++ linuxgsm.sh
@@ -46,7 +46,7 @@ userinput="${1}"
 ## GitHub Branch Select
 # Allows for the use of different function files
 # from a different repo and/or branch.
-githubuser="GameServerManagers"
+githubuser="mathiassmichno"
 githubrepo="LinuxGSM"
 githubbranch="master"
 
