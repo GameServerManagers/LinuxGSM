f32ef345ef83e3c81e1b55c6248b165d8534eb46
Daniel Gibbs <me@danielgibbs.co.uk>
2018-08-13 21:58:39 +0100

    Added missing Colours (#1987)
    * Added missing colours
    
    * Added option to turn ansi colours off

--- lgsm/functions/core_messages.sh
+++ lgsm/functions/core_messages.sh
@@ -12,13 +12,18 @@ if [ "${ansi}" != "off" ]; then
 	# echo colors
 	default="\e[0m"
 	red="\e[31m"
+	lightred="\e[91m"
 	green="\e[32m"
+	lightgreen="\e[92m"
 	yellow="\e[33m"
 	lightyellow="\e[93m"
 	blue="\e[34m"
 	lightblue="\e[94m"
 	magenta="\e[35m"
+	lightmagenta="\e[95m"
 	cyan="\e[36m"
+	lightcyan="\e[96m"
+
 	# carriage return & erase to end of line
 	creeol="\r\033[K"
 fi
