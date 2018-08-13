11a6193b6520ce1befd9521ce628bbd8f549013e
Daniel Gibbs <me@danielgibbs.co.uk>
2018-08-13 22:08:24 +0100

    Corrected details code

--- lgsm/functions/command_details.sh
+++ lgsm/functions/command_details.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # LinuxGSM command_details.sh function
 # Author: Daniel Gibbs
-# Contributor: UltimateByte
 # Website: https://linuxgsm.com
 # Description: Displays server information.
 
@@ -25,24 +24,10 @@ else
 	info_glibc.sh
 	info_parms.sh
 	info_messages.sh
-query_gamedig.sh
-fn_info_message_distro
-fn_info_message_performance
-fn_info_message_disk
-fn_info_message_gameserver
-fn_info_message_script
-fn_info_message_backup
-# Some game servers do not have parms.
-if [ "${gamename}" != "TeamSpeak 3" ]&&[ "${engine}" != "avalanche2.0" ]&&[ "${engine}" != "dontstarve" ]&&[ "${engine}" != "projectzomboid" ]&&[ "${engine}" != "renderware" ]; then
-	fn_parms
-	fn_info_message_commandlineparms
-fi
-fn_info_message_ports
-fn_info_message_select_engine
-fn_info_message_statusbottom
-core_exit.sh
+	query_gamedig.sh
 
 	#Output details
+	query_gamedig.sh
 	fn_info_message_distro
 	fn_info_message_performance
 	fn_info_message_disk
