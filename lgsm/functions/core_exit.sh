cb5acaaa6406e7d3708a278137a22d3802f5e767
Daniel Gibbs <me@danielgibbs.co.uk>
2018-10-07 12:19:29 +0100

    testing another rm solution

--- lgsm/functions/core_exit.sh
+++ lgsm/functions/core_exit.sh
@@ -16,7 +16,7 @@ fn_exit_dev_debug(){
 
 # If running dependency check as root will remove any files that belong to root user.
 if [ "$(whoami)" == "root" ]; then
-	find "${lgsmdir}"/ -group root -prune -exec rm -rf {} \;
+	find "${lgsmdir}"/ -group root -prune -exec rm -rf {} +
 fi
 
 if [ -n "${exitbypass}" ]; then
