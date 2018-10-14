f3a0f2f48220e6687889d0e97e341514e9300363
Daniel Gibbs <me@danielgibbs.co.uk>
2018-10-14 01:27:07 +0100

    Moved fix to install

--- lgsm/functions/fix.sh
+++ lgsm/functions/fix.sh
@@ -62,8 +62,6 @@ if [ "${function_selfname}" != "command_install.sh" ]; then
 		fix_ss3.sh
 	elif [ "${gamename}" == "Multi Theft Auto" ]; then
 		fix_mta.sh
-	elif [ "${shortname}" == "wurm" ]; then
-		fix_wurm.sh
 	fi
 fi
 
@@ -85,6 +83,8 @@ if [ "${function_selfname}" == "command_install.sh" ]; then
 			fix_ut.sh
 		elif [ "${gamename}" == "Unreal Tournament 3" ]; then
 			fix_ut3.sh
+		elif [ "${shortname}" == "wurm" ]; then
+			fix_wurm.sh
 		else
 			fn_print_information_nl "No fixes required."
 		fi
