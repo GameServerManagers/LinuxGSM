ad2afdcda0281e546d6e92642e22399c1ff098c6
Daniel Gibbs <me@danielgibbs.co.uk>
2018-10-31 18:00:04 +0000

    fix_tf2.sh now matches fix_dst.sh as the same issue occurs

--- lgsm/functions/fix_tf2.sh
+++ lgsm/functions/fix_tf2.sh
@@ -8,22 +8,10 @@ local commandname="FIX"
 local commandaction="Fix"
 local function_selfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-if [[ -f /etc/redhat-release ]]; then
-	echo "Applying missing libcurl-gnutls.so.4 fix for Redhat-based systems."
-	cd ${serverfiles}/bin || exit
-	if [[ -L libcurl-gnutls.so.4 ]]; then
-		echo "Fix already applied."
-	else
-		curl_lib="/usr/lib/libcurl.so.4"
-		if [[ -L $curl_lib ]]; then
-			ln -nfs $curl_lib libcurl-gnutls.so.4
-			if [[ "$?" != "0" ]]; then
-				echo "Fix failed, ln exitied non-zero."
-			else
-				echo "Fix applied successfully."
-			fi
-		else
-			echo "Missing library: $curl_lib, dnf -y install libcurl.i686"
-		fi
-	 fi
+# Fixes: Team Fortress 2 Segmentation fault for Red-Hat Distros #2062.
+if [ -f "/etc/redhat-release" ] && [ ! -f "${serverfiles}/bin/libcurl-gnutls.so.4" ]; then
+	fixname="libcurl-gnutls.so.4 missing"
+	fn_fix_msg_start
+	ln -s "/usr/lib/libcurl.so.4" "${serverfiles}/bin/libcurl-gnutls.so.4"
+	fn_fix_msg_end
 fi
