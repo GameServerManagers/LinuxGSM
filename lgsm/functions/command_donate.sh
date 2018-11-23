26b726d60f513e2fb346e57ca5c5775027fdd901
Daniel Gibbs <me@danielgibbs.co.uk>
2018-11-23 19:56:17 +0000

    Visual updates

--- lgsm/functions/command_donate.sh
+++ lgsm/functions/command_donate.sh
@@ -4,15 +4,15 @@
 # Website: https://linuxgsm.com
 # Description: Shows ways to donate
 
+echo ""
 echo "Support LinuxGSM"
 echo "================================="
 echo ""
 echo "Been using LinuxGSM?"
 echo "Consider donating to support development."
 echo ""
-echo "* Patreon: https://linuxgsm.com/patreon"
-echo "* PayPal: https://linuxgsm.com/paypal"
-echo "* Flattr: https://linuxgsm.com/flattr"
-echo "* Buy me a Coffee: https://linuxgsm.com/coffee"
+echo "* ${blue}Patreon:${default} https://linuxgsm.com/patreon"
+echo "* ${blue}PayPal:${default} https://linuxgsm.com/paypal"
+echo "* ${blue}Flattr:${default} https://linuxgsm.com/flattr"
 echo ""
 echo "LinuxGSM has been going since 2012"
