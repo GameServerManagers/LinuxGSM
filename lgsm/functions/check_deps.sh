06d891358cfe29d68e952af36ac270736f7f87a4
Daniel Gibbs <me@danielgibbs.co.uk>
2018-12-15 00:28:03 +0000

    refactor(check): add epel as a requirement for centos

--- lgsm/functions/check_deps.sh
+++ lgsm/functions/check_deps.sh
@@ -430,7 +430,9 @@ fn_deps_build_redhat(){
 	# LinuxGSM requirements
 	## CentOS 6
 	if [ "${distroversion}" == "6" ]; then
-		array_deps_required=( curl wget util-linux-ng python file gzip bzip2 unzip binutils bc jq )
+		array_deps_required=( epel-release curl wget util-linux-ng python file gzip bzip2 unzip binutils bc jq )
+	if [ "${distroversion}" == "7" ]; then
+		array_deps_required=( epel-release curl wget util-linux python file gzip bzip2 unzip binutils bc jq )
 	elif [ "${distroid}" == "fedora" ]; then
 			array_deps_required=( curl wget util-linux python2 file gzip bzip2 unzip binutils bc jq )
 	elif [[ "${distroname}" == *"Amazon Linux AMI"* ]]; then
