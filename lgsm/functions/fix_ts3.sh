9bc5458898208ab081dee496f9c6f665aa63ae3d
Daniel Gibbs <me@danielgibbs.co.uk>
2018-12-15 00:01:29 +0000

    fix(ts3server): re-add support for libmariadb2 (#2114)
    * fix(ts3server): re-add support for libmariadb2 #1924

---
+++ lgsm/functions/fix_ts3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# LinuxGSM fix_ts3.sh function
+# Author: Daniel Gibbs
+# Website: https://linuxgsm.com
+# Description: Resolves various issues with Teamspeak 3.
+
+local commandname="FIX"
+local commandaction="Fix"
+local function_selfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+# Fixes: makes libmariadb2 available #1924
+if [ ! -f "${serverfiles}/libmariadb.so.2" ]; then
+	ln -s "${serverfiles}/redist/libmariadb.so.2" "${serverfiles}/libmariadb.so.2"
+fi
