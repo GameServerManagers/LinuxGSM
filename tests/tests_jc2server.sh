f5aa6bedad1ee42d01e3b3f1d4a644debe731b64
Daniel Gibbs <me@danielgibbs.co.uk>
2018-12-20 20:06:46 +0000

    feat(travisci): improve test numbering and visual

--- tests/tests_jc2server.sh
+++ tests/tests_jc2server.sh
@@ -359,7 +359,7 @@ else
 	fi
 
 	# Prevents running of core_exit.sh for Travis.
-	if [ "${travistest}" != "1" ]; then
+	if [ -z "${travistest}" ]; then
 		getopt=$1
 		core_getopt.sh
 	fi
@@ -467,10 +467,15 @@ echo "Testing Branch: $TRAVIS_BRANCH"
 echo "================================="
 
 echo ""
-echo "0.1 - Create log dir's"
-echo "================================="
+echo "0.0 - Pre-test Tasks"
+echo "=================================================================="
 echo "Description:"
 echo "Create log dir's"
+echo ""
+
+echo ""
+echo "0.1 - Create log dir's"
+echo "================================="
 echo ""
 (
 	exec 5>"${TRAVIS_BUILD_DIR}/dev-debug.log"
@@ -500,11 +505,15 @@ echo "================="
 grep functionfile= "${TRAVIS_BUILD_DIR}/dev-debug.log" | sed 's/functionfile=//g'
 
 echo ""
-echo "1.0 - start - no files"
+echo "1.0 - Pre-install tests"
+echo "=================================================================="
+echo ""
+
+echo "1.1 - start - no files"
 echo "================================="
 echo "Description:"
 echo "test script reaction to missing server files."
-echo "Command: ./jc2server start"
+echo "Command: ./${gameservername} start"
 echo ""
 # Allows for testing not on Travis CI
 if [ ! -v TRAVIS ]; then
@@ -524,11 +533,11 @@ echo "================="
 grep functionfile= "${TRAVIS_BUILD_DIR}/dev-debug.log" | sed 's/functionfile=//g'
 
 echo ""
-echo "1.1 - getopt"
+echo "1.2 - getopt"
 echo "================================="
 echo "Description:"
 echo "displaying options messages."
-echo "Command: ./jc2server"
+echo "Command: ./${gameservername}"
 echo ""
 (
 	exec 5>"${TRAVIS_BUILD_DIR}/dev-debug.log"
@@ -542,11 +551,11 @@ echo "================="
 grep functionfile= "${TRAVIS_BUILD_DIR}/dev-debug.log" | sed 's/functionfile=//g'
 
 echo ""
-echo "1.2 - getopt with incorrect args"
+echo "1.3 - getopt with incorrect args"
 echo "================================="
 echo "Description:"
 echo "displaying options messages."
-echo "Command: ./jc2server abc123"
+echo "Command: ./${gameservername} abc123"
 echo ""
 getopt="abc123"
 (
@@ -561,11 +570,15 @@ echo "================="
 grep functionfile= "${TRAVIS_BUILD_DIR}/dev-debug.log" | sed 's/functionfile=//g'
 
 echo ""
+echo "2.0 - Instalation"
+echo "=================================================================="
+
+echo ""
 echo "2.0 - install"
 echo "================================="
 echo "Description:"
 echo "install ${gamename} server."
-echo "Command: ./jc2server auto-install"
+echo "Command: ./${gameservername} auto-install"
 (
 	exec 5>"${TRAVIS_BUILD_DIR}/dev-debug.log"
 	BASH_XTRACEFD="5"
@@ -578,11 +591,15 @@ echo "================="
 grep functionfile= "${TRAVIS_BUILD_DIR}/dev-debug.log" | sed 's/functionfile=//g'
 
 echo ""
+echo "3.0 - Start/Stop/Restart Tests"
+echo "=================================================================="
+
+echo ""
 echo "3.1 - start"
 echo "================================="
 echo "Description:"
 echo "start ${gamename} server."
-echo "Command: ./jc2server start"
+echo "Command: ./${gameservername} start"
 requiredstatus="OFFLINE"
 fn_setstatus
 (
@@ -601,7 +618,7 @@ echo "3.2 - start - online"
 echo "================================="
 echo "Description:"
 echo "start ${gamename} server while already running."
-echo "Command: ./jc2server start"
+echo "Command: ./${gameservername} start"
 requiredstatus="ONLINE"
 fn_setstatus
 (
@@ -620,7 +637,7 @@ echo "3.3 - start - updateonstart"
 echo "================================="
 echo "Description:"
 echo "will update server on start."
-echo "Command: ./jc2server start"
+echo "Command: ./${gameservername} start"
 requiredstatus="OFFLINE"
 fn_setstatus
 (
@@ -639,7 +656,7 @@ echo "3.4 - stop"
 echo "================================="
 echo "Description:"
 echo "stop ${gamename} server."
-echo "Command: ./jc2server stop"
+echo "Command: ./${gameservername} stop"
 requiredstatus="ONLINE"
 fn_setstatus
 (
@@ -658,7 +675,7 @@ echo "3.5 - stop - offline"
 echo "================================="
 echo "Description:"
 echo "stop ${gamename} server while already stopped."
-echo "Command: ./jc2server stop"
+echo "Command: ./${gameservername} stop"
 requiredstatus="OFFLINE"
 fn_setstatus
 (
@@ -677,7 +694,7 @@ echo "3.6 - restart"
 echo "================================="
 echo "Description:"
 echo "restart ${gamename}."
-echo "Command: ./jc2server restart"
+echo "Command: ./${gameservername} restart"
 requiredstatus="ONLINE"
 fn_setstatus
 (
@@ -696,7 +713,7 @@ echo "3.7 - restart - offline"
 echo "================================="
 echo "Description:"
 echo "restart ${gamename} while already stopped."
-echo "Command: ./jc2server restart"
+echo "Command: ./${gameservername} restart"
 requiredstatus="OFFLINE"
 fn_setstatus
 (
@@ -711,11 +728,15 @@ echo "================="
 grep functionfile= "${TRAVIS_BUILD_DIR}/dev-debug.log" | sed 's/functionfile=//g'
 
 echo ""
+echo "4.0 - Update Tests"
+echo "=================================================================="
+
+echo ""
 echo "4.1 - update"
 echo "================================="
 echo "Description:"
 echo "check for updates."
-echo "Command: ./jc2server update"
+echo "Command: ./${gameservername} update"
 requiredstatus="OFFLINE"
 fn_setstatus
 (
@@ -880,11 +901,15 @@ sed -i "/BindIP/c\BindIP                      = \"${travisip}\"," "${serverfiles
 echo "IP: ${travisip}"
 
 echo ""
+echo "5.0 - Monitor Tests"
+echo "=================================================================="
+
+echo ""
 echo "5.1 - monitor - online"
 echo "================================="
 echo "Description:"
 echo "run monitor server while already running."
-echo "Command: ./jc2server monitor"
+echo "Command: ./${gameservername} monitor"
 requiredstatus="ONLINE"
 fn_setstatus
 (
@@ -903,7 +928,7 @@ echo "5.2 - monitor - offline - with lockfile"
 echo "================================="
 echo "Description:"
 echo "run monitor while server is offline with lockfile."
-echo "Command: ./jc2server monitor"
+echo "Command: ./${gameservername} monitor"
 requiredstatus="OFFLINE"
 fn_setstatus
 fn_print_info_nl "creating lockfile."
@@ -924,7 +949,7 @@ echo "5.3 - monitor - offline - no lockfile"
 echo "================================="
 echo "Description:"
 echo "run monitor while server is offline with no lockfile."
-echo "Command: ./jc2server monitor"
+echo "Command: ./${gameservername} monitor"
 requiredstatus="OFFLINE"
 fn_setstatus
 (
@@ -939,12 +964,12 @@ echo "================="
 grep functionfile= "${TRAVIS_BUILD_DIR}/dev-debug.log" | sed 's/functionfile=//g'
 
 echo ""
-echo "5.4 - monitor - query_gsquery.py failure"
+echo "5.4 - test-alert"
 echo "================================="
 echo "Description:"
-echo "query_gsquery.py will fail to query port."
-echo "Command: ./jc2server monitor"
-requiredstatus="ONLINE"
+echo "run monitor while server is offline with no lockfile."
+echo "Command: ./${gameservername} test-alert"
+requiredstatus="OFFLINE"
 fn_setstatus
 cp "${servercfgfullpath}" "config.lua"
 sed -i 's/[0-9]\+/0/' "${servercfgfullpath}"
@@ -952,7 +977,7 @@ sed -i 's/[0-9]\+/0/' "${servercfgfullpath}"
 	exec 5>"${TRAVIS_BUILD_DIR}/dev-debug.log"
 	BASH_XTRACEFD="5"
 	set -x
-	command_monitor.sh
+	command_test_alert.sh
 )
 fn_test_result_fail
 echo "run order"
@@ -965,11 +990,15 @@ cp -v "config.lua" "${servercfgfullpath}"
 echo "================================="
 
 echo ""
-echo "6.0 - details"
+echo "6.0 - Details Tests"
+echo "=================================================================="
+
+echo ""
+echo "6.1 - details"
 echo "================================="
 echo "Description:"
 echo "display details."
-echo "Command: ./jc2server details"
+echo "Command: ./${gameservername} details"
 requiredstatus="ONLINE"
 fn_setstatus
 (
@@ -984,11 +1013,11 @@ echo "================="
 grep functionfile= "${TRAVIS_BUILD_DIR}/dev-debug.log" | sed 's/functionfile=//g'
 
 echo ""
-echo "6.1 - post details"
+echo "6.2 - postdetails"
 echo "================================="
 echo "Description:"
 echo "post details."
-echo "Command: ./jc2server postdetails"
+echo "Command: ./${gameservername} postdetails"
 requiredstatus="ONLINE"
 fn_setstatus
 (
@@ -1003,11 +1032,15 @@ echo "================="
 grep functionfile= "${TRAVIS_BUILD_DIR}/dev-debug.log" | sed 's/functionfile=//g'
 
 echo ""
-echo "7.0 - backup"
+echo "7.0 - Backup Tests"
+echo "=================================================================="
+
+echo ""
+echo "7.1 - backup"
 echo "================================="
 echo "Description:"
 echo "run a backup."
-echo "Command: ./jc2server backup"
+echo "Command: ./${gameservername} backup"
 requiredstatus="ONLINE"
 fn_setstatus
 echo "test de-activated until issue #1839 fixed"
@@ -1018,11 +1051,15 @@ echo "================="
 grep functionfile= "${TRAVIS_BUILD_DIR}/dev-debug.log" | sed 's/functionfile=//g'
 
 echo ""
-echo "8.0 - dev - detect glibc"
+echo "8.0 - Development Tools Tests"
+echo "=================================================================="
+
+echo ""
+echo "8.1 - dev - detect glibc"
 echo "================================="
 echo "Description:"
 echo "detect glibc."
-echo "Command: ./jc2server detect-glibc"
+echo "Command: ./${gameservername} detect-glibc"
 requiredstatus="ONLINE"
 fn_setstatus
 (
@@ -1037,11 +1074,11 @@ echo "================="
 grep functionfile= "${TRAVIS_BUILD_DIR}/dev-debug.log" | sed 's/functionfile=//g'
 
 echo ""
-echo "8.1 - dev - detect ldd"
+echo "8.2 - dev - detect ldd"
 echo "================================="
 echo "Description:"
 echo "detect ldd."
-echo "Command: ./jc2server detect-ldd"
+echo "Command: ./${gameservername} detect-ldd"
 requiredstatus="ONLINE"
 fn_setstatus
 (
@@ -1056,11 +1093,11 @@ echo "================="
 grep functionfile= "${TRAVIS_BUILD_DIR}/dev-debug.log" | sed 's/functionfile=//g'
 
 echo ""
-echo "8.2 - dev - detect deps"
+echo "8.3 - dev - detect deps"
 echo "================================="
 echo "Description:"
 echo "detect dependencies."
-echo "Command: ./jc2server detect-deps"
+echo "Command: ./${gameservername} detect-deps"
 requiredstatus="ONLINE"
 fn_setstatus
 (
@@ -1075,11 +1112,11 @@ echo "================="
 grep functionfile= "${TRAVIS_BUILD_DIR}/dev-debug.log" | sed 's/functionfile=//g'
 
 echo ""
-echo "8.3 - dev - query raw"
+echo "8.4 - dev - query-raw"
 echo "================================="
 echo "Description:"
-echo "detect glibc."
-echo "Command: ./jc2server detect-glibc"
+echo "raw query output."
+echo "Command: ./${gameservername} query-raw"
 requiredstatus="ONLINE"
 fn_setstatus
 (
