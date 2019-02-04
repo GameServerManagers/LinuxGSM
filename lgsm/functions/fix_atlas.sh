f53ea6ee02e3865d90f6bf51477ee036bd163754
xtoor <47255103+xtoor@users.noreply.github.com>
2019-02-03 20:55:11 -0500

    Create fix_atlas.sh

---
+++ lgsm/functions/fix_atlas.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# LinuxGSM fix_atlas.sh function
+# Author: Daniel Gibbs
+# Modified by xtoor for Atlas
+# Website: https://linuxgsm.com
+# Description: Resolves various issues with ATLAS Dedicated Server.
+
+# Symlinking the SteamCMD directory into the correct ATLAS directory so that the mods auto-management will work.
+if [ ! -d "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux" ]; then
+	ln -s "${steamcmddir}" "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux"
+fi
+
+if [ ! -d "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamapps" ]; then
+	ln -s "$HOME/Steam/steamapps/" "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamapps"
+fi
