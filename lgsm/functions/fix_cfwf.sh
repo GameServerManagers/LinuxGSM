2973f4bd9c7382f9c8d7b8e9ce86dc8cf0863900
BackInTheMatrix <sjcl5011@gmail.com>
2019-03-04 22:16:33 +0100

    Check if steamclient.so is different
    Check if steamclient.so is different

--- lgsm/functions/fix_cfwf.sh
+++ lgsm/functions/fix_cfwf.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # LinuxGSM fix_cfwf.sh function
-# Author: BackInTheMatrix
+# Author: Daniel Gibbs
+# Contributor: BackInTheMatrix
 # Website: https://linuxgsm.com
 # Description: Resolves various issues with Citadel: Forged With Fire.
 
-# Copying steamclient.so to correct Plugins folder, to ensure steam server launches.
-cp "${steamcmddir}/linux64/steamclient.so" "${serverfiles}/Citadel/Plugins/UWorks/Source/ThirdParty/Linux/steamclient.so"
+# Copy steamclient.so to Plugins folder to ensure that steam server launches.
+if [ "$(diff "${steamcmddir}/linux64/steamclient.so" "${serverfiles}/Citadel/Plugins/UWorks/Source/ThirdParty/Linux/steamclient.so" >/dev/null)" ]; then
+	cp -f "${steamcmddir}/linux64/steamclient.so" "${serverfiles}/Citadel/Plugins/UWorks/Source/ThirdParty/Linux/steamclient.so"
+fi
