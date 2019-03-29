3fe097f58c41ab2cfee004edf827ecec79b7efbe
Daniel Gibbs <me@danielgibbs.co.uk>
2019-03-29 23:24:31 +0000

    feat(mcserver): refactor mcserver updater

--- lgsm/functions/update_minecraft.sh
+++ lgsm/functions/update_minecraft.sh
@@ -8,100 +8,109 @@ local commandname="UPDATE"
 local commandaction="Update"
 local function_selfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-fn_update_dl(){
-	latestmcreleaselink=$(${curlpath} -s "https://launchermeta.mojang.com/mc/game/version_manifest.json" | jq -r '.latest.release as $latest | .versions[] | select(.id == $latest) | .url')
+fn_update_minecraft_dl(){
+	latestmcreleaselink=$(${curlpath} -s "https://launchermeta.${remotelocation}/mc/game/version_manifest.json" | jq -r '.latest.release as $latest | .versions[] | select(.id == $latest) | .url')
 	latestmcbuildurl=$(${curlpath} -s "${latestmcreleaselink}" | jq -r '.downloads.server.url')
-	fn_fetch_file "${latestmcbuildurl}" "${tmpdir}" "minecraft_server.${availablebuild}.jar"
+	fn_fetch_file "${latestmcbuildurl}" "${tmpdir}" "minecraft_server.${remotebuild}.jar"
 	echo -e "copying to ${serverfiles}...\c"
-	fn_script_log "Copying to ${serverfiles}"
-	cp "${tmpdir}/minecraft_server.${availablebuild}.jar" "${serverfiles}/minecraft_server.jar"
+	cp "${tmpdir}/minecraft_server.${remotebuild}.jar" "${serverfiles}/minecraft_server.jar"
 	local exitcode=$?
-	chmod u+x "${serverfiles}/minecraft_server.jar"
-	if [ ${exitcode} -eq 0 ]; then
+	if [ "${exitcode}" == "0" ]; then
 		fn_print_ok_eol_nl
+		fn_script_log_pass "Copying to ${serverfiles}"
+		chmod u+x "${serverfiles}/minecraft_server.jar"
+		fn_clear_tmp
 	else
 		fn_print_fail_eol_nl
+		fn_script_log_fatal "Copying to ${serverfiles}"
+		core_exit.sh
 	fi
 }
 
-fn_update_currentbuild(){
-	# Gets current build info
-	# Checks if current build info is available. If it fails, then a server restart will be forced to generate logs.
+fn_update_minecraft_localbuild(){
+	# Gets local build info.
+	fn_print_dots "Checking for update: ${remotelocation}: checking local build"
+	# Checks if current build info is remote. If it fails, then a server restart will be forced to generate logs.
 	if [ ! -f "${consolelogdir}/${servicename}-console.log" ]; then
-		fn_print_error "Checking for update: mojang.com"
+		fn_print_error "Checking for update: ${remotelocation}: checking local build"
 		sleep 0.5
-		fn_print_error_nl "Checking for update: mojang.com: No logs with server version found"
-		fn_script_log_error "Checking for update: mojang.com: No logs with server version found"
+		fn_print_error_nl "Checking for update: ${remotelocation}: checking local build: no log files"
+		fn_script_log_error "Checking for update: ${remotelocation}: checking local build: no log files"
 		sleep 0.5
-		fn_print_info_nl "Checking for update: mojang.com: Forcing server restart"
-		fn_script_log_info "Checking for update: mojang.com: Forcing server restart"
+		fn_print_info_nl "Checking for update: ${remotelocation}: checking local build: forcing server restart"
+		fn_script_log_info "Forcing server restart"
 		sleep 0.5
 		exitbypass=1
 		command_stop.sh
 		exitbypass=1
 		command_start.sh
-		sleep 0.5
 		# Check again and exit on failure.
 		if [ ! -f "${consolelogdir}/${servicename}-console.log" ]; then
-			fn_print_fail_nl "Checking for update: mojang.com: Still No logs with server version found"
-			fn_script_log_fatal "Checking for update: mojang.com: Still No logs with server version found"
-			core_exit.sh
+			localbuild="0"
+			fn_print_error "Checking for update: ${remotelocation}: checking local build"
+			fn_script_log_error "Checking local build"
 		fi
+		sleep 0.5
 	fi
 
-	# Get current build from logs
-	currentbuild=$(cat "${serverfiles}/logs/latest.log" 2> /dev/null | grep version | grep -Eo '((\.)?[0-9]{1,3}){1,3}\.[0-9]{1,3}')
-	if [ -z "${currentbuild}" ]; then
-		fn_print_error_nl "Checking for update: mojang.com: Current build version not found"
-		fn_script_log_error "Checking for update: mojang.com: Current build version not found"
-		sleep 0.5
-		fn_print_info_nl "Checking for update: mojang.com: Forcing server restart"
-		fn_script_log_info "Checking for update: mojang.com: Forcing server restart"
-		exitbypass=1
-		command_stop.sh
-		exitbypass=1
-		command_start.sh
-		currentbuild=$(cat "${serverfiles}/logs/latest.log" 2> /dev/null | grep version | grep -Eo '((\.)?[0-9]{1,3}){1,3}\.[0-9]{1,3}')
-		if [ -z "${currentbuild}" ]; then
-			fn_print_fail_nl "Checking for update: mojang.com: Current build version still not found"
-			fn_script_log_fatal "Checking for update: mojang.com: Current build version still not found"
-			core_exit.sh
+	if [ -f "${consolelogdir}/${servicename}-console.log" ]; then
+		# Get current build from logs
+		localbuild=$(cat "${serverfiles}/logs/latest.log" 2> /dev/null | grep version | grep -Eo '((\.)?[0-9]{1,3}){1,3}\.[0-9]{1,3}')
+		if [ -z "${localbuild}" ]; then
+			fn_print_error_nl "Checking for update: ${remotelocation}: checking local build: not found"
+			fn_script_log_error "Local build info not found"
+			sleep 0.5
+			fn_print_info_nl "Checking for update: ${remotelocation}: checking local build: forcing server restart"
+			fn_script_log_info "Forcing server restart"
+			exitbypass=1
+			command_stop.sh
+			exitbypass=1
+			command_start.sh
+			localbuild=$(cat "${serverfiles}/logs/latest.log" 2> /dev/null | grep version | grep -Eo '((\.)?[0-9]{1,3}){1,3}\.[0-9]{1,3}')
+			if [ -z "${localbuild}" ]; then
+				fn_print_fail_nl "Checking for update: ${remotelocation}: checking local build: not found"
+				fn_script_log_fatal "Local build version still not found"
+				core_exit.sh
+			fi
 		fi
 	fi
 }
 
-fn_update_availablebuild(){
-	# Gets latest build info.
-	availablebuild=$(${curlpath} -s "https://launchermeta.mojang.com/mc/game/version_manifest.json" | jq -r '.latest.release')
-	# Checks if availablebuild variable has been set
-	if [ -z "${availablebuild}" ]; then
-		fn_print_fail "Checking for update: mojang.com"
-		sleep 0.5
-		fn_print_fail "Checking for update: mojang.com: Not returning version info"
-		fn_script_log_fatal "Failure! Checking for update: mojang.com: Not returning version info"
+fn_update_minecraft_remotebuild(){
+	# Gets remote build info.
+	fn_print_dots "Checking for update: ${remotelocation}: checking remote build"
+	remotebuild=$(${curlpath} -s "https://launchermeta.${remotelocation}/mc/game/version_manifest.json" | jq -r '.latest.release')
+	# Checks if remotebuild variable has been set.
+	if [ -v "${remotebuild}" ]; then
+		fn_print_fail "Checking for update: ${remotelocation}: checking remote build"
+		fn_script_log_fatal "Checking remote build"
 		core_exit.sh
-	elif [ "${installer}" == "1" ]; then
-		:
 	else
-		fn_print_ok_nl "Checking for update: mojang.com"
-		fn_script_log_pass "Checking for update: mojang.com"
-		sleep 0.5
+		fn_print_ok "Checking for update: ${remotelocation}: checking remote build"
+		fn_script_log_pass "Checking remote build"
 	fi
+	sleep 0.5
 }
 
-fn_update_compare(){
-	# Removes dots so if can compare version numbers
-	currentbuilddigit=$(echo "${currentbuild}" | tr -cd '[:digit:]')
-	availablebuilddigit=$(echo "${availablebuild}" | tr -cd '[:digit:]')
-
-	if [ "${currentbuilddigit}" -ne "${availablebuilddigit}" ]; then
-		echo -e "\n"
-		echo -e "Update available:"
+fn_update_minecraft_compare(){
+	# Removes dots so if statement can compare version numbers.
+	fn_print_dots "Checking for update: ${remotelocation}"
+	localbuilddigit=$(echo "${localbuild}" | tr -cd '[:digit:]')
+	remotebuilddigit=$(echo "${remotebuild}" | tr -cd '[:digit:]')
+	sleep 0.5
+	if [ "${localbuilddigit}" -ne "${remotebuilddigit}" ]; then
+		fn_print_ok_nl "Checking for update: ${remotelocation}"
 		sleep 0.5
-		echo -e "	Current build: ${red}${currentbuild}${default}"
-		echo -e "	Available build: ${green}${availablebuild}${default}"
-		echo -e ""
+		echo -en "\n"
+		echo -e "Update ${mta_update_string}:"
+		echo -e "* Local build: ${green}${localbuild}${default}"
+		echo -e "* Remote build: ${green}${remotebuild}${default}"
+		fn_script_log_info "Update available"
+		fn_script_log_info "Local build: ${localbuild}"
+		fn_script_log_info "Remote build: ${remotebuild}"
+		fn_script_log_info "${localbuild} > ${remotebuild}"
 		sleep 0.5
+		echo -en "\n"
 		echo -en "applying update.\r"
 		sleep 1
 		echo -en "applying update..\r"
@@ -109,16 +118,13 @@ fn_update_compare(){
 		echo -en "applying update...\r"
 		sleep 1
 		echo -en "\n"
-		fn_script_log "Update available"
-		fn_script_log "Current build: ${currentbuild}"
-		fn_script_log "Available build: ${availablebuild}"
-		fn_script_log "${currentbuild} > ${availablebuild}"
 
 		unset updateonstart
 
 		check_status.sh
 		if [ "${status}" == "0" ]; then
-			fn_update_dl
+			exitbypass=1
+			fn_update_factorio_dl
 			exitbypass=1
 			command_start.sh
 			exitbypass=1
@@ -126,33 +132,36 @@ fn_update_compare(){
 		else
 			exitbypass=1
 			command_stop.sh
-			fn_update_dl
+			exitbypass=1
+			fn_update_factorio_dl
 			exitbypass=1
 			command_start.sh
 		fi
 		alert="update"
 		alert.sh
 	else
-		echo -e "\n"
-		echo -e "No update available:"
-		echo -e "	Current version: ${green}${currentbuild}${default}"
-		echo -e "	Available version: ${green}${availablebuild}${default}"
-		echo -e ""
-		fn_print_ok_nl "No update available"
-		fn_script_log_info "Current build: ${currentbuild}"
-		fn_script_log_info "Available build: ${availablebuild}"
+		fn_print_ok_nl "Checking for update: ${remotelocation}"
+		sleep 0.5
+		echo -en "\n"
+		echo -e "No update available"
+		echo -e "* Local build: ${green}${localbuild}${default}"
+		echo -e "* Remote build: ${green}${remotebuild}${default}"
+		fn_script_log_info "No update available"
+		fn_script_log_info "Local build: ${localbuild}"
+		fn_script_log_info "Remote build: ${remotebuild}"
 	fi
 }
 
+# The location where the builds are checked and downloaded.
+remotelocation="mojang.com"
 if [ "${installer}" == "1" ]; then
-	fn_update_availablebuild
-	fn_update_dl
+	fn_update_minecraft_remotebuild
+	fn_update_minecraft_dl
 else
-	# Checks for server update from mojang.com
-	fn_print_dots "Checking for update: mojang.com"
-	fn_script_log_info "Checking for update: mojang.com"
+	fn_print_dots "Checking for update: ${remotelocation}"
+	fn_script_log_info "Checking for update: ${remotelocation}"
 	sleep 0.5
-	fn_update_currentbuild
-	fn_update_availablebuild
-	fn_update_compare
+	fn_update_minecraft_localbuild
+	fn_update_minecraft_remotebuild
+	fn_update_minecraft_compare
 fi
