2e2ed6888a9c56ac7d1409e167b82dcb687c2161
Daniel Gibbs <me@danielgibbs.co.uk>
2019-04-09 21:09:45 +0100

    correct config

--- tests/tests_mcserver.sh
+++ tests/tests_mcserver.sh
@@ -774,7 +774,7 @@ echo "Description:"
 echo "Inserting Travis IP in to config."
 echo "Allows monitor to work"
 travisip=$(ip -o -4 addr|grep eth0|awk '{print $4}'|grep -oe '\([0-9]\{1,3\}\.\?\)\{4\}'|grep -v 127.0.0)
-sed -i "/server-ip=/c\server-ip=${travisip}" "${serverfiles}/config.lua"
+sed -i "/server-ip=/c\server-ip=${travisip}" "${servercfgdefault}"
 echo "IP: ${travisip}"
 
 echo ""
