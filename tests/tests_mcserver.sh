3d884310db4dfeb547e5d2a8a346c8294fb29433
Daniel Gibbs <me@danielgibbs.co.uk>
2019-04-09 23:27:01 +0100

    added internet IP next to query raw

--- tests/tests_mcserver.sh
+++ tests/tests_mcserver.sh
@@ -999,6 +999,20 @@ echo "================="
 grep functionfile= "${TRAVIS_BUILD_DIR}/dev-debug.log" | sed 's/functionfile=//g'
 
 echo ""
+echo "Inserting IP address"
+echo "================================="
+echo "Description:"
+echo "Inserting Travis IP in to config."
+echo "Allows monitor to work"
+if [ "$(ip -o -4 addr|grep eth0)" ]; then
+	travisip=$(ip -o -4 addr | grep eth0 | awk '{print $4}' | grep -oe '\([0-9]\{1,3\}\.\?\)\{4\}' | grep -v 127.0.0)
+else
+	travisip=$(ip -o -4 addr | grep ens | awk '{print $4}' | grep -oe '\([0-9]\{1,3\}\.\?\)\{4\}' | sort -u | grep -v 127.0.0)
+fi
+sed -i "/server-ip=/c\server-ip=${travisip}" "${serverfiles}/server.properties"
+echo "IP: ${travisip}"
+
+echo ""
 echo "8.4 - dev - query-raw"
 echo "================================="
 echo "Description:"
