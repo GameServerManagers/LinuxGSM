1ae1e3d1fe74c566d0afd31fbebe16824aa7ca4e
Daniel Gibbs <me@danielgibbs.co.uk>
2019-05-12 22:50:07 +0100

    updates to messages
    converted spaces to tabs

--- lgsm/functions/command_send.sh
+++ lgsm/functions/command_send.sh
@@ -10,30 +10,31 @@ local function_selfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 check.sh
 fn_print_header
+fn_print_information_nl "Send allows a command to be sent to the console."
 if [ "${shortname}" == "rust" ]||[ "${shortname}" == "hw" ]||[ "${shortname}" == "ark" ]||[ "${shortname}" == "ts3" ]; then
-        fn_print_information_nl "${gamename} does not allow server commands to be entered in to the console"
-        core_exit.sh
+	fn_print_information_nl "${gamename} does not allow server commands to be entered into the console."
+	core_exit.sh
 fi
 
-sleep 0.5
 check_status.sh
 if [ "${status}" != "0" ]; then
 	if [ -n "${userinput2}" ]; then
 		command_to_send="${userinput2}"
-    else
-    	command_to_send=$( fn_prompt_message "Write the command you want to send: " )
-    fi
-    fn_print_info_nl "Sending \"${command_to_send}\""
+	else
+			command_to_send=$( fn_prompt_message "send: " )
+	fi
+	fn_print_information_nl "Sending \"${command_to_send}\"."
 	fn_print_dots "Sending command to console"
-	fn_script_log_pass "Sending command \"${command_to_send}\" to console"
-	sleep 0.5
-    tmux send-keys -t "${servicename}" "${command_to_send}" ENTER
-    fn_print_ok_nl "Command sended to console"
-    fn_script_log_pass "Sended command \"${command_to_send}\" to console"
+	tmux send-keys -t "${servicename}" "${command_to_send}" ENTER
+	ffn_print_complete_nl "Command sent to console."
+	fn_script_log_pass "Command \"${command_to_send}\" sent to console"
 else
-    fn_print_error_nl "Server not running"
-    fn_script_log_error "Failed to access: Server not running"
-    sleep 0.5
+	fn_print_error_nl "Server not running"
+	fn_script_log_error "Failed to access: Server not running"
+	if fn_prompt_yn "Do you want to start the server?" Y; then
+		exitbypass=1
+		command_start.sh
+	fi
 fi
 
 core_exit.sh
