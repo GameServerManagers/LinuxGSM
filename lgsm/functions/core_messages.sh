83520eee8eba163f7ec8429504eaac618a67ea4c
lucas duval <duval.lucas@hotmail.fr>
2019-05-13 09:40:23 +0900

    Add menu selector which take a string with option on each line

--- lgsm/functions/core_messages.sh
+++ lgsm/functions/core_messages.sh
@@ -407,3 +407,82 @@ fn_print_update_eol(){
 fn_print_update_eol_nl(){
 	echo -e "${cyan}UPDATE${default}"
 }
+
+# Menu selector
+##################################
+
+# Bash Menu
+fn_menu_bash_string() {
+	local resultvar=$1
+	title=$2
+	caption=$3
+	options=$4
+	fn_print_horizontal
+	fn_print_center "${title}"
+	fn_print_center "${caption}"
+	fn_print_horizontal
+	menu_options=()
+	while read -r line; do
+		if [ -n "${line}" ];then
+			var=${line:1:-1}
+			menu_options+=( "${var}" )
+		fi
+	done <<<  ${options}
+	menu_options+=( "Cancel" )
+	select option in "${menu_options[@]}"; do
+		if [ -n "${option}" ]&&[ "${option}" != "Cancel" ]; then
+			eval "$resultvar=\"${option/%\ */}\""
+		fi
+		break
+	done
+}
+
+ # Whiptail/Dialog Menu
+fn_menu_whiptail_string() {
+	local menucmd=$1
+	local resultvar=$2
+	title=$3
+	caption=$4
+	options=$5
+	height=${6:-40}
+	width=${7:-80}
+	menuheight=${8:-30}
+	prefix=${9}
+	menu_options=()
+	while read -r line; do
+		if [ -n "${line}" ];then
+			var=${line:1:-1}
+			menu_options+=( ${var//\"} "${prefix}${var//\"}" )
+		fi
+	done <<< "${options}"
+	OPTION=$(${menucmd} --title "${title}" --menu "${caption}" "${height}" "${width}" "${menuheight}" "${menu_options[@]}" 3>&1 1>&2 2>&3)
+	if [ $? == 0 ]; then
+		eval "$resultvar=\"${OPTION}\""
+	else
+		eval "$resultvar="
+	fi
+}
+
+ # Menu selector
+fn_menu_string() {
+	local resultvar=$1
+	local selection=""
+	title=$2
+	caption=$3
+	options=$4
+	prefix=${5:""}
+	# Get menu command
+	for menucmd in whiptail dialog bash; do
+		if [ -x "$(command -v "${menucmd}")" ]; then
+			menucmd=$(command -v "${menucmd}")
+			break
+		fi
+	done
+	case "$(basename "${menucmd}")" in
+		whiptail|dialog)
+			fn_menu_whiptail_string "${menucmd}" selection "${title}" "${caption}" "${options}" 40 80 30 "${prefix}";;
+		*)
+			fn_menu_bash_string selection "${title}" "${caption}" "${options}";;
+	esac
+	eval "$resultvar=\"${selection}\""
+}
