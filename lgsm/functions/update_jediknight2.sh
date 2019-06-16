0ceaaff51ec8b352761c12a27768916c1e3a7766
Daniel Gibbs <me@danielgibbs.co.uk>
2019-06-16 14:55:26 +0100

    Updated JK2server
    Still needs development

---
+++ lgsm/functions/update_jediknight2.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+# LinuxGSM update_mumble.sh function
+# Author: Daniel Gibbs
+# Website: https://linuxgsm.com
+# Description: Handles updating of Mumble servers.
+
+local commandname="UPDATE"
+local commandaction="Update"
+local function_selfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+fn_update_mumble_dl(){
+	fn_fetch_file "https://github.com/mvdevs/jk2mv/releases/download/${remotebuild}/jk2mv-${remotebuild}-dedicated.zip" "${tmpdir}" "jk2mv-${remotebuild}-dedicated.zip"
+	fn_dl_extract "${tmpdir}" "jk2mv-${remotebuild}-dedicated.zip" "${tmpdir}"
+	echo -e "copying to ${serverfiles}...\c"
+	cp -R "${tmpdir}/murmur-static_${mumblearch}-${remotebuild}/"* "${serverfiles}"
+	local exitcode=$?
+	if [ "${exitcode}" == "0" ]; then
+		fn_print_ok_eol_nl
+		fn_script_log_pass "Copying to ${serverfiles}"
+		fn_clear_tmp
+	else
+		fn_print_fail_eol_nl
+		fn_script_log_fatal "Copying to ${serverfiles}"
+		core_exit.sh
+	fi
+}
+
+fn_update_mumble_localbuild(){
+	# Gets local build info.
+	fn_print_dots "Checking for update: ${remotelocation}: checking local build"
+	# Uses executable to find local build.
+	cd "${executabledir}" || exit
+	if [ -f "${executable}" ]; then
+		localbuild=$(${executable} -version 2>&1 >/dev/null | awk '{print $5}')
+		fn_print_ok "Checking for update: ${remotelocation}: checking local build"
+		fn_script_log_pass "Checking local build"
+	else
+		localbuild="0"
+		fn_print_error "Checking for update: ${remotelocation}: checking local build"
+		fn_script_log_error "Checking local build"
+	fi
+}
+
+fn_update_mumble_remotebuild(){
+	# Gets remote build info.
+	remotebuild=$(${curlpath}-s "https://api.github.com/repos/mvdevs/jk2mv/releases/latest"| grep dedicated.zip | tail -1 | awk -F"/" '{ print $8 }')
+	if [ "${installer}" != "1" ]; then
+		fn_print_dots "Checking for update: ${remotelocation}: checking remote build"
+		# Checks if remotebuild variable has been set.
+		if [ -z "${remotebuild}" ]||[ "${remotebuild}" == "null" ]; then
+			fn_print_fail "Checking for update: ${remotelocation}: checking remote build"
+			fn_script_log_fatal "Checking remote build"
+			core_exit.sh
+		else
+			fn_print_ok "Checking for update: ${remotelocation}: checking remote build"
+			fn_script_log_pass "Checking remote build"
+		fi
+	else
+		# Checks if remotebuild variable has been set.
+		if [ -z "${remotebuild}" ]||[ "${remotebuild}" == "null" ]; then
+			fn_print_failure "Unable to get remote build"
+			fn_script_log_fatal "Unable to get remote build"
+			core_exit.sh
+		fi
+	fi
+}
+
+fn_update_mumble_compare(){
+	# Removes dots so if statement can compare version numbers.
+	fn_print_dots "Checking for update: ${remotelocation}"
+	localbuilddigit=$(echo "${localbuild}" | tr -cd '[:digit:]')
+	remotebuilddigit=$(echo "${remotebuild}" | tr -cd '[:digit:]')
+	if [ "${localbuilddigit}" -ne "${remotebuilddigit}" ]||[ "${forceupdate}" == "1" ]; then
+		fn_print_ok_nl "Checking for update: ${remotelocation}"
+		echo -en "\n"
+		echo -e "Update available"
+		echo -e "* Local build: ${red}${localbuild} ${mumblearch}${default}"
+		echo -e "* Remote build: ${green}${remotebuild} ${mumblearch}${default}"
+		fn_script_log_info "Update available"
+		fn_script_log_info "Local build: ${localbuild} ${mumblearch}"
+		fn_script_log_info "Remote build: ${remotebuild} ${mumblearch}"
+		fn_script_log_info "${localbuild} > ${remotebuild}"
+		fn_sleep_time
+		echo -en "\n"
+		echo -en "applying update.\r"
+		sleep 1
+		echo -en "applying update..\r"
+		sleep 1
+		echo -en "applying update...\r"
+		sleep 1
+		echo -en "\n"
+
+		unset updateonstart
+
+		check_status.sh
+		# If server stopped.
+		if [ "${status}" == "0" ]; then
+			exitbypass=1
+			fn_update_mumble_dl
+			exitbypass=1
+			command_start.sh
+			exitbypass=1
+			command_stop.sh
+		# If server started.
+		else
+			exitbypass=1
+			command_stop.sh
+			exitbypass=1
+			fn_update_mumble_dl
+			exitbypass=1
+			command_start.sh
+		fi
+		alert="update"
+		alert.sh
+	else
+		fn_print_ok_nl "Checking for update: ${remotelocation}"
+		echo -en "\n"
+		echo -e "No update available"
+		echo -e "* Local build: ${green}${localbuild} ${mumblearch}${default}"
+		echo -e "* Remote build: ${green}${remotebuild} ${mumblearch}${default}"
+		fn_script_log_info "No update available"
+		fn_script_log_info "Local build: ${localbuild} ${mumblearch}"
+		fn_script_log_info "Remote build: ${remotebuild} ${mumblearch}"
+	fi
+}
+
+# The location where the builds are checked and downloaded.
+remotelocation="jk2mv.org"
+
+# Game server architecture.
+mumblearch="x86"
+
+if [ "${installer}" == "1" ]; then
+	fn_update_mumble_remotebuild
+	fn_update_mumble_dl
+else
+	fn_print_dots "Checking for update: ${remotelocation}"
+	fn_script_log_info "Checking for update: ${remotelocation}"
+	fn_update_mumble_localbuild
+	fn_update_mumble_remotebuild
+	fn_update_mumble_compare
+fi
