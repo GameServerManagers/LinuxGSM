bef5e2b5d6db8f14165664665146d9f30f411f93
Robin Bourne <cam40902@gmail.com>
2019-06-21 16:09:05 +0100

    Fix push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,7 +6,7 @@ git config --global user.name "Bourne-ID"
 git remote set-url origin https://Bourne-ID:${GH_TOKEN}@github.com/Bourne-ID/LinuxGSM.git
 
 git checkout ${TRAVIS_BRANCH}
-git add ./utilslgsm/data/default_parameters.csv
+git add ./lgsm/data/default_parameters.csv
 git commit --message "Travis build: $(date +%Y-%m-%d)"
 
 git push --set-upstream origin ${TRAVIS_BRANCH}
