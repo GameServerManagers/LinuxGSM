e5d467f2838bcbb2a7eb6b645e8aeaf7a88a707e
Robin Bourne <cam40902@gmail.com>
2019-06-27 11:34:52 +0100

    Change repo

--- linuxgsm.sh
+++ linuxgsm.sh
@@ -46,9 +46,9 @@ userinput="${1}"
 ## GitHub Branch Select
 # Allows for the use of different function files
 # from a different repo and/or branch.
-githubuser="GameServerManagers"
+githubuser="Bourne-ID"
 githubrepo="LinuxGSM"
-githubbranch="master"
+githubbranch="devops/defaultcheck"
 
 # Core function that is required first.
 core_functions.sh(){
