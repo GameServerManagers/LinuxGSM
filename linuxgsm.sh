418b0629c80165745d877c14898294be03a71677
psymin <psymin@runlinux.net>
2019-08-14 14:16:32 +0000

    Added my git login info

--- linuxgsm.sh
+++ linuxgsm.sh
@@ -46,9 +46,9 @@ userinput="${1}"
 ## GitHub Branch Select
 # Allows for the use of different function files
 # from a different repo and/or branch.
-githubuser="GameServerManagers"
+githubuser="psymin"
 githubrepo="LinuxGSM"
-githubbranch="master"
+githubbranch="feature/warfork"
 
 # Core function that is required first.
 core_functions.sh(){
