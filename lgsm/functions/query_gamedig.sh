698ccd3495b71b9bf6fb5cb02b4f62dfa8a16744
Daniel Gibbs <me@danielgibbs.co.uk>
2019-08-25 15:32:07 +0100

    bug

--- lgsm/functions/query_gamedig.sh
+++ lgsm/functions/query_gamedig.sh
@@ -62,7 +62,7 @@ if [ "$(command -v gamedig 2>/dev/null)" ]&&[ "$(command -v jq 2>/dev/null)" ]; 
 			fi
 		done
 
-		local shortname_query_arra=( soldat )
+		local shortname_query_array=( soldat )
 		for engine_query in "${engine_query_array[@]}"
 		do
 			if [ "${engine_query}" == "${engine}" ]; then
