11638be687251662078d0ee92fd2d4ed3ed949d1
Daniel Gibbs <me@danielgibbs.co.uk>
2019-08-27 15:04:06 +0100

    and if backup dir not equal legacy dir

--- lgsm/functions/command_backup.sh
+++ lgsm/functions/command_backup.sh
@@ -92,7 +92,7 @@ fn_backup_dir(){
 # Migrate Backups from old dir before refactor
 fn_backup_migrate_olddir(){
 	# Check if old backup dir is there before the refactor and move the backups
-	if [ -d "${rootdir}/backups" ]; then
+	if [ -d "${rootdir}/backups" ]||[ "${rootdir}/backups" != "${backupdir}" ]; then
 		fn_print_dots "Backup directory is being migrated"
 		fn_script_log_info "Backup directory is being migrated"
 		fn_script_log_info "${rootdir}/backups > ${backupdir}"
