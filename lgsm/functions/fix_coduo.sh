0fee1e839a35239a12147dcf4aaa117d2fcdd138
Frisasky <digimoncn@gmail.com>
2019-08-30 20:11:42 +0800

    Update fix_coduo.sh

--- lgsm/functions/fix_coduo.sh
+++ lgsm/functions/fix_coduo.sh
@@ -1,11 +1,1 @@
-#!/usr/bin/env bash
-# LinuxGSM fix_coduo.sh function
-# Author: Alexander Hurd
-# Website: https://linuxgsm.com
-# Description: Fixes for Call of Duty: United Offensive
 
-local commandname="FIX"
-local commandaction="Fix"
-
-# Force glibc fix.
-fix_glibc.sh
