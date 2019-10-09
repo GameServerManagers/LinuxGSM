a8dd4a7d1d608be5536583505611ca06f54fa9a8
Frisasky <digimoncn@gmail.com>
2019-10-09 17:16:38 +0800

    Update check_deps.sh

--- lgsm/functions/check_deps.sh
+++ lgsm/functions/check_deps.sh
@@ -448,7 +448,7 @@ fn_deps_build_redhat(){
 	# LinuxGSM requirements.
 	# CentOS
 	if [ "${distroversion}" == "6" ]; then
-		array_deps_required=( epel-release curl wget util-linux-ng python3 file gzip bzip2 unzip binutils bc jq )
+		array_deps_required=( epel-release curl wget util-linux-ng python file gzip bzip2 unzip binutils bc jq )
 	elif [ "${distroversion}" == "7" ]; then
 		array_deps_required=( epel-release curl wget util-linux python3 file gzip bzip2 unzip binutils bc jq )
 	elif [ "${distroversion}" == "8" ]; then
