8555ccc061c1e2e8f8093dd644bbf6d2273012a1
Daniel Gibbs <me@danielgibbs.co.uk>
2019-10-13 14:25:43 +0100

    added new download link

--- lgsm/functions/install_server_files.sh
+++ lgsm/functions/install_server_files.sh
@@ -30,7 +30,7 @@ fn_install_server_files(){
 	elif [ "${shortname}" == "ges" ]; then
 		remote_fileurl="https://files.linuxgsm.com/GoldenEyeSource/GoldenEye_Source_v5.0.6_full_server.tar.bz2"; local_filedir="${tmpdir}"; local_filename="GoldenEye_Source_v5.0.6_full_server.tar.bz2"; chmodx="nochmodx" run="norun"; force="noforce"; md5="c45c16293096706e8b5e2cd64a6f2931"
 	elif [ "${shortname}" == "mohaa" ]; then
-		remote_fileurl="https://files.linuxgsm.com/MedalofHonorAlliedAssault/MOH_REVIVAL_V2.tar.bz2"; local_filedir="${tmpdir}"; local_filename="MOH_REVIVAL_V2.tar.bz2"; chmodx="nochmodx" run="norun"; force="noforce"; md5="0ed3b7a5a917447df07a8ea662381aed"
+		remote_fileurl="https://files.linuxgsm.com/MedalofHonorAlliedAssault/moh_revival_v1.12_RC3.5.1.tar.bz2"; local_filedir="${tmpdir}"; local_filename="moh_revival_v1.12_RC3.5.1.tar.bz2"; chmodx="nochmodx" run="norun"; force="noforce"; md5="8a997a2991583f311ed4f3206286842a"
 	elif [ "${shortname}" == "ns" ]; then
 		remote_fileurl="https://files.linuxgsm.com/NaturalSelection/ns_dedicated_server_v32.tar.bz2"; local_filedir="${tmpdir}"; local_filename="ns_dedicated_server_v32.tar.bz2"; chmodx="nochmodx" run="norun"; force="noforce"; md5="499cf63324b76925ada6baf5f2eacd67"
 	elif [ "${shortname}" == "q2" ]; then
