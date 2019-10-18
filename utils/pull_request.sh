3103567e88cbc30614ccb683fcc43e1cd9f33e5b
BourneID <cam40902@gmail.com>
2019-10-18 23:51:37 +0100

    failure test

---
+++ utils/pull_request.sh
@@ -0,0 +1,11 @@
+# Process GSF to get latest manditory fields
+
+# Get list of game servers folders with changes - this may be none
+
+# process game servers to ensure manditory fields exist
+
+# report to PR
+
+echo "Failutr Test"
+exit 12
+
