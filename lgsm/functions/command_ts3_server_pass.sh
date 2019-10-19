6a8cd82a837182e4dd8389e81774f89b93b73439
Daniel Gibbs <me@danielgibbs.co.uk>
2019-10-20 00:32:06 +0100

    messages

--- lgsm/functions/command_ts3_server_pass.sh
+++ lgsm/functions/command_ts3_server_pass.sh
@@ -19,11 +19,12 @@ fn_serveradmin_password_prompt(){
 	fi
 	fn_script_log_info "Initiating ${gamename} ServerAdmin password change"
 	read -rp "Enter new password: " newpassword
+	fn_print_info_nl "Changing password"
+	fn_script_log_info "Changing password"
 }
 
 fn_serveradmin_password_set(){
-	fn_print_info_nl "Starting "
-	fn_script_log_info "Starting server with new password"
+
 	# Start server in "new password mode".
 	ts3serverpass="1"
 	exitbypass="1"
@@ -35,6 +36,7 @@ fn_serveradmin_password_set(){
 # Running functions.
 check.sh
 fn_serveradmin_password_prompt
+
 check_status.sh
 if [ "${status}" != "0" ]; then
 	# Stop any running server.
@@ -43,8 +45,6 @@ if [ "${status}" != "0" ]; then
 	fn_serveradmin_password_set
 	parms="serveradmin_password="${newpassword}" inifile="${servercfgfullpath}" > /dev/null 2>&1"
 	ts3serverpass="0"
-	fn_print_info_nl "Restarting server normally"
-	fn_script_log_info "Restarting server normally"
 	command_restart.sh
 else
 	fn_serveradmin_password_set
