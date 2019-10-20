395d6e00ab2afb85e87bd73b4c93c552746b550b
Daniel Gibbs <me@danielgibbs.co.uk>
2019-10-21 00:43:22 +0100

    cpuusedmhz

--- lgsm/functions/info_analytics.sh
+++ lgsm/functions/info_analytics.sh
@@ -27,8 +27,8 @@ curl https://www.google-analytics.com/collect -d "tid=UA-655379-31" -d "aip=1" -
 
 # Level 2 Analytics
 ## CPU usage of a game server
-if [ "${cpuused}" ]; then
-  curl https://www.google-analytics.com/collect -d "tid=UA-655379-31" -d "aip=1" -d "cid=${uuid}" -d "t=event" -d "ec=cpuused" -d "ea=${shortname}" -d "el=${cpuusedmhz}%" -d "v=1" > /dev/null 2>&1
+if [ "${cpuusedmhz}" ]; then
+  curl https://www.google-analytics.com/collect -d "tid=UA-655379-31" -d "aip=1" -d "cid=${uuid}" -d "t=event" -d "ec=cpuused" -d "ea=${shortname}" -d "el=${cpuusedmhz}" -d "v=1" > /dev/null 2>&1
 fi
 ## Ram usage of a game server
 if [ "${ramused}" ]; then
