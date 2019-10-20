5c3c36e8d2d5e7d3fb450d6406eeaa5eb319f9a3
Daniel Gibbs <me@danielgibbs.co.uk>
2019-10-20 01:01:22 +0100

    fixing privilege key generation

--- lgsm/functions/install_ts3db.sh
+++ lgsm/functions/install_ts3db.sh
@@ -71,6 +71,6 @@ echo -e "================================="
 fn_sleep_time
 fn_print_information_nl "Save these details for later."
 cd "${executabledir}" || exit
-${executable} start inifile=ts3-server.ini
+./ts3server_startscript.sh start inifile=ts3-server.ini | tee "${serverfiles}/privilege_key.txt"
 sleep 5
-${executable} stop
+./ts3server_startscript.sh stop
