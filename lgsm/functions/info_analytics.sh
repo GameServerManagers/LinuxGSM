52a7b68d98edd82e0170226dc12cf1c3837cf973
Daniel Gibbs <me@danielgibbs.co.uk>
2019-10-20 23:24:44 +0100

    anonymous ip

--- lgsm/functions/info_analytics.sh
+++ lgsm/functions/info_analytics.sh
@@ -7,4 +7,4 @@
 # Data collected: Game Server, Distro
 
 info_distro.sh
-curl https://www.google-analytics.com/collect -d "tid=UA-655379-31" -d "t=event" -d "ec=Distro" -d "ea=${distroid}" -d "el=${distroname}" -d "v=1" -d "cid=12345678"
+curl https://www.google-analytics.com/collect -d "tid=UA-655379-31" -d "aip=1" -d "t=event" -d "ec=Distro" -d "ea=${distroid}" -d "el=${distroname}" -d "v=1" -d "cid=12345678"
