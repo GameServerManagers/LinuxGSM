6ff8eeee28db1515e6d2fa5506359847bbf60ce6
Daniel Gibbs <me@danielgibbs.co.uk>
2019-10-22 19:47:33 +0100

    correctedvar

--- lgsm/functions/info_analytics.sh
+++ lgsm/functions/info_analytics.sh
@@ -19,7 +19,7 @@ fi
 uuid=$(cat "${datadir}/uuid.txt")
 # results are rounded up to reduce number of different results in analytics
 # nearest 100Mhz
-cpuusedmhzroundup=$(((${cpuusedmhz} + 99) / 100 * 100))
+cpuusedmhzroundup=$(((${cpuused} + 99) / 100 * 100))
 # nearest 100MB
 memusedroundup=$(((${memused} + 99) / 100 * 100))
 
