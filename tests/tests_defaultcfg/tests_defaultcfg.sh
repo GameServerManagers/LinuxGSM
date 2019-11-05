a39c7a8d699b2897e8a38c9e3128f25bdd2daae8
Daniel Gibbs <me@danielgibbs.co.uk>
2019-11-05 22:54:19 +0000

    added 2.0 check

--- tests/tests_defaultcfg/tests_defaultcfg.sh
+++ tests/tests_defaultcfg/tests_defaultcfg.sh
@@ -38,3 +38,23 @@ while IFS= read -r -d $'\0' line; do
 	fi
 	rm defaultcfgtemp.txt
 done
+
+echo -e ""
+echo -e "2.0 - Check Comment"
+echo -e "=================================================================="
+echo -e "Description:"
+echo -e "test checks that comments in ALL _default.cfg files are correct."
+echo -e ""
+find lgsm/config-default/config-lgsm/ ! -name '*template.cfg' -name "*.cfg" -type f -print0 |
+while IFS= read -r -d $'\0' line; do
+        grep # ${line}  > defaultcfgtemp.txt
+        diffoutput=$(diff tests/tests_defaultcfg/defaultcfg_0.txt  defaultcfgtemp.txt | grep '^<')
+        if [ "${diffoutput}" ]; then
+                echo "File with errors:"
+                echo "${line}"
+                echo -e "================================="
+                echo "${diffoutput}"
+                echo ""
+        fi
+        rm defaultcfgtemp.txt
+done
