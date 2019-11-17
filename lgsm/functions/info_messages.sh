9af38487525d7e7fbfa1062e9040949202aed24e
Daniel Gibbs <me@danielgibbs.co.uk>
2019-11-17 00:07:42 +0000

    added missing factorio rcon port

--- lgsm/functions/info_messages.sh
+++ lgsm/functions/info_messages.sh
@@ -744,7 +744,8 @@ fn_info_message_factorio(){
 	echo -e ""
 	{
 		echo -e "${lightblue}DESCRIPTION\tDIRECTION\tPORT\tPROTOCOL${default}"
-		echo -e "> Game\tINBOUND\t${port}\ttcp"
+		echo -e "> Game\tINBOUND\t${port}\tudp"
+    echo -e "> Game\tINBOUND\t${rconport}\ttcp"
 	} | column -s $'\t' -t
 }
 
