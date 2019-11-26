e085ce941ec4e36f3787aadcb682549ab1ef0c5d
Frisasky <digimoncn@gmail.com>
2019-11-26 15:35:07 +0800

    Update install_server_files.sh

--- lgsm/functions/install_server_files.sh
+++ lgsm/functions/install_server_files.sh
@@ -133,7 +133,7 @@ fn_install_server_files_steamcmd(){
 		counter="0"
 		while [ "${counter}" -le "4" ]; do
 			counter=$((counter+1))
-			${unbuffer} ./steamcmd.sh +login "${steamuser}" "${steampass}" +force_install_dir "${serverfiles}" +app_set_config 90 mod ${appidmod} +app_update "${appid}" ${branch} validate +quit
+			${unbuffer} ./steamcmd.sh +login "${steamuser}" "${steampass}" +force_install_dir "${serverfiles}" +app_set_config 90 mod ${appidmod} +app_update "${appid}" "${branch}" validate +quit
 			local exitcode=$?
 		done
 	fi
