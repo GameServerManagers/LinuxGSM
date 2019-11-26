98e2a6760867bb9a38e7af97e7558fda46353c92
Frisasky <digimoncn@gmail.com>
2019-11-26 17:31:56 +0800

    Update install_server_files.sh

--- lgsm/functions/install_server_files.sh
+++ lgsm/functions/install_server_files.sh
@@ -115,7 +115,7 @@ fn_install_server_files_steamcmd(){
 					${unbuffer} ./steamcmd.sh +login "${steamuser}" "${steampass}" +force_install_dir "${serverfiles}" +app_set_config 90 mod "${appidmod}" +app_update "${appid}" -beta ${branch} validate +quit
 					local exitcode=$?
 				elif [ "${gamename}" == "Jedi Knight II: Jedi Outcast" ]; then
-					${unbuffer} ./steamcmd.sh +@sSteamCmdForcePlatformType windows +login "${steamuser}" "${steampass}" +force_install_dir "${filesdir}" +app_update "${appid}" ${branch} -validate +quit
+					${unbuffer} ./steamcmd.sh +@sSteamCmdForcePlatformType windows +login "${steamuser}" "${steampass}" +force_install_dir "${filesdir}" +app_update "${appid}" -beta "${branch}" -validate +quit
 					local exitcode=$?
 				else
 					${unbuffer} ./steamcmd.sh +login "${steamuser}" "${steampass}" +force_install_dir "${serverfiles}" +app_update "${appid}" -beta ${branch} validate +quit
