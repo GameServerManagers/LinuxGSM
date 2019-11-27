a1fd3353928e68d591f2a6b911dc0fdb19bcdb0c
Daniel Gibbs <me@danielgibbs.co.uk>
2019-11-27 16:21:25 +0000

    Update tests_kcov.sh

--- tests/tests_kcov.sh
+++ tests/tests_kcov.sh
@@ -3,9 +3,9 @@ echo -e "Installing kcov"
 curl -L "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/shunit2/shunit2-2.1.6.tgz" | tar zx
 wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
 tar xzf master.tar.gz
-cd kcov-master
+cd kcov-master || exit
 mkdir build
-cd build
+cd build || exit
 cmake ..
 make
 sudo make install
