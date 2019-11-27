e5803065644856a4f4befebe1e7566f032a2c38c
Daniel Gibbs <me@danielgibbs.co.uk>
2019-11-27 13:03:31 +0000

    Update tests_ts3server.sh

--- tests/tests_ts3server.sh
+++ tests/tests_ts3server.sh
@@ -478,6 +478,8 @@ fn_test_result_na(){
 	fn_print_fail_nl "TEST N/A"
 }
 
+sleeptime="0"
+
 echo -e "================================="
 echo -e "Travis CI Tests"
 echo -e "Linux Game Server Manager"
