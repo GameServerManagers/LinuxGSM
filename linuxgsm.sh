05de5efb8332b08be3f55eac9add0425586d3c82
Daniel Gibbs <me@danielgibbs.co.uk>
2019-12-05 09:01:49 +0000

    release v19.12.5

--- linuxgsm.sh
+++ linuxgsm.sh
@@ -20,7 +20,7 @@ if [ -f ".dev-debug" ]; then
 	set -x
 fi
 
-version="v19.12.4"
+version="v19.12.5"
 shortname="core"
 gameservername="core"
 rootdir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
