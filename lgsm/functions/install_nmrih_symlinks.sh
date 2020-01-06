d2d7bd846bff90dc6359756285a4ef6740f0674d
Denperidge <denperidge@gmail.com>
2020-01-06 19:53:56 +0100

    nmrih_symlinks now use pre-existing location var

--- lgsm/functions/install_nmrih_symlinks.sh
+++ lgsm/functions/install_nmrih_symlinks.sh
@@ -4,11 +4,10 @@
 # Description: Create symlinks for renamed No More Room In Hell serverfiles 
 # Solution from Steam Community post: https://steamcommunity.com/app/224260/discussions/2/1732089092441769414/
 
-local bin=~/serverfiles/bin/
-ln -s $bin/vphysics_srv.so $bin/vphysics.so
-ln -s $bin/studiorender_srv.so $bin/studiorender.so
-ln -s $bin/soundemittersystem_srv.so $bin/soundemittersystem.so
-ln -s $bin/shaderapiempty_srv.so $bin/shaderapiempty.so
-ln -s $bin/scenefilecache_srv.so $bin/scenefilecache.so
-ln -s $bin/replay_srv.so $bin/replay.so
-ln -s $bin/materialsystem_srv.so $bin/materialsystem.so 
+ln -s $serverfiles/bin/vphysics_srv.so $serverfiles/bin/vphysics.so
+ln -s $serverfiles/bin/studiorender_srv.so $serverfiles/bin/studiorender.so
+ln -s $serverfiles/bin/soundemittersystem_srv.so $serverfiles/bin/soundemittersystem.so
+ln -s $serverfiles/bin/shaderapiempty_srv.so $serverfiles/bin/shaderapiempty.so
+ln -s $serverfiles/bin/scenefilecache_srv.so $serverfiles/bin/scenefilecache.so
+ln -s $serverfiles/bin/replay_srv.so $serverfiles/bin/replay.so
+ln -s $serverfiles/bin/materialsystem_srv.so $serverfiles/bin/materialsystem.so 
