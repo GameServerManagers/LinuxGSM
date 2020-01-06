524cf08adfe661dfe94a2b64e0130f7d5f6e5685
Denperidge <denperidge@gmail.com>
2020-01-06 12:52:45 +0100

    Fixed variable assignment in nmrih_symlinks

--- lgsm/functions/install_nmrih_symlinks.sh
+++ lgsm/functions/install_nmrih_symlinks.sh
@@ -4,7 +4,7 @@
 # Description: Create symlinks for renamed No More Room In Hell serverfiles 
 # Solution from Steam Community post: https://steamcommunity.com/app/224260/discussions/2/1732089092441769414/
 
-local bin = ~/serverfiles/bin/
+local bin=~/serverfiles/bin/
 ln -s $bin/vphysics_srv.so $bin/vphysics.so
 ln -s $bin/studiorender_srv.so $bin/studiorender.so
 ln -s $bin/soundemittersystem_srv.so $bin/soundemittersystem.so
