3b3de615c5ae5fe38e85dd4f2bcbc52838496316
H3o66 <chris.h3o66@gmail.com>
2020-02-03 22:57:07 +0100

    rework postinstall

--- lgsm/functions/fix_av_postinstall.sh
+++ lgsm/functions/fix_av_postinstall.sh
@@ -10,12 +10,12 @@ local commandaction="Fix"
 export LD_LIBRARY_PATH="${serverfiles}/linux64"
 
 fn_parms(){
-	parms="--datapath ${avdatapath} --galaxy-name ${avgalaxy} --stderr-to-log --stdout-to-log --init-folders-only"
+	parms="--datapath ${avdatapath} --galaxy-name ${avgalaxy} --init-folders-only"
 }
 
 fn_print_information "starting ${gamename} server to generate configs."
 fn_sleep_time
 # go to the executeable dir and start the init of the server
-cd "${executabledir}" || return 2
+cd "${systemdir}" || return 2
 fn_parms
-"${executable}" ${parms} >/dev/null
+"${executabledir}/${executable}" ${parms} >/dev/null
