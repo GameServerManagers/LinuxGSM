c25a300730bf71c5fddcb5b044e8c681e52db558
Daniel Gibbs <me@danielgibbs.co.uk>
2020-03-10 23:27:23 +0000

    removed libdir as no longer required

--- lgsm/functions/fix_mta.sh
+++ lgsm/functions/fix_mta.sh
@@ -4,7 +4,7 @@
 # Contributor: ChaosMTA
 # Website: https://linuxgsm.com
 # Description: Installs the libmysqlclient for database functions on the server
-local modulename="FIX"
+local commandname="FIX"
 local commandaction="Fix"
 local function_selfname=$(basename "$(readlink -f "${BASH_SOURCE[0]}")")
 
@@ -15,5 +15,3 @@ if [ ! -f "${lgsmdir}/lib/libmysqlclient.so.16" ]; then
 	fn_fetch_file "https://nightly.mtasa.com/files/modules/64/libmysqlclient.so.16" "${lgsmdir}/lib" "libmysqlclient.so.16" "chmodx" "norun" "noforce" "6c188e0f8fb5d7a29f4bc413b9fed6c2"
 	fn_fix_msg_end
 fi
-
-export LD_LIBRARY_PATH=:"${libdir}"
