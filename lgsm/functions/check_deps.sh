6b678c03b1df01217c66badf9438e73e79bb82ef
Daniel Gibbs <me@danielgibbs.co.uk>
2020-03-13 10:11:15 +0000

    change lib32tinfo5 to libtinfo5:i386

--- lgsm/functions/check_deps.sh
+++ lgsm/functions/check_deps.sh
@@ -352,7 +352,7 @@ fn_deps_build_debian(){
 	# No More Room in Hell, Counter-Strike: Source, Garry's Mod and Zombie Panic: Source
 	elif [ "${shortname}" == "nmrih" ]||[ "${shortname}" == "css" ]||[ "${shortname}" == "gmod" ]||[ "${shortname}" == "zps" ]; then
 		if [ "${arch}" == "x86_64" ]; then
-			array_deps_required+=( lib32tinfo5 )
+			array_deps_required+=( libtinfo5:i386 )
 		else
 			array_deps_required+=( libtinfo5 )
 		fi
