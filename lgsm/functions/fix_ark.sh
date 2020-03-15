87b01361dac2fb78351d2b2b6cfd9e20ada001c5
Daniel Gibbs <me@danielgibbs.co.uk>
2020-03-15 21:37:33 +0000

    fix(arkserver): remove broken symbolic steamcmd links

--- lgsm/functions/fix_ark.sh
+++ lgsm/functions/fix_ark.sh
@@ -4,6 +4,16 @@
 # Website: https://linuxgsm.com
 # Description: Resolves various issues with ARK: Survival Evolved.
 
+# removes the symlink if broken. fixes issue with older versions of LinuxGSM linking to /home/arkserver/steamcmd
+# rather than ${HOME}/.steam. This fix could be deprecated eventually.
+if [ ! -e "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux" ] ; then
+	rm -f "${serverfiles:?}/Engine/Binaries/ThirdParty/SteamCMD/Linux"
+fi
+
+if [ ! -e "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamapps" ] ; then
+	rm -f "${serverfiles:?}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamapps"
+fi
+
 # Symlinking the SteamCMD directory into the correct ARK directory so that the mods auto-management will work.
 if [ ! -d "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux" ]; then
 	ln -s "${HOME}/.steam/steamcmd" "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux"
