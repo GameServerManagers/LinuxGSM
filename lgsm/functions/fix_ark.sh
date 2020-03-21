cd371aea71de6808da3abe387bb1d9b3c3281378
Daniel Gibbs <me@danielgibbs.co.uk>
2020-03-21 15:08:53 +0000

    added if symlink exists

--- lgsm/functions/fix_ark.sh
+++ lgsm/functions/fix_ark.sh
@@ -22,7 +22,7 @@ if [ ! -f "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamcmd.sh"
 fi
 
 # if the steamapps symlink is incorrect unlink it.
-if [ "$(readlink ${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamapps)" != "${HOME}/Steam/steamapps" ]; then
+if [ -L "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamapps" ]&&[ "$(readlink ${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamapps)" != "${HOME}/Steam/steamapps" ]; then
 	fixname="incorrect steamapps symlink"
 	fn_fix_msg_start
 	unlink "${serverfiles:?}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamapps"
