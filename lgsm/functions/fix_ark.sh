86d8e14c29474b32e90445a49f61deea5591de0f
Daniel Gibbs <me@danielgibbs.co.uk>
2020-03-21 00:46:35 +0000

    yet another ark mod fix

--- lgsm/functions/fix_ark.sh
+++ lgsm/functions/fix_ark.sh
@@ -4,23 +4,26 @@
 # Website: https://linuxgsm.com
 # Description: Resolves various issues with ARK: Survival Evolved.
 
-# removes the symlink if broken. fixes issue with older versions of LinuxGSM linking to /home/arkserver/steamcmd
+# removes the symlink if broken or does not match what is excpected.
+# fixes issue with older versions of LinuxGSM linking to /home/arkserver/steamcmd
 # rather than ${HOME}/.steam. This fix could be deprecated eventually.
-if [ ! -e "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux" ]; then
-	fixname="broken steamcmd symlink"
+if [ ! -e "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux" ]||[ "$(readlink ${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux)" != "${HOME}/.steam/steamcmd" ]; then
+	fixname="broken SteamCMD symlink"
 	fn_fix_msg_start
-	rm -f "${serverfiles:?}/Engine/Binaries/ThirdParty/SteamCMD/Linux"
+	unlink "${serverfiles:?}/Engine/Binaries/ThirdParty/SteamCMD/Linux"
 	fn_fix_msg_end
 fi
 
-if [ ! -e "${HOME}/.steam/steamcmd/steamapps" ]; then
-	fixname="broken steamcmd symlink"
+if [ ! -e "${HOME}/.steam/steamcmd/SteamApps" ]||[ "$(readlink ${HOME}/.steam/steamcmd/SteamApps)" != "${HOME}/.steam/SteamApps" ]; then
+	fixname="broken SteamApps symlink"
 	fn_fix_msg_start
-	rm -f "${HOME}/.steam/steamcmd/steamapps"
+	unlink "${HOME}/.steam/steamcmd/SteamApps"
 	fn_fix_msg_end
 fi
 
 # Symlinking the SteamCMD directory into the correct ARK directory so that the mods auto-management will work.
+
+# Put symlink to SteamCMD in to Linux dir
 if [ ! -d "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux" ]; then
 	fixname="steamcmd symlink"
 	fn_fix_msg_start
@@ -28,9 +31,10 @@ if [ ! -d "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux" ]; then
 	fn_fix_msg_end
 fi
 
-if [ ! -d "${HOME}/.steam/steamcmd/steamapps" ]; then
+# Put symlink to SteamApps dir in steamcmd dir
+if [ ! -d "${HOME}/.steam/steamcmd/SteamApps" ]; then
 	fixname="steamcmd symlink"
 	fn_fix_msg_start
-	ln -s "${HOME}/Steam/steamapps" "${HOME}/.steam/steamcmd/steamapps"
+	ln -s "${HOME}/.steam/SteamApps" "${HOME}/.steam/steamcmd/SteamApps"
 	fn_fix_msg_end
 fi
