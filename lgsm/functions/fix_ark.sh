71bcb317568a66f008b25b46cc15747852aa6f53
Daniel Gibbs <me@danielgibbs.co.uk>
2020-03-23 19:59:59 +0000

    fix(arkserver): allow selecting of correct steamapps dir

--- lgsm/functions/fix_ark.sh
+++ lgsm/functions/fix_ark.sh
@@ -4,6 +4,15 @@
 # Website: https://linuxgsm.com
 # Description: Resolves various issues with ARK: Survival Evolved.
 
+# Steam mods directory selecter
+# This allows LinxuGSM to select either ~/.steam or ~/Steam. depending on what is being used
+if [ -d "${HOME}/.steam/SteamApps" ]; then
+	steamappsdir="${HOME}/.steam/SteamApps"
+elif [ -d "${HOME}/Steam/steamapps" ]; then
+	steamappsdir="${HOME}/Steam/steamapps"
+fi
+
+
 # removes the symlink if exists.
 # fixes issue with older versions of LinuxGSM linking to /home/arkserver/steamcmd
 if [ -L "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux" ]; then
@@ -24,7 +33,7 @@ if [ ! -f "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamcmd.sh"
 fi
 
 # if the steamapps symlink is incorrect unlink it.
-if [ -d "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux" ]&&[ -L "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamapps" ]&&[ "$(readlink ${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamapps)" != "${HOME}/Steam/steamapps" ]; then
+if [ -d "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux" ]&&[ -L "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamapps" ]&&[ "$(readlink ${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamapps)" != "${steamappsdir}" ]; then
 	fixname="incorrect steamapps symlink"
 	fn_fix_msg_start
 	unlink "${serverfiles:?}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamapps"
@@ -35,6 +44,6 @@ fi
 if [ ! -L "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamapps" ]; then
 	fixname="steamapps symlink"
 	fn_fix_msg_start
-	ln -s "${HOME}/Steam/steamapps" "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamapps"
+	ln -s "${steamappsdir}" "${serverfiles}/Engine/Binaries/ThirdParty/SteamCMD/Linux/steamapps"
 	fn_fix_msg_end
 fi
