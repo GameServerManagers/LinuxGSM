eb67e75064ce8e1bc46fdb786dfcbbaafbc655b6
Daniel Gibbs <me@danielgibbs.co.uk>
2020-04-01 21:31:04 +0100

    added ifs to vars

--- lgsm/functions/check_lastupdate.sh
+++ lgsm/functions/check_lastupdate.sh
@@ -4,10 +4,14 @@
 # Website: https://linuxgsm.com
 # Description: Checks lock file to see when last update happened.
 
-laststart=$(cat "${lockdir}/laststart.lock")
-lastupdate=$(cat "${lockdir}/lastupdate.lock")
+if [ -f "${lockdir}/laststart.lock" ]; then
+	laststart=$(cat "${lockdir}/laststart.lock")
+fi
+if [ -f "${lockdir}/lastupdate.lock" ]; then
+	lastupdate=$(cat "${lockdir}/lastupdate.lock")
+fi
 
-if [ "${laststart}" -lt "${lastupdate}" ]; then
+if [ -n "${laststart}" ]&&[ -n "${lastupdate}" ]&&[ "${laststart}" -lt "${lastupdate}" ]; then
 	fn_print_info "${selfname} has not been restarted since last update"
 	fn_script_log_info "${selfname} has not been restarted since last update"
 	command_restart.sh
