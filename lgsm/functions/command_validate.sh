ca7ad806dd9e1f02e577e044fc6a540734392492
Daniel Gibbs <me@danielgibbs.co.uk>
2020-04-03 22:10:43 +0100

    comments

--- lgsm/functions/command_validate.sh
+++ lgsm/functions/command_validate.sh
@@ -18,23 +18,27 @@ fn_validation(){
 	if [ -d "${steamcmddir}" ]; then
 		cd "${steamcmddir}" || exit
 	fi
-	# Detects if unbuffer command is available for 32 bit distributions only.
+	# Detects if unbuffer command is available, for 32 bit distributions only.
 	info_distro.sh
 	if [ "$(command -v stdbuf)" ]&&[ "${arch}" != "x86_64" ]; then
 		unbuffer="stdbuf -i0 -o0 -e0"
 	fi
 
+	# If goldsrc servers. Goldsrc requires extra commands.
 	if [ "${appid}" == "90" ]; then
+		# If using a specific branch
 		if [ "${branch}" ]; then
 			${unbuffer} ${steamcmdcommand} +login "${steamuser}" "${steampass}" +force_install_dir "${serverfiles}" +app_info_print 70 +app_set_config 90 mod "${appidmod}" +app_update "${appid}" "${branch}" +app_update "${appid}" -beta "${branch}" validate +quit | tee -a "${lgsmlog}"
 		else
 			${unbuffer} ${steamcmdcommand} +login "${steamuser}" "${steampass}" +force_install_dir "${serverfiles}" +app_info_print 70 +app_set_config 90 mod "${appidmod}" +app_update "${appid}" "${branch}" +app_update "${appid}" validate +quit | tee -a "${lgsmlog}"
 		fi
+	# All other servers.
 	elif [ "${branch}" ]; then
 		${unbuffer} ${steamcmdcommand} +login "${steamuser}" "${steampass}" +force_install_dir "${serverfiles}" +app_update "${appid}" -beta "${branch}" validate +quit | tee -a "${lgsmlog}"
 	else
 		${unbuffer} ${steamcmdcommand} +login "${steamuser}" "${steampass}" +force_install_dir "${serverfiles}" +app_update "${appid}" validate +quit | tee -a "${lgsmlog}"
 	fi
+	# Error checking.
 	if [ $? != 0 ]; then
 		fn_print_fail "Validating files: SteamCMD"
 		fn_script_log_fatal "Validating files: SteamCMD: FAIL"
@@ -43,7 +47,6 @@ fn_validation(){
 		fn_script_log_pass "Validating files: SteamCMD: OK"
 	fi
 	fix.sh
-
 }
 
 fn_print_dots "Validating files:"
