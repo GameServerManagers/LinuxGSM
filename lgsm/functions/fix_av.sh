7dd7415f5f38ba4a3ffbec672b1f5113b0995916
H3o66 <chris.h3o66@gmail.com>
2020-04-07 18:11:25 +0200

    revert a few changes and set server.sh as start binary

--- lgsm/functions/fix_av.sh
+++ lgsm/functions/fix_av.sh
@@ -7,13 +7,6 @@
 local commandname="FIX"
 local commandaction="Fix"
 
-if [ ! -d "${serverfiles}/bin/data" ]; then
-	fixname="symlink data dir"
-	fn_fix_msg_start
-	ln -s "${serverfiles}/data" "${serverfiles}/bin/data"
-	fn_fix_msg_end
-fi
-
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${serverfiles}:${serverfiles}/linux64"
 
 if [ "${postinstall}" == "1" ]; then
