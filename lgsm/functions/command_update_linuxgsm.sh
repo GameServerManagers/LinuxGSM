08cfde41bc7aee46a5eb134fcd2c241f59669895
Daniel Gibbs <me@danielgibbs.co.uk>
2020-05-16 23:29:26 +0100

    messages

--- lgsm/functions/command_update_linuxgsm.sh
+++ lgsm/functions/command_update_linuxgsm.sh
@@ -149,14 +149,16 @@ if [ -n "${functionsdir}" ]; then
 		do
 			# check if module exists in the repo and remove if missing.
 			# commonly used if module names change.
+			echo -en "checking ${remotereponame} module ${functionfile}...\c"
 			github_file_url_dir="lgsm/functions"
 			if [ "${remotereponame}" == "GitHub" ]; then
-				curl -IsfL "https://raw.githubusercontent.com/${githubuser}/${githubrepo}/${githubbranch}/${github_file_url_dir}/${functionfile}" 1>/dev/null
+				curl -IsfL "https://aw.githubusercontent.com/${githubuser}/${githubrepo}/${githubbranch}/${github_file_url_dir}/${functionfile}" 1>/dev/null
 			else
 				curl -IsfL "https://bitbucket.org/${githubuser}/${githubrepo}/raw/${githubbranch}/${github_file_url_dir}/${functionfile}" 1>/dev/null
 			fi
 			if [ $? -ne 0 ]; then
-				fn_print_fail_eol_nl
+				fn_print_error_eol_nl
+				fn_script_log_error "Checking ${remotereponame} module ${functionfile}"
 				echo -en "removing module ${functionfile}...\c"
 				fn_script_log_fatal "Removing module ${functionfile}"
 				if ! rm -f "${functionfile:?}"; then
