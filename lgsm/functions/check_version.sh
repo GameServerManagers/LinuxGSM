06f7093deb1b6d43f79300e44b8b1bb782e0665d
Daniel Gibbs <me@danielgibbs.co.uk>
2020-05-17 01:11:50 +0100

    messages

--- lgsm/functions/check_version.sh
+++ lgsm/functions/check_version.sh
@@ -7,5 +7,10 @@
 
 if [ -n "${modulesversion}" ]&&[ -n "${version}" ]&&[ "${version}" != "${modulesversion}" ]; then
 	exitbypass=1
+	fn_print_error "Version mismatch"
+	echo -en "${selfname}: ${version}"
+	echo -en "modules: ${modulesversion}"
+	echo -en ""
+	fn_script_log_error "Version mismatch: ${selfname}: ${version}: modules: ${modulesversion}"
 	command_update_linuxgsm.sh
 fi
