3ae3d89bf745bb21bac637a452cc96fef5991100
Christian <h3o66@users.noreply.github.com>
2020-05-24 15:26:45 +0200

    fix(download): fix some files not downloading (#2904)

--- lgsm/functions/fix_mta.sh
+++ lgsm/functions/fix_mta.sh
@@ -11,6 +11,6 @@ if [ ! -f "${lgsmdir}/lib/libmysqlclient.so.16" ]; then
 	fixname="libmysqlclient16"
 	fn_fix_msg_start_nl
 	fn_sleep_time
-	fn_fetch_file "https://nightly.mtasa.com/files/modules/64/libmysqlclient.so.16" "${lgsmdir}/lib" "libmysqlclient.so.16" "chmodx" "norun" "noforce" "6c188e0f8fb5d7a29f4bc413b9fed6c2"
+	fn_fetch_file "https://nightly.mtasa.com/files/modules/64/libmysqlclient.so.16" "" "" "" "${lgsmdir}/lib" "libmysqlclient.so.16" "chmodx" "norun" "noforce" "6c188e0f8fb5d7a29f4bc413b9fed6c2"
 	fn_fix_msg_end
 fi
