1c233e5452a0dac1046a1303de2c997a3fe6f3c3
scott <zedc130@comcast.net>
2020-05-25 20:42:24 +0900

    fix.sh
    accounting for the new d/l function

--- lgsm/functions/fix_openttd.sh
+++ lgsm/functions/fix_openttd.sh
@@ -10,5 +10,5 @@ local commandaction="Fix"
 
 mkdir "${rootdir}/.openttd/baseset"
 fn_print_information_nl "Downloading graphics pack"
-fn_fetch_file "https://cdn.openttd.org/opengfx-releases/0.6.0/opengfx-0.6.0-all.zip" "${tmpdir}" "opengfx-0.6.0-all.zip" "nochmodx" "norun" "noforce" "994d8ce816542b74130964971736d4d6"
+fn_fetch_file "https://cdn.openttd.org/opengfx-releases/0.6.0/opengfx-0.6.0-all.zip" "" "" "" "${tmpdir}" "opengfx-0.6.0-all.zip" "nochmodx" "norun" "noforce" "994d8ce816542b74130964971736d4d6"
 fn_dl_extract "${tmpdir}" "opengfx-0.6.0-all.zip" "${rootdir}/.openttd/baseset"
