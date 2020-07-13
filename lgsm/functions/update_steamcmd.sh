51ef4eeaa157f02d93c683af26b50509b6a38643
Daniel Gibbs <me@danielgibbs.co.uk>
2020-07-13 21:01:19 +0100

    betapassword remotebuild

--- lgsm/functions/update_steamcmd.sh
+++ lgsm/functions/update_steamcmd.sh
@@ -43,7 +43,9 @@ fn_update_steamcmd_remotebuild(){
 		find "${HOME}" -type f -name "appinfo.vdf" -exec rm -f {} \;
 	fi
 
-	if [ -n "${branch}" ]; then
+	if [ -n "${branch}" ]&&[ -n "${betapassword}" ]; then
+		remotebuild=$(${steamcmdcommand} +login "${steamuser}" "${steampass}" +app_info_update 1 +app_info_print "${appid}" -beta "${branch}" -betapassword "${betapassword}" +quit | sed '1,/branches/d' | sed "1,/${branchname}/d" | grep -m 1 buildid | tr -cd '[:digit:]')
+	elif [ -n "${branch}" ]; then
 		remotebuild=$(${steamcmdcommand} +login "${steamuser}" "${steampass}" +app_info_update 1 +app_info_print "${appid}" -beta "${branch}" +quit | sed '1,/branches/d' | sed "1,/${branchname}/d" | grep -m 1 buildid | tr -cd '[:digit:]')
 	else
 		remotebuild=$(${steamcmdcommand} +login "${steamuser}" "${steampass}" +app_info_update 1 +app_info_print "${appid}" +quit | sed '1,/branches/d' | sed "1,/${branchname}/d" | grep -m 1 buildid | tr -cd '[:digit:]')
