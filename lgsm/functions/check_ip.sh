3a430e42d9a68ae33c64686d30b21e099104be75
Daniel Gibbs <me@danielgibbs.co.uk>
2020-09-09 20:57:38 +0100

    bug

--- lgsm/functions/check_ip.sh
+++ lgsm/functions/check_ip.sh
@@ -17,10 +17,11 @@ else
 fi
 getip=$(${ipcommand} -o -4 addr | awk '{print $4}' | grep -oe '\([0-9]\{1,3\}\.\?\)\{4\}'|sort -u|grep -v 127.0.0)
 getipwc=$(${ipcommand} -o -4 addr | awk '{print $4}' | grep -oe '\([0-9]\{1,3\}\.\?\)\{4\}'|sort -u|grep -vc 127.0.0)
-# Check if server has m ultiple IP addresses
+
+# Check if server has multiple IP addresses
 
 # If the IP variable has been set by user.
-if [ -n "${ip}" ]; then
+if [ -n "${ip}" ]||[ "${ip}" != "0.0.0.0" ]; then
 	queryips=( "${ip}" )
 # If game server IP is set IP address in game config.
 elif [ -n "${configip}" ]; then
