fa5a6689972b24e51b8da1ab094546a13c733432
Christian <h3o66@users.noreply.github.com>
2020-09-13 22:41:01 +0200

    fix(csserver): fix missing steamclient.so (#3030)
    Co-authored-by: Daniel Gibbs <me@danielgibbs.co.uk>

---
+++ lgsm/functions/fix_cs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# LinuxGSM fix_cs.sh function
+# Author: Christian Birk
+# Website: https://linuxgsm.com
+# Description: Resolves various issues with Counter Strike.
+
+functionselfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+if [ "${shortname}" == "cs" ]; then
+	# Fixes: steamclient.so: cannot open shared object file: No such file or directory
+	if [ ! -f "${serverfiles}/steamclient.so" ]; then
+		fixname="steamclient.so x86"
+		fn_fix_msg_start
+		if [ -f "${HOME}/.steam/steamcmd/linux32/steamclient.so" ]; then
+			cp "${steamcmddir}/linux32/steamclient.so" "${serverfiles}/steamclient.so" >> "${lgsmlog}"
+		elif [ -f "${steamcmddir}/linux32/steamclient.so" ]; then
+			cp "${steamcmddir}/linux32/steamclient.so" "${serverfiles}/steamclient.so" >> "${lgsmlog}"
+		fi
+		fn_fix_msg_end
+	fi
+fi
