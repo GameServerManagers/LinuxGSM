30165604c1ba0a9d0a76877d050610871912db97
Daniel Gibbs <me@danielgibbs.co.uk>
2020-09-15 10:10:18 +0100

    more changes to steamclient.so

--- lgsm/functions/fix_rust.sh
+++ lgsm/functions/fix_rust.sh
@@ -7,4 +7,4 @@
 functionselfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 # Fixes: [Raknet] Server Shutting Down (Shutting Down).
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${systemdir}/RustDedicated_Data/Plugins/x86_64"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${serverfiles}:${serverfiles}/RustDedicated_Data/Plugins/x86_64"
--- lgsm/functions/fix_sdtd.sh
+++ lgsm/functions/fix_sdtd.sh
@@ -6,17 +6,4 @@
 
 functionselfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${serverfiles}"
-
-# Fixes: [S_API FAIL] SteamAPI_Init() failed; unable to locate a running instance of Steam, or a local steamclient.so.
-if [ ! -f "${serverfiles}/steamclient.so" ]; then
-	fixname="steamclient.so x86_64"
-	fn_fix_msg_start
-	mkdir -p "${serverfiles}"
-	if [ -f "${HOME}/.steam/steamcmd/linux64/steamclient.so" ]; then
-		cp "${HOME}/.steam/steamcmd/linux64/steamclient.so" "${serverfiles}/steamclient.so"
-	elif [ -f "${steamcmddir}/linux64/steamclient.so" ]; then
-		cp "${steamcmddir}/linux64/steamclient.so" "${serverfiles}/steamclient.so"
-	fi
-	fn_fix_msg_end
-fi
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${serverfiles}/7DaysToDieServer_Data/Plugins/x86_64"
