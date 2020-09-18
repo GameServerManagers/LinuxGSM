487e7566f92af885f6b222a52cf9ba5e6d48a6f5
Daniel Gibbs <me@danielgibbs.co.uk>
2020-09-18 10:52:06 +0100

    sendgrid

---
+++ lgsm/functions/alert_sendgrid.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# LinuxGSM alert_sendgrid.sh function
+# Author: Daniel Gibbs
+# Website: https://linuxgsm.com
+# Description: Sends Sendgrid Email alert.
+
+functionselfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+if [ "${mailgunapiregion}" == "eu" ]; then
+	mailgunapiurl="https://api.eu.mailgun.net"
+else
+	mailgunapiurl="https://api.mailgun.net"
+fi
+
+fn_print_dots "Sending Email alert: Mailgun: ${email}"
+
+mailgunsend=$(curl -s --user "api:${mailguntoken}" \
+-F from="LinuxGSM <${mailgunemailfrom}>" \
+-F to="LinuxGSM Admin <${mailgunemail}>" \
+-F subject="${alertemoji} ${alertsubject} ${alertemoji}" \
+-F o:tag='alert' \
+-F o:tag='LinuxGSM' \
+-F text="$(cat "${alertlog}")" "${mailgunapiurl}/v3/${mailgundomain}/messages")
+
+if [ -z "${mailgunsend}" ]; then
+	fn_print_fail_nl "Sending Email alert: Mailgun: ${email}"
+	fn_script_log_fatal "Sending Email alert: Mailgun: ${email}"
+else
+	fn_print_ok_nl "Sending Email alert: Mailgun: ${email}"
+	fn_script_log_pass "Sending Email alert: Mailgun: ${email}"
+fi
