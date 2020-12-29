6ec97a7c420e7d0480ee6c29853243cd21e0bad0
Daniel Gibbs <me@danielgibbs.co.uk>
2020-12-29 15:39:40 +0000

    initial commit of skel

---
+++ lgsm/functions/command_skeleton.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# LinuxGSM command_skeleton.sh function
+# Author: Daniel Gibbs
+# Website: https://linuxgsm.com
+# Description: Creates an copy of a game servers directorys.
+
+
+fn_print_dots ""
+check.sh
+
+# Find all directorys and create them in the skel directory
+find ${rootdir} -type d -not \( -path ./skel -prune \)
