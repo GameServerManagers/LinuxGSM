a075300589ffe88c9fe31eda9cf82db7347f0a41
Christian <h3o66@users.noreply.github.com>
2020-12-31 11:33:33 +0100

    feat(check-update): add new command check-update for steamcmd game servers (#3146)

---
+++ lgsm/functions/command_check_update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# LinuxGSM command_check_update.sh function
+# Author: Daniel Gibbs
+# Website: https://linuxgsm.com
+# Description: Handles updating of servers.
+
+commandname="CHECK-UPDATE"
+commandaction="check for Update"
+functionselfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
+fn_firstcommand_set
+
+fn_print_dots ""
+check.sh
+core_logs.sh
+
+core_steamcmd.sh
+
+check_steamcmd.sh
+
+fn_update_steamcmd_localbuild
+fn_update_steamcmd_remotebuild
+fn_update_steamcmd_compare
+
+core_exit.sh
