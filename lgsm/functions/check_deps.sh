42d919d7072c093047772ac06b0bf5d4598c245a
H3o66 <chris.h3o66@gmail.com>
2021-01-11 15:57:51 +0100

    fix(pvr): add new dependency

--- lgsm/functions/check_deps.sh
+++ lgsm/functions/check_deps.sh
@@ -464,6 +464,9 @@ fn_deps_build_debian(){
 	# Post Scriptum
 	elif [ "${shortname}" == "pstbs" ]; then
 		array_deps_required+=( libgconf-2-4 )
+	# Pavlov VR
+	elif [ "${shortname}" == "pvr" ]; then
+		array_deps_required+=( libc++1 )
 	fi
 	fn_deps_email
 	fn_check_loop
@@ -579,6 +582,9 @@ fn_deps_build_redhat(){
 	# Post Scriptum
 	elif [ "${shortname}" == "pstbs" ]; then
 		array_deps_required+=( GConf2 )
+	# Pavlov VR
+	elif [ "${shortname}" == "pvr" ]; then
+		array_deps_required+=( libcxx )
 	fi
 	fn_deps_email
 	fn_check_loop
