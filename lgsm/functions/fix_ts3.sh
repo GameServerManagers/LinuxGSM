efb71a54cb9d6b0fd3625c85b4ac7bc3c3d4b237
H3o66 <chris.h3o66@gmail.com>
2021-01-27 19:19:00 +0100

    fix(ts3): add fix to detect accoutning file and delete if possible

--- lgsm/functions/fix_ts3.sh
+++ lgsm/functions/fix_ts3.sh
@@ -14,3 +14,20 @@ if [ ! -f "${serverfiles}/libmariadb.so.2" ]; then
 	cp "${serverfiles}/redist/libmariadb.so.2" "${serverfiles}/libmariadb.so.2"
 	fn_fix_msg_end
 fi
+
+# Fixes: failed to register local accounting service: No such file or directory
+accountingfile="/dev/shm/7gbhujb54g8z9hu43jre8"
+if [ -f "${accountingfile}" ]; then
+	# check permissions for the file if the current user owns it, if not exit as well and exit as well
+	if [ "$( stat -c %U ${accountingfile})" == "$(whoami)" ]; then
+		fixname="Delete file ${accountingfile}"
+		fn_fix_msg_start
+		rm -f "${accountingfile}"
+		fn_fix_msg_end
+	# file is not owned by the current user and needs to be deleted manually
+	else
+		fn_print_error_nl "File ${accountingfile} is not owned by $(whoami) and needs to be deleted manually"
+		fn_script_log_fatal "File ${accountingfile} is not owned by $(whoami) and needs to be deleted manually"
+		core_exit.sh
+	fi
+fi
