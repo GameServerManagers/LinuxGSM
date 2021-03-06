bb2721c624c6b1d713a6815fa8ac59926dd3b8c5
H3o66 <chris.h3o66@gmail.com>
2021-03-06 22:53:08 +0100

    fix: regression from #3328 for tmux version

--- lgsm/functions/info_distro.sh
+++ lgsm/functions/info_distro.sh
@@ -73,8 +73,10 @@ glibcversion=$(ldd --version | sed -n '1s/.* //p')
 # e.g: tmux 1.6
 if [ ! "$(command -V tmux 2>/dev/null)" ]; then
 	tmuxv="${red}NOT INSTALLED!${default}"
+	tmuxvdigit="0"
 else
-	if [ "$(tmux -V | sed "s/tmux //" | sed -n '1 p' | tr -cd '[:digit:]')" -lt "16" ]; then
+	tmuxvdigit="$(tmux -V | sed "s/tmux //" | sed -n '1 p' | tr -cd '[:digit:]')"
+	if [ "${tmuxvdigit}" -lt "16" ]; then
 		tmuxv="$(tmux -V) (>= 1.6 required for console log)"
 	else
 		tmuxv=$(tmux -V)
