44366cc22b9a8864b83682adece19bb8312c0ff8
Daniel Gibbs <me@danielgibbs.co.uk>
2021-04-10 23:16:30 +0100

    add more ports
    merge
    
    
    words

--- lgsm/functions/command_dev_query_raw.sh
+++ lgsm/functions/command_dev_query_raw.sh
@@ -32,10 +32,35 @@ else
 	echo -e "Game:"
 fi
 
+if [ -v port6 ]; then
+	echo -e "Game ipv6: \t${port6} \t$(ss -tupl|grep ${port6}|wc -l) \t$(ss -tupl|grep ${port6}|grep tcp|awk '{ print $2 }') \t$(ss -tupl|grep ${port6}|grep udp|awk '{ print $2 }')"
+else
+	echo -e "Game ipv6:"
+fi
+
 if [ -v queryport ]; then
 	echo -e "Query: \t${queryport} \t$(ss -tupl|grep ${queryport}|wc -l) \t$(ss -tupl|grep ${queryport}|grep tcp|awk '{ print $2 }') \t$(ss -tupl|grep ${queryport}|grep udp|awk '{ print $2 }')"
 else
 	echo -e "Query:"
+fi
+
+if [ -v httpport ]; then
+	echo -e "HTTP: \t${httpport} \t$(ss -tupl|grep ${httpport}|wc -l) \t$(ss -tupl|grep ${httpport}|grep tcp|awk '{ print $2 }') \t$(ss -tupl|grep ${httpport}|grep udp|awk '{ print $2 }')"
+else
+	echo -e "HTTP:"
+fi
+
+if [ -v httpqueryport ]; then
+	echo -e "HTTP Query: \t${httpqueryport} \t$(ss -tupl|grep ${httpqueryport}|wc -l) \t$(ss -tupl|grep ${httpqueryport}|grep tcp|awk '{ print $2 }') \t$(ss -tupl|grep ${httpqueryport}|grep udp|awk '{ print $2 }')"
+else
+	echo -e "HTTP Query:"
+fi
+
+
+if [ -v webadminport ]; then
+	echo -e "Web Admin: \t${webadminport} \t$(ss -tupl|grep ${webadminport}|wc -l) \t$(ss -tupl|grep ${webadminport}|grep tcp|awk '{ print $2 }') \t$(ss -tupl|grep ${webadminport}|grep udp|awk '{ print $2 }')"
+else
+	echo -e "Web Admin:"
 fi
 
 if [ -v clientport ]; then
@@ -50,11 +75,6 @@ else
 	echo -e "RCON:"
 fi
 
-if [ -v httpport ]; then
-	echo -e "HTTP: \t${httpport} \t$(ss -tupl|grep ${httpport}|wc -l) \t$(ss -tupl|grep ${httpport}|grep tcp|awk '{ print $2 }') \t$(ss -tupl|grep ${httpport}|grep udp|awk '{ print $2 }')"
-else
-	echo -e "HTTP:"
-fi
 
 if [ -v steamport ]; then
 	echo -e "Steam: \t${steamport} \t$(ss -tupl|grep ${steamport}|wc -l) \t$(ss -tupl|grep ${steamport}|grep tcp|awk '{ print $2 }') \t$(ss -tupl|grep ${steamport}|grep udp|awk '{ print $2 }')"
@@ -68,6 +88,59 @@ else
 	echo -e "Raw UDP:"
 fi
 
+if [ -v masterport ]; then
+	echo -e "Game: Master: \t${masterport} \t$(ss -tupl|grep ${masterport}|wc -l) \t$(ss -tupl|grep ${masterport}|grep tcp|awk '{ print $2 }') \t$(ss -tupl|grep ${masterport}|grep udp|awk '{ print $2 }')"
+else
+	echo -e "Game: Master:"
+fi
+
+if [ -v steamauthenticationport ]; then
+	echo -e "Steam: Auth: \t${steamauthenticationport} \t$(ss -tupl|grep ${steamauthenticationport}|wc -l) \t$(ss -tupl|grep ${steamauthenticationport}|grep tcp|awk '{ print $2 }') \t$(ss -tupl|grep ${steamauthenticationport}|grep udp|awk '{ print $2 }')"
+else
+	echo -e "Steam: Auth:"
+fi
+
+if [ -v steammasterserverport ]; then
+	echo -e "Steam: Master: \t${steammasterserverport} \t$(ss -tupl|grep ${steammasterserverport}|wc -l) \t$(ss -tupl|grep ${steammasterserverport}|grep tcp|awk '{ print $2 }') \t$(ss -tupl|grep ${steammasterserverport}|grep udp|awk '{ print $2 }')"
+else
+	echo -e "Steam: Master:"
+fi
+
+if [ -v beaconport ]; then
+	echo -e "Beacon: \t${beaconport} \t$(ss -tupl|grep ${beaconport}|wc -l) \t$(ss -tupl|grep ${beaconport}|grep tcp|awk '{ print $2 }') \t$(ss -tupl|grep ${beaconport}|grep udp|awk '{ print $2 }')"
+else
+	echo -e "Beacon:"
+fi
+
+if [ -v appport ]; then
+	echo -e "App: \t${appport} \t$(ss -tupl|grep ${appport}|wc -l) \t$(ss -tupl|grep ${appport}|grep tcp|awk '{ print $2 }') \t$(ss -tupl|grep ${appport}|grep udp|awk '{ print $2 }')"
+else
+	echo -e "App:"
+fi
+
+if [ -v telnetport ]; then
+	echo -e "Telnet: \t${telnetport} \t$(ss -tupl|grep ${telnetport}|wc -l) \t$(ss -tupl|grep ${telnetport}|grep tcp|awk '{ print $2 }') \t$(ss -tupl|grep ${telnetport}|grep udp|awk '{ print $2 }')"
+else
+	echo -e "Telnet:"
+fi
+
+if [ -v sourcetvport ]; then
+	echo -e "SourceTV: \t${sourcetvport} \t$(ss -tupl|grep ${sourcetvport}|wc -l) \t$(ss -tupl|grep ${sourcetvport}|grep tcp|awk '{ print $2 }') \t$(ss -tupl|grep ${sourcetvport}|grep udp|awk '{ print $2 }')"
+else
+	echo -e "SourceTV:"
+fi
+
+if [ -v fileport ]; then
+	echo -e "File: \t${fileport} \t$(ss -tupl|grep ${fileport}|wc -l) \t$(ss -tupl|grep ${fileport}|grep tcp|awk '{ print $2 }') \t$(ss -tupl|grep ${fileport}|grep udp|awk '{ print $2 }')"
+else
+	echo -e "File:"
+fi
+
+if [ -v udplinkport ]; then
+	echo -e "UDP Link: \t${udplinkport} \t$(ss -tupl|grep ${udplinkport}|wc -l) \t$(ss -tupl|grep ${udplinkport}|grep tcp|awk '{ print $2 }') \t$(ss -tupl|grep ${udplinkport}|grep udp|awk '{ print $2 }')"
+else
+	echo -e "UDP Link:"
+fi
 } | column -s $'\t' -t
 echo -e ""
 echo -e "${lightgreen}Ports Raw Output${default}"
