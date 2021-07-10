b2484c8994ff681843fbc19598405434e16e7bc7
Daniel Gibbs <me@danielgibbs.co.uk>
2021-07-10 22:40:49 +0100

    httpport

--- lgsm/functions/info_messages.sh
+++ lgsm/functions/info_messages.sh
@@ -1382,7 +1382,7 @@ fn_info_message_kf(){
 	fn_messages_separator
 	{
 		echo -e "${lightblue}Web Admin enabled:\t${default}${webadminenabled}"
-		echo -e "${lightblue}Web Admin url:\t${default}http://${webadminip}:${webadminport}"
+		echo -e "${lightblue}Web Admin url:\t${default}http://${webadminip}:${httpport}"
 		echo -e "${lightblue}Web Admin username:\t${default}${webadminuser}"
 		echo -e "${lightblue}Web Admin password:\t${default}${webadminpass}"
 	} | column -s $'\t' -t
