49c3b09df91eeadf1a5e2e81d6bb02128a1a77ce
Claiyc <58397976+Claiyc@users.noreply.github.com>
2021-08-01 23:16:23 +0200

    add doublequotes in fix_lo.sh

--- lgsm/functions/fix_lo.sh
+++ lgsm/functions/fix_lo.sh
@@ -10,7 +10,7 @@ local APPID_FILE=${executabledir}/steam_appid.txt
 if [ ! -f "${APPID_FILE}" ]; then
     fn_print_information "adding ${APPID_FILE} to ${gamename} server."
     fn_sleep_time
-    echo "903950" > ${APPID_FILE}
+    echo "903950" > "${APPID_FILE}"
 else
     fn_print_information "${APPID_FILE} already exists. No action to be taken."
     fn_sleep_time
