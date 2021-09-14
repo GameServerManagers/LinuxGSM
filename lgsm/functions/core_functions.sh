436c87de1918e92dc1558cfe8d29f663a33a2453
jamiew0w <jamiew0w@users.noreply.github.com>
2021-09-14 21:20:10 +0000

    feat(alerts): add Gotify support to alerts (#3509)
    * added gotify to default configs
    
    * added gotify to info_messages.sh
    
    * added gotify to core_function.sh
    
    * added gotify to alerts.sh
    
    * core of alert_gotify.sh
    
    * fixed unterminated string in default configs
    
    * fixed mistakes in alert_gotify.sh
    
    * fixed bad quoting in alert_gotify.sh
    
    * first working version, gotify does not like emojis
    
    * added back curl options
    
    * added catch for $gotifywebhook to alert.sh
    
    * tabs over spaces
    
    * standardised json
    
    * message
    
    Co-authored-by: Daniel Gibbs <me@danielgibbs.co.uk>

--- lgsm/functions/core_functions.sh
+++ lgsm/functions/core_functions.sh
@@ -570,6 +570,11 @@ functionfile="${FUNCNAME[0]}"
 fn_fetch_function
 }
 
+alert_gotify.sh(){
+functionfile="${FUNCNAME[0]}"
+fn_fetch_function
+}
+
 alert_telegram.sh(){
 functionfile="${FUNCNAME[0]}"
 fn_fetch_function
