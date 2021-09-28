d2a71894789465505254ddd19694f4c2e504c39f
Daniel Gibbs <me@danielgibbs.co.uk>
2021-09-28 22:15:29 +0100

    update datafile

--- lgsm/functions/check_deps.sh
+++ lgsm/functions/check_deps.sh
@@ -312,13 +312,6 @@ fi
 
 info_distro.sh
 
-# some RHEL based distros use 8.4 instead of just 8.
-if [[ "${distroidlike}" == *"rhel"* ]]||[ "${distroid}" == "rhel" ]; then
-	distroversioncsv="${distroversionrh}"
-else
-	distroversioncsv="${distroversion}"
-fi
-
 if [ ! -f "${tmpdir}/dependency-no-check.tmp" ]&&[ ! -f "${datadir}/${distroid}-${distroversioncsv}.csv" ]; then
 	# Check that the disto dependency csv file exists.
 	fn_check_file_github "lgsm/data" "${distroid}-${distroversioncsv}.csv"
