fb2d5544105ee749a78df0d796f173b8ccebfec0
Claiyc <58397976+Claiyc@users.noreply.github.com>
2021-10-29 23:13:37 +0200

    fix: add sf & lo to info_game (#3653)

--- lgsm/functions/info_game.sh
+++ lgsm/functions/info_game.sh
@@ -790,6 +790,14 @@ fn_info_game_kf2(){
 	defaultmap=${defaultmap:-"NOT SET"}
 }
 
+fn_info_game_lo(){
+	# Parameters
+	servername=${servername:-"NOT SET"}
+	port=${port:-"0"}
+	queryport=${queryport:-"0"}
+	maxplayers=${slots:-"0"}
+}
+
 fn_info_game_mc(){
 	# Config
 	if [ ! -f "${servercfgfullpath}" ]; then
@@ -1550,6 +1558,14 @@ fn_info_game_sdtd(){
 	fi
 }
 
+fn_info_game_sf(){
+	# Parameters
+	servername=${selfname:-"NOT SET"}
+	port=${port:-"0"}
+	queryport=${queryport:-"0"}
+	beaconport=${beaconport:-"0"}
+}
+
 fn_info_game_sof2(){
 	# Config
 	if [ ! -f "${servercfgfullpath}" ]; then
@@ -2230,6 +2246,8 @@ elif [ "${shortname}" == "kf" ]; then
 	fn_info_game_kf
 elif [ "${shortname}" == "kf2" ]; then
 	fn_info_game_kf2
+elif [ "${shortname}" == "lo" ]; then
+	fn_info_game_lo
 elif [ "${shortname}" == "mc" ]||[ "${shortname}" == "pmc" ]; then
 	fn_info_game_mc
 elif [ "${shortname}" == "mcb" ]; then
@@ -2280,6 +2298,8 @@ elif [ "${shortname}" == "scpsl" ]||[ "${shortname}" == "scpslsm" ]; then
 	fn_info_game_scpsl
 elif [ "${shortname}" == "sdtd" ]; then
 	fn_info_game_sdtd
+elif [ "${shortname}" == "sf" ]; then
+	fn_info_game_sf
 elif [ "${shortname}" == "sof2" ]; then
 	fn_info_game_sof2
 elif [ "${shortname}" == "sol" ]; then
