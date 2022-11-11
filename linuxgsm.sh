6cf4c1e00ea78be10dee9cbadf3f0faa3510c03f
alasdair <alasdair.coille@gmail.com>
2022-11-11 21:32:53 +0100

    Revert "Have fork reference self"
    This reverts commit 756cac442f17e824e3301f216a824a54df93a5b7.

--- linuxgsm.sh
+++ linuxgsm.sh
@@ -47,9 +47,9 @@ userinput2="${2}"
 ## GitHub Branch Select
 # Allows for the use of different function files
 # from a different repo and/or branch.
-githubuser="AlasdairHaig"
-githubrepo="LinuxGSM"
-githubbranch="master"
+[ -n "${LGSM_GITHUBUSER}" ] && githubuser="${LGSM_GITHUBUSER}" || githubuser="GameServerManagers"
+[ -n "${LGSM_GITHUBREPO}" ] && githubrepo="${LGSM_GITHUBREPO}" || githubrepo="LinuxGSM"
+[ -n "${LGSM_GITHUBBRANCH}" ] && githubbranch="${LGSM_GITHUBBRANCH}" || githubbranch="master"
 
 # Check that curl is installed before doing anything
 if [ ! "$(command -v curl 2>/dev/null)" ]; then
