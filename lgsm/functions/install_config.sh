c9536b3d921f4e49c14af3424a37882f7929bcfa
Marcin Jakubowski <marcin648@gmail.com>
2023-02-07 22:09:27 +0000

    feat(ets2): add remote game config

--- lgsm/functions/install_config.sh
+++ lgsm/functions/install_config.sh
@@ -444,6 +444,14 @@ elif [ "${shortname}" == "etl" ]; then
 	fn_default_config_remote
 	fn_set_config_vars
 	fn_list_config_locations
+elif [ "${shortname}" == "ets2" ]; then
+	gamedirname="EuroTruckSimulator2"
+	fn_check_cfgdir
+	array_configs+=(server_config.sii)
+	fn_fetch_default_config
+	fn_default_config_remote
+	fn_set_config_vars
+	fn_list_config_locations
 elif [ "${shortname}" == "fctr" ]; then
 	gamedirname="Factorio"
 	array_configs+=(server-settings.json)
