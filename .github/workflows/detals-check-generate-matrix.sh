1c705daf923eea5ad085b0f0b10d3e50d51a3cb0
Daniel Gibbs <me@danielgibbs.co.uk>
2023-05-15 21:22:01 +0100

    codacy

--- .github/workflows/detals-check-generate-matrix.sh
+++ .github/workflows/detals-check-generate-matrix.sh
@@ -6,10 +6,14 @@ echo -n "{" > "shortnamearray.json"
 echo -n "\"include\":[" >> "shortnamearray.json"
 
 while read line; do
-	export shortname=$(echo "$line" | awk -F, '{ print $1 }')
-	export servername=$(echo "$line" | awk -F, '{ print $2 }')
-	export gamename=$(echo "$line" | awk -F, '{ print $3 }')
-	export distro=$(echo "$line" | awk -F, '{ print $4 }')
+	shortname=$(echo "$line" | awk -F, '{ print $1 }')
+	export shortname
+	servername=$(echo "$line" | awk -F, '{ print $2 }')
+	export servername
+	gamename=$(echo "$line" | awk -F, '{ print $3 }')
+	export gamename
+	distro=$(echo "$line" | awk -F, '{ print $4 }')
+	export distro
 	echo -n "{" >> "shortnamearray.json"
 	echo -n "\"shortname\":" >> "shortnamearray.json"
 	echo -n "\"${shortname}\"" >> "shortnamearray.json"
