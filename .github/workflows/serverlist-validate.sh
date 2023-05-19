027bc2003b58d4df849258bc5f09119b4613f7aa
Daniel Gibbs <me@danielgibbs.co.uk>
2023-05-19 13:00:57 +0100

    dir

--- .github/workflows/serverlist-validate.sh
+++ .github/workflows/serverlist-validate.sh
@@ -2,8 +2,8 @@
 echo "Checking that all the game servers are listed in all csv files"
 echo "this check will ensure serverlist.csv has the same number of lines (-2 lines) as the other csv files"
 # count the number of lines in the serverlist.csv
-cd lgsm/modules
-serverlistcount="$(wc -l < lgsm/modules/serverlist.csv)"
+cd lgsm/data
+serverlistcount="$(wc -l < serverlist.csv)"
 echo "serverlistcount: $serverlistcount"
 # get list of all csv files starting with ubunutu debian centos
 csvlist="$(ls -1 | grep -E '^(ubuntu|debian|centos|rhel|almalinux|rocky).*\.csv$')"
