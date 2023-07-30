ae6d811fd90a13d1c4aa6655548d1527e56a2c52
Daniel Gibbs <me@danielgibbs.co.uk>
2023-07-31 00:39:45 +0100

    refactor: improve file path handling in check_last_update.sh
    The code changes refactor the file path handling in check_last_update.sh to use the correct lock directory. This ensures that the last update time is correctly retrieved and compared with the last start time.

--- lgsm/modules/check_last_update.sh
+++ lgsm/modules/check_last_update.sh
@@ -11,12 +11,12 @@ moduleselfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
 if [ -f "${lockdir}/${selfname}-last-started.lock" ]; then
 	laststart=$(cat "${lockdir}/${selfname}-last-started.lock")
 fi
-if [ -f last-updated.lock" ]; then
-	lastupdate=$(cat last-updated.lock")
+if [ -f "${lockdir}/last-updated.lock" ]; then
+	lastupdate=$(cat "${lockdir}/last-updated.lock")
 fi
 
 check_status.sh
-if [ -f last-updated.lock" ] && [ "${status}" != "0" ]; then
+if [ -f "${lockdir}/last-updated.lock" ] && [ "${status}" != "0" ]; then
 	if [ ! -f "${lockdir}/${selfname}-last-started.lock" ] || [ "${laststart}" -lt "${lastupdate}" ]; then
 		fn_print_info "${selfname} has not been restarted since last update"
 		fn_script_log_info "${selfname} has not been restarted since last update"
