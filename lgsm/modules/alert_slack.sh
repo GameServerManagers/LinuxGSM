3912d02caaa6c46dfa49a33426b6c15924003c56
Daniel Gibbs <me@danielgibbs.co.uk>
2023-10-17 22:56:21 +0100

    refactor: improve alert message formatting and remove unnecessary code
    The commit refactors the alert message formatting in the `alert_pushbullet.sh`, `alert_slack.sh`, and `alert_telegram.sh` files. It improves the structure and readability of the messages by using proper markdown syntax. Additionally, it removes unnecessary code from the files to simplify their implementation.

--- lgsm/modules/alert_slack.sh
+++ lgsm/modules/alert_slack.sh
@@ -10,54 +10,68 @@ moduleselfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
 json=$(
 	cat << EOF
 {
-		"attachments": [
-			{
-				"color": "#36a64f",
-				"blocks": [
-					{
-										"type": "section",
-										"text": {
-												"type": "mrkdwn",
-												"text": "*LinuxGSM Alert*"
-										}
-								},
-								{
-										"type": "section",
-										"text": {
-												"type": "mrkdwn",
-												"text": "*${alertemoji} ${alerttitle}* \n ${alertmessage}"
-										}
-								},
-								{
-										"type": "divider"
-								},
-								{
-										"type": "section",
-										"fields": [
-												{
-														"type": "mrkdwn",
-														"text": "*Game:* \n ${gamename}"
-												},
-												{
-														"type": "mrkdwn",
-														"text": "*Server IP:* \n ${alertip}:${port}"
-												},
-												{
-														"type": "mrkdwn",
-														"text": "*Server Name:* \n ${servername}"
-												}
-										]
-								},
-					 {
-										"type": "section",
-										"text": {
-														"type": "mrkdwn",
-														"text": "Hostname: ${HOSTNAME} / More info: ${alerturl}"
-										}
-								}
-						]
-			}
-		]
+	"attachments": [
+		{
+			"color": "${alertcolourhex}",
+			"blocks": [
+				{
+					"type": "header",
+					"text": {
+						"type": "plain_text",
+						"text": "${alerttitle}",
+						"emoji": true
+					}
+				},
+				{
+					"type": "divider"
+				},
+				{
+					"type": "section",
+					"fields": [
+						{
+							"type": "mrkdwn",
+							"text": "*Game*\n${gamename}"
+						},
+						{
+							"type": "mrkdwn",
+							"text": "*Server IP*\n\`${alertip}:${port}\`"
+						},
+						{
+							"type": "mrkdwn",
+							"text": "*Hostname*\n${HOSTNAME}"
+						}
+					],
+					"accessory": {
+						"type": "image",
+						"image_url": "${alerticon}",
+						"alt_text": "cute cat"
+					}
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*Information*\n${alertmessage}"
+					}
+				},
+				{
+					"type": "context",
+					"elements": [
+						{
+							"type": "image",
+							"image_url": "https://raw.githubusercontent.com/${githubuser}/${githubrepo}/${githubbranch}/lgsm/data/alert_discord_logo.jpg",
+							"alt_text": "LinuxGSM icon"
+						},
+						{
+							"type": "plain_text",
+							"text": "Sent by LinuxGSM ${version}",
+							"emoji": true
+						}
+					]
+				}
+			]
+		}
+	]
 }
 EOF
 )
