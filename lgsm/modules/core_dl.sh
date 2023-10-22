069e3bd4e1232ff01131efa6239a7ff22d13c715
Daniel Gibbs <me@danielgibbs.co.uk>
2023-10-22 23:19:24 +0100

    refactor: remove legacy code
    core_functsions.sh is no longer required
    
    refactor: remove legacy code
    
    A lot of time has passed on many of these changes. Moving old legacy code conversions
    
    refactor: remove unused alert modules
    
    This commit removes the unused alert modules `alert.sh`, `alert_discord.sh`, and `alert_email.sh`. These modules were not being used in the codebase and were therefore deleted.

--- lgsm/modules/core_dl.sh
+++ lgsm/modules/core_dl.sh
@@ -481,12 +481,8 @@ fn_fetch_file() {
 fn_fetch_file_github() {
 	github_file_url_dir="${1}"
 	github_file_url_name="${2}"
-	# For legacy versions - code can be removed at a future date
-	if [ "${legacymode}" == "1" ]; then
-		remote_fileurl="https://raw.githubusercontent.com/${githubuser}/${githubrepo}/${githubbranch}/${github_file_url_dir}/${github_file_url_name}"
-		remote_fileurl_backup="https://bitbucket.org/${githubuser}/${githubrepo}/raw/${githubbranch}/${github_file_url_dir}/${github_file_url_name}"
 	# If master branch will currently running LinuxGSM version to prevent "version mixing". This is ignored if a fork.
-	elif [ "${githubbranch}" == "master" ] && [ "${githubuser}" == "GameServerManagers" ] && [ "${commandname}" != "UPDATE-LGSM" ]; then
+	if [ "${githubbranch}" == "master" ] && [ "${githubuser}" == "GameServerManagers" ] && [ "${commandname}" != "UPDATE-LGSM" ]; then
 		remote_fileurl="https://raw.githubusercontent.com/${githubuser}/${githubrepo}/${version}/${github_file_url_dir}/${github_file_url_name}"
 		remote_fileurl_backup="https://bitbucket.org/${githubuser}/${githubrepo}/raw/${version}/${github_file_url_dir}/${github_file_url_name}"
 	else
