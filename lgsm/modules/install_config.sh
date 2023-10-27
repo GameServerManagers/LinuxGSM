75006b2262b032ddc29af6f8ff08bdcd45157758
Daniel Gibbs <me@danielgibbs.co.uk>
2023-10-27 20:03:33 +0100

    config

--- lgsm/modules/install_config.sh
+++ lgsm/modules/install_config.sh
@@ -346,6 +346,12 @@ elif [ "${shortname}" == "cs" ]; then
 	fn_default_config_remote
 	fn_set_config_vars
 	fn_list_config_locations
+elif [ "${shortname}" == "cs2" ]; then
+	array_configs+=(server.cfg)
+	fn_fetch_default_config
+	fn_default_config_remote
+	fn_set_config_vars
+	fn_list_config_locations
 elif [ "${shortname}" == "cscz" ]; then
 	array_configs+=(server.cfg)
 	fn_fetch_default_config
