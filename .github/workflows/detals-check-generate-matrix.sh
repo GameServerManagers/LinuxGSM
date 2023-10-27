310e0b7606f5f40313f142d6a077e42d289776a9
Daniel Gibbs <me@danielgibbs.co.uk>
2023-10-27 23:00:27 +0100

    details check

--- .github/workflows/detals-check-generate-matrix.sh
+++ .github/workflows/detals-check-generate-matrix.sh
@@ -6,20 +6,18 @@ echo -n "{" > "shortnamearray.json"
 echo -n "\"include\":[" >> "shortnamearray.json"
 
 while read -r line; do
-	if [ "${shortname}" != "sbots" ] && [ "${shortname}" != "inss" ]; then
-		shortname=$(echo "$line" | awk -F, '{ print $1 }')
-		export shortname
-		servername=$(echo "$line" | awk -F, '{ print $2 }')
-		export servername
-		gamename=$(echo "$line" | awk -F, '{ print $3 }')
-		export gamename
-		distro=$(echo "$line" | awk -F, '{ print $4 }')
-		export distro
-		echo -n "{" >> "shortnamearray.json"
-		echo -n "\"shortname\":" >> "shortnamearray.json"
-		echo -n "\"${shortname}\"" >> "shortnamearray.json"
-		echo -n "}," >> "shortnamearray.json"
-	fi
+	shortname=$(echo "$line" | awk -F, '{ print $1 }')
+	export shortname
+	servername=$(echo "$line" | awk -F, '{ print $2 }')
+	export servername
+	gamename=$(echo "$line" | awk -F, '{ print $3 }')
+	export gamename
+	distro=$(echo "$line" | awk -F, '{ print $4 }')
+	export distro
+	echo -n "{" >> "shortnamearray.json"
+	echo -n "\"shortname\":" >> "shortnamearray.json"
+	echo -n "\"${shortname}\"" >> "shortnamearray.json"
+	echo -n "}," >> "shortnamearray.json"
 done < <(tail -n +2 serverlist.csv)
 sed -i '$ s/.$//' "shortnamearray.json"
 echo -n "]" >> "shortnamearray.json"
