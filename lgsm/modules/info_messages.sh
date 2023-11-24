dea17e9d9ade814a2f963cec49411685eeb53fc0
Daniel Gibbs <me@danielgibbs.co.uk>
2023-11-24 12:39:52 +0000

    feat: add port command for DayZ server
    This commit adds a new port command for the DayZ server in the info_messages.sh file. The port command is used to retrieve information about the DayZ server's ports using the "enfMain" keyword.

--- lgsm/modules/info_messages.sh
+++ lgsm/modules/info_messages.sh
@@ -692,6 +692,8 @@ fn_info_messages_ports() {
 		portcommand="ss -tuplwn | grep AvorionServer"
 	elif [ "${shortname}" == "bf1942" ]; then
 		portcommand="ss -tuplwn | grep bf1942_lnxded"
+	elif [ "${shortname}" == "dayz" ]; then
+		portcommand="ss -tuplwn | grep enfMain"
 	elif [ "${shortname}" == "mc" ] || [ "${shortname}" == "nec" ] || [ "${shortname}" == "pmc" ] || [ "${shortname}" == "vpmc" ] || [ "${shortname}" == "wmc" ]; then
 		portcommand="ss -tuplwn | grep java"
 	elif [ "${shortname}" == "terraria" ]; then
