605950e7f883183116d51928d24d27ac5cdec091
Daniel Gibbs <me@danielgibbs.co.uk>
2024-02-10 23:19:43 +0000

    and

--- lgsm/modules/install_gamedig.sh
+++ lgsm/modules/install_gamedig.sh
@@ -5,7 +5,7 @@
 # Website: https://linuxgsm.com
 # Description: Installs nodejs and gamedig
 
-if [ "$(command -v node)" ] || [ ! -f "${lgsmdir}/node_modules/gamedig/bin/gamedig.js" ]; then
+if [ "$(command -v node)" ] && [ ! -f "${lgsmdir}/node_modules/gamedig/bin/gamedig.js" ]; then
 	cd "${lgsmdir}" || exit
 	npm install gamedig@5.0.0-beta.2
 fi
