7b30b92d247d6c5d373bc8b067839493eeba0fbd
Daniel Gibbs <me@danielgibbs.co.uk>
2024-02-18 00:31:23 +0000

    download package.json and run npm install

--- lgsm/modules/install_gamedig.sh
+++ lgsm/modules/install_gamedig.sh
@@ -7,5 +7,6 @@
 
 if [ "$(command -v node)" ] && [ ! -f "${lgsmdir}/node_modules/gamedig/bin/gamedig.js" ]; then
 	cd "${lgsmdir}" || exit
-	npm install gamedig@5.0.0-beta.2
+	wget -N --no-check-certificate "https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/${githubbranch}/package.json"
+	npm install
 fi
