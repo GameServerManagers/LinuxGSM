095de5bb87db2c61b48fda5c673b4357e7f27aed
Daniel Gibbs <me@danielgibbs.co.uk>
2024-02-20 22:18:48 +0000

    adding message

--- lgsm/modules/install_gamedig.sh
+++ lgsm/modules/install_gamedig.sh
@@ -6,6 +6,9 @@
 # Description: Installs nodejs and gamedig
 
 if [ "$(command -v node)" ] && [ ! -f "${lgsmdir}/node_modules/gamedig/bin/gamedig.js" ]; then
+	echo -e ""
+	echo -e "${bold}${lightyellow}Installing Gamedig${default}"
+	fn_script_log_info "Installing Gamedig"
 	cd "${lgsmdir}" || exit
 	wget -N --no-check-certificate "https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/${githubbranch}/package.json"
 	npm install
