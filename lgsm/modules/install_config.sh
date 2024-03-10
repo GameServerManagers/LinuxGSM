21eb3cb625735f8a9153fd3fa747cfab82427cf9
Cristopfer Luis <ocristopfer@gmail.com>
2024-03-10 10:27:51 -0300

    fix: install config

--- lgsm/modules/install_config.sh
+++ lgsm/modules/install_config.sh
@@ -496,12 +496,8 @@ elif [ "${shortname}" == "hl2dm" ]; then
 	fn_set_config_vars
 	fn_list_config_locations
 elif [ "${shortname}" == "hz" ]; then
-	fn_check_cfgdir
-	array_configs+=(GameUserSettings.ini)
-	fn_fetch_default_config
-	fn_default_config_remote
-	fn_set_config_vars
-	fn_list_config_locations
+	# Config is generated on first run
+	:
 elif [ "${shortname}" == "ins" ]; then
 	array_configs+=(server.cfg)
 	fn_fetch_default_config
