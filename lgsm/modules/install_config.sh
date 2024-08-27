4d3f40ec5617ef0d73db9e8fd93b7c8323b0d308
Daniel Gibbs <me@danielgibbs.co.uk>
2024-08-28 00:47:27 +0100

    change to remove config

--- lgsm/modules/install_config.sh
+++ lgsm/modules/install_config.sh
@@ -842,7 +842,9 @@ elif [ "${shortname}" == "wmc" ]; then
 	fn_default_config_remote
 	fn_set_config_vars
 elif [ "${shortname}" == "xnt" ]; then
-	fn_default_config_local
+	fn_fetch_default_config
+	fn_default_config_remote
+	fn_set_config_vars
 	fn_list_config_locations
 elif [ "${shortname}" == "wurm" ]; then
 	array_configs+=(server.cfg)
