581ed3af573b34704c66560f889b513c640d51db
Daniel Gibbs <me@danielgibbs.co.uk>
2024-08-28 01:01:43 +0100

    remove serverconfigdefault

--- lgsm/modules/install_config.sh
+++ lgsm/modules/install_config.sh
@@ -841,6 +841,7 @@ elif [ "${shortname}" == "wmc" ]; then
 	fn_fetch_default_config
 	fn_default_config_remote
 	fn_set_config_vars
+	fn_list_config_locations
 elif [ "${shortname}" == "xnt" ]; then
 	array_configs+=(server.cfg)
 	fn_fetch_default_config
