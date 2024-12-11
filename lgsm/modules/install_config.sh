36717af4f2c24d2a9446478c12f332f7de08f88e
Tom Burnett <burnett.tommy@gmail.com>
2024-12-11 14:29:44 -0800

    fix(q3): Fix q3-only typo in install_config.sh (#4709)

--- lgsm/modules/install_config.sh
+++ lgsm/modules/install_config.sh
@@ -614,7 +614,7 @@ elif [ "${shortname}" == "q2" ]; then
 	fn_list_config_locations
 elif [ "${shortname}" == "q3" ]; then
 	array_configs+=(server.cfg)
-	fn_fetch_default_configs
+	fn_fetch_default_config
 	fn_default_config_remote
 	fn_set_config_vars
 	fn_list_config_locations
