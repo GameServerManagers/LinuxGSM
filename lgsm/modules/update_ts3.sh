1639bd5d0d37098f3545b7ced7a1fcb9e577b15f
Daniel Gibbs <me@danielgibbs.co.uk>
2025-01-05 13:27:03 +0000

    feat: add extra checks to fail if jq is not installed (#4731)

--- lgsm/modules/update_ts3.sh
+++ lgsm/modules/update_ts3.sh
@@ -171,6 +171,12 @@ fi
 # The location where the builds are checked and downloaded.
 remotelocation="teamspeak.com"
 
+if [ "$(command -v jq 2> /dev/null)" ]; then
+	fn_print_fail_nl "jq is not installed"
+	fn_script_log_fatal "jq is not installed"
+	core_exit.sh
+fi
+
 if [ "${firstcommandname}" == "INSTALL" ]; then
 	fn_update_remotebuild
 	fn_update_dl
