b2eb64532b4458c9f59fd880f9872ad79b7ecfab
Daniel Gibbs <me@danielgibbs.co.uk>
2025-03-07 16:27:57 +0000

    fix(alert): Restore more info in alert

--- lgsm/modules/alert_discord.sh
+++ lgsm/modules/alert_discord.sh
@@ -10,68 +10,81 @@ moduleselfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
 json=$(
 	cat << EOF
 {
-    "username": "LinuxGSM",
-    "avatar_url": "https://raw.githubusercontent.com/${githubuser}/${githubrepo}/${githubbranch}/lgsm/data/alert_discord_logo.jpg",
-    "file": "content",
-    "embeds": [
-        {
-            "author": {
-                "name": "LinuxGSM Alert",
-                "url": "",
-                "icon_url": "https://raw.githubusercontent.com/${githubuser}/${githubrepo}/${githubbranch}/lgsm/data/alert_discord_logo.jpg"
-            },
-            "title": "${alerttitle}",
-            "url": "",
-            "description": "",
-            "color": "${alertcolourdec}",
-            "type": "content",
-            "thumbnail": {
-                "url": "${alerticon}"
-            },
-            "fields": [
-                {
-                    "name": "Server Name",
-                    "value": "${servername}"
-                },
-                {
-                    "name": "Information",
-                    "value": "${alertmessage}"
-                },
-                {
-                    "name": "Game",
-                    "value": "${gamename}",
-                    "inline": true
-                },
-                {
-                    "name": "Server Time",
-                    "value": "$(date)",
-                    "inline": true
-                }
+	"username": "LinuxGSM",
+	"avatar_url": "https://raw.githubusercontent.com/${githubuser}/${githubrepo}/${githubbranch}/lgsm/data/alert_discord_logo.jpg",
+	"file": "content",
+	"embeds": [
+		{
+			"author": {
+				"name": "LinuxGSM Alert",
+				"url": "",
+				"icon_url": "https://raw.githubusercontent.com/${githubuser}/${githubrepo}/${githubbranch}/lgsm/data/alert_discord_logo.jpg"
+			},
+			"title": "${alerttitle}",
+			"url": "",
+			"description": "",
+			"color": "${alertcolourdec}",
+			"type": "content",
+			"thumbnail": {
+				"url": "${alerticon}"
+			},
+			"fields": [
+				{
+					"name": "Server Name",
+					"value": "${servername}"
+				},
+				{
+					"name": "Information",
+					"value": "${alertmessage}"
+				},
+				{
+					"name": "Game",
+					"value": "${gamename}",
+					"inline": true
+				},
+				{
+					"name": "Server Time",
+					"value": "$(date)",
+					"inline": true
+				}
 EOF
 )
 
 if [ -n "${querytype}" ]; then
 	json+=$(
 		cat << EOF
-                ,
-                {
-                    "name": "Is my Game Server Online?",
-                    "value": "https://ismygameserver.online/${imgsoquerytype}/${alertip}:${queryport}",
-                    "inline": true
-                }
+				,
+				{
+					"name": "Is my Game Server Online?",
+					"value": "https://ismygameserver.online/${imgsoquerytype}/${alertip}:${queryport}",
+					"inline": true
+				}
+EOF
+	)
+fi
+
+if [ -n "{alerturl}" ]; then
+	json+=$(
+		cat << EOF
+				,
+				{
+					"name": "More info",
+					"value": "${alerturl}",
+					"inline": true
+				}
 EOF
 	)
 fi
 
 json+=$(
 	cat << EOF
-            ],
-            "footer": {
-                "icon_url": "https://raw.githubusercontent.com/${githubuser}/${githubrepo}/${githubbranch}/lgsm/data/alert_discord_logo.jpg",
-                "text": "Sent by LinuxGSM ${version}"
-            }
-        }
-    ]
+			],
+			"footer": {
+				"icon_url": "https://raw.githubusercontent.com/${githubuser}/${githubrepo}/${githubbranch}/lgsm/data/alert_discord_logo.jpg",
+				"text": "Sent by LinuxGSM ${version}"
+			}
+		}
+	]
 }
 EOF
 )
