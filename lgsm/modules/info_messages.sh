7e937133f14d54ef72df76f4ee69483dfb748dd2
Daniel Gibbs <me@danielgibbs.co.uk>
2025-03-08 21:46:33 +0000

    fix(tf): change queryport to udp

--- lgsm/modules/info_messages.sh
+++ lgsm/modules/info_messages.sh
@@ -1658,7 +1658,7 @@ fn_info_messages_tf() {
 	{
 		fn_port "header"
 		fn_port "Game" port udp
-		fn_port "Query" queryport tcp
+		fn_port "Query" queryport udp
 		fn_port "Beacon" beaconport udp
 		fn_port "Shutdown" shutdownport tcp
 	} | column -s $'\t' -t
